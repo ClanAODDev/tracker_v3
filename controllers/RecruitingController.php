b9c9705d372f09f5d3c36e16cd00baab390bbd51
Chris Deaton <dcdeaton@ncsu.edu>
2015-03-13 17:01:53 -0400

    fix recruiting controller

--- controllers/RecruitingController.php
+++ controllers/RecruitingController.php
@@ -5,7 +5,7 @@ class RecruitingController
 	{
 		$user = User::find($_SESSION['userid']);
 		$member = Member::find($_SESSION['username']);
-		$tools = Tool::getToolsByRole($user->role);
+		$tools = Tool::find_all($user->role);
 		$divisions = Division::find_all();
 		$division = Division::find($member->game_id);
 		Flight::render('recruiting/index', array(), 'content');
