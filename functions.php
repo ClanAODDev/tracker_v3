2a60f5ec9bdf637b76b2bc81d0feb77c651cc351
Chris Deaton <dcdeaton@ncsu.edu>
2015-03-13 05:49:40 -0400

    add helper function objToArray

--- functions.php
+++ functions.php
@@ -74,3 +74,18 @@ function arrayToObject($d) {
         return $d;
     }
 }
+
+
+function objectToArray($d) {
+    if (is_object($d)) {
+        $d = get_object_vars($d);
+    }
+
+    if (is_array($d)) {
+        return array_map(__FUNCTION__, $d);
+    }
+    else {
+
+        return $d;
+    }
+}
