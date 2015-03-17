5bc2085c72e245b85093bf9e04e6f03ba9893a1f
Chris Deaton <dcdeaton@ncsu.edu>
2015-03-16 21:17:22 -0400

    Add video to section 1 help

--- views/application/help.php
+++ views/application/help.php
@@ -34,33 +34,10 @@
 			<p>Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
 
 			<h2 id='sec1'>Content</h2>
-			<p>
-				Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-				dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut.
-			</p>
-			<div class='row'>
-				<div class='col-md-6'>
-					<div class='panel panel-default'>
-						<div class='panel-heading'><h3>Hello.</h3></div>
-						<div class='panel-body'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-							Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-							dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-							Aliquam in felis sit amet augue.
-						</div>
-					</div>
-				</div>
-				<div class='col-md-6'>
-					<div class='panel panel-default'>
-						<div class='panel-heading'><h3>Hello.</h3></div>
-						<div class='panel-body'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-							Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-							dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-							Aliquam in felis sit amet augue.
-						</div>
-					</div>
-				</div>  
+			<div class="embed-responsive embed-responsive-4by3">
+				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/QAXekTvVucc" frameborder="0" allowfullscreen></iframe>
 			</div>
-
+			
 			<hr>
 
 			<h2 id='sec2'>Section 2</h2>
