9f739d1f8dc3159a01e16ed4d220253e2970d487
Chris Deaton <dcdeaton@ncsu.edu>
2015-03-20 12:11:49 -0400

    update favicon

--- views/layouts/application.php
+++ views/layouts/application.php
@@ -4,7 +4,7 @@
 	<title>AOD | Division Tracker v2</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<base href="<?php echo Flight::get('base_url') ?>">
-	<link rel="Shortcut Icon" href="assets/images/favicon.ico" type="image/x-icon" />
+	<link rel="Shortcut Icon" href="favicon.ico" type="image/x-icon" />
 	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.2/lumen/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/jquery.powertip.min.css">
 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">	
