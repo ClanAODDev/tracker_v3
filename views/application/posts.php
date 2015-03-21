7112c2183d90978e394d8de5530f815acf6f5e76
Chris Deaton <dcdeaton@ncsu.edu>
2015-03-21 03:07:28 -0400

    fix user post profile link

--- views/application/posts.php
+++ views/application/posts.php
@@ -4,7 +4,7 @@
 			<div class='panel-heading'><?php echo Member::avatar($post->forum_id) . " " .  $post->title; ?></div>
 			<div class='panel-body'><?php echo $post->content ?></div>
 			<div class='panel-footer text-muted text-right'>
-				<small>Posted <?php echo date("Y-m-d", strtotime($post->date)); ?> by <a href='/member/{$authorId}'><?php echo Member::findForumName($post->forum_id) ?></a></small>
+				<small>Posted <?php echo date("Y-m-d", strtotime($post->date)); ?> by <a href='member/<?php echo $post->forum_id ?>'><?php echo Member::findForumName($post->forum_id) ?></a></small>
 			</div>
 		</div>
 	<?php endforeach; ?>
