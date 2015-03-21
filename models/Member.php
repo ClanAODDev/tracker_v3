6adabcb59f783b590b737dcb4e13adc03adb50e7
Chris Deaton <dcdeaton@ncsu.edu>
2015-03-21 01:59:32 -0400

    unknown user gets tracker icon
    Signed-off-by: Chris Deaton <dcdeaton@ncsu.edu>

--- models/Member.php
+++ models/Member.php
@@ -45,7 +45,7 @@ class Member extends Application {
 	public static function avatar($mid, $type = "thumb")
 	{
 		$forum_img = "http://www.clanaod.net/forums/image.php?type={$type}&u={$mid}";
-		$unknown   = "assets/images/blank_avatar.jpg";
+		$unknown   = "assets/images/icons/tiny/tracker.png";
 		list($width, $height) = getimagesize($forum_img);
 
 		if ($width > 10 && $height > 10) {
