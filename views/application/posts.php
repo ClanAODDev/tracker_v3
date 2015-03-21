fb0e9f18b3e5b1eec083cae06b07cb8d858edc42
Chris Deaton <dcdeaton@ncsu.edu>
2015-03-21 01:53:29 -0400

    gap between title and avatar (posts)
    Signed-off-by: Chris Deaton <dcdeaton@ncsu.edu>

--- views/application/posts.php
+++ views/application/posts.php
@@ -1,7 +1,7 @@
 <?php if (count($posts)) : ?>
 	<?php foreach($posts as $post) : ?>
 		<div class='panel panel-default'>
-			<div class='panel-heading'><?php echo Member::avatar($post->forum_id) .  $post->title; ?></div>
+			<div class='panel-heading'><?php echo Member::avatar($post->forum_id) . " " .  $post->title; ?></div>
 			<div class='panel-body'><?php echo $post->content ?></div>
 			<div class='panel-footer text-muted text-right'>
 				<small>Posted <?php echo date("Y-m-d", strtotime($post->date)); ?> by <a href='/member/{$authorId}'><?php echo Member::findForumName($post->forum_id) ?></a></small>
