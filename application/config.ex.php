67e6a8be703b4f45ee40ca09bfe5cf5b8b6474bf
dcdeaton <dcdeaton@ncsu.edu>
2015-03-30 09:45:36 -0400

    redo organization

--- application/config.ex.php
+++ application/config.ex.php
@@ -1,16 +1,16 @@
 <?php
 
 date_default_timezone_set('America/New_York');
-require_once 'flight/Flight.php';
+require_once 'application/flight/Flight.php';
 
 // Autoload models and controllers
-Flight::path('models');
-Flight::path('controllers');
+Flight::path('application/models');
+Flight::path('application/controllers');
 
 // Set views path and environment
-Flight::set('flight.views.path', 'views');
+Flight::set('flight.views.path', 'application/views');
 Flight::set('root_dir', dirname(__FILE__));
-Flight::set('base_url', '/tracker-v2/');
+Flight::set('base_url', '/Division-Tracker/');
 
 // Set database credentials
 define('DB_HOST', 'localhost');
--- config.ex.php
+++ config.ex.php
@@ -1,7 +1,7 @@
 <?php
 
 date_default_timezone_set('America/New_York');
-require_once 'application/flight/Flight.php';
+require_once 'flight/Flight.php';
 
 // Autoload models and controllers
 Flight::path('models');
@@ -10,7 +10,7 @@ Flight::path('controllers');
 // Set views path and environment
 Flight::set('flight.views.path', 'views');
 Flight::set('root_dir', dirname(__FILE__));
-Flight::set('base_url', '/tracker-v2/');
+Flight::set('base_url', '/Division-Tracker/');
 
 // Set database credentials
 define('DB_HOST', 'localhost');
