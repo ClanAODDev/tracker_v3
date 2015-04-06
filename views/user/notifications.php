e9333c32361c9c5eb7e33af9f4bdf359fcc478ea
Chris Deaton <dcdeaton@ncsu.edu>
2015-04-06 12:05:07 -0400

    notifications adjustments

--- views/user/notifications.php
+++ views/user/notifications.php
@@ -3,7 +3,7 @@
 <?php endif; ?>
 <?php if (count($alerts)) : ?>
 	<?php foreach($alerts as $alert) : ?>
-		<div data-id="<?php echo $alert->id; ?>" data-user="<?php echo $user->id; ?>" class="alert-dismissable alert alert-<?php echo $alert->type; ?> fade in" role="alert">
+		<div data-id="<?php echo $alert->id; ?>" data-user="<?php echo $user->id; ?>" class="alert-dismissable alert alert-<?php echo $alert->type; ?> fade-in" role="alert">
 			<button type="button" class="close" data-dismiss="alert">
 				<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
 			</button>
