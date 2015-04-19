c92ac68d3a0f7730bdce8a184e924b68678054e7
flashadvocate <flashadvocate@gmail.com>
2015-04-19 10:23:25 -0400

    update 404 message
    differentiate between 404 and session expiration

--- views/errors/404.php
+++ views/errors/404.php
@@ -1,16 +1,18 @@
 <div class="page-404">	
-
 	<div class="error-code">404</div>
-
 	<div class="error-text">
 		<span class="oops">Aww, crap</span><br>
 		<span class="hr"></span>
 		<br>
-		THAT PAGE DOESN'T EXIST... OR YOU DON'T HAVE ACCESS TO IT
+		<?php if (!isset($_SESSION['userid'])) : ?>
+			<p>Your session has expired. Please log back in.</p>
+			<p class="margin-top-50"><a href="./" class="btn btn-2x btn-info"><i class="fa fa-user"></i> Log In</a></p>
+		<?php else : ?>
+			<p>That page doesn't exist... or you don't have access to it.</p>
+			<p class="margin-top-50"><a href="./" class="btn btn-2x btn-info"><i class="fa fa-home"></i> Take Me Home</a></p>
+		<?php endif; ?>
 	</div> <!-- / .error-text -->
 	<div class="margin-top-50"></div>
-	<a href="./" class="btn btn-2x btn-info"><i class="fa fa-home"></i> Take Me Home</a>
-
 </div>
 
 
