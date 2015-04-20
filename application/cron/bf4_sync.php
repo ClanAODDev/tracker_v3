5c46395cf5c4272ac0fcddccb673062071448de1
Chris Deaton <dcdeaton@ncsu.edu>
2015-04-19 21:19:29 -0400

    update crons to account for last user id

--- application/cron/bf4_sync.php
+++ application/cron/bf4_sync.php
@@ -9,6 +9,12 @@ if (dbConnect()) {
 	try {
 
 		$next_player = $pdo->query("SELECT value FROM crontab WHERE name = 'bf4_next_player'")->fetch(); 
+		$last_player = $pdo->query("SELECT max(id) as value FROM member")->fetch();
+
+		if ($next_player['value'] > $last_player['value']) {
+			$next_player['value'] = 1;
+		}
+
 		$params = $pdo->query("SELECT member_id, battlelog_id FROM member WHERE id = {$next_player['value']} AND status_id = 1")->fetch(); 
 
 		if (empty($params)) {
--- application/cron/bfh_sync.php
+++ application/cron/bfh_sync.php
@@ -8,7 +8,13 @@ if (dbConnect()) {
 
 	try {
 
-		$next_player = $pdo->query("SELECT value FROM crontab WHERE name = 'bfh_next_player'")->fetch(); 
+		$next_player = $pdo->query("SELECT value FROM crontab WHERE name = 'bfh_next_player'")->fetch();
+		$last_player = $pdo->query("SELECT max(id) as value FROM member")->fetch();
+
+		if ($next_player['value'] > $last_player['value']) {
+			$next_player['value'] = 1;
+		}
+
 		$params = $pdo->query("SELECT member_id, battlelog_id FROM member WHERE id = {$next_player['value']} AND status_id = 1")->fetch(); 
 
 		if (empty($params)) {
