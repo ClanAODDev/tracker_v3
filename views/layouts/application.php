412e94395af84c504c292f58d041fe4749e224ee
Chris Deaton <dcdeaton@ncsu.edu>
2015-04-21 19:15:08 -0400

    ie bug where ie ignores base href

--- views/layouts/application.php
+++ views/layouts/application.php
@@ -4,6 +4,17 @@
 	<title>AOD | Division Tracker v2</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<base href="<?php echo Flight::get('base_url') ?>">
+
+	<!--[if IE]>
+	<script type="text/javascript">
+    // Fix for IE ignoring relative base tags.
+    (function() {
+        var baseTag = document.getElementsByTagName('base')[0];
+        baseTag.href = baseTag.href;
+    })();
+	</script>
+	<![endif]-->
+	
 	<link rel="Shortcut Icon" href="favicon.ico" type="image/x-icon" />
 	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.2/lumen/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/jquery.powertip.min.css">
