8e7f352228140beeaea358f4aa99145ee2bc7ddc
Chris Deaton <dcdeaton@ncsu.edu>
2015-04-21 01:11:15 -0400

    update member edit, recruits section

--- views/member/recruits.php
+++ views/member/recruits.php
@@ -1,14 +1,19 @@
-<?php if (count($recruits)) : 
-	$recruits = arrayToObject($recruits); ?>
-	<?php foreach ($recruits as $player) : ?>
-		<a href="member/<?php echo $player->member_id ?>" class="list-group-item clearfix">
-			<span class="col-xs-5"><?php echo $player->abbr . " " . $player->forum_name ?></span>
-			<span class="col-xs-5 text-muted">Recruited <?php echo formatTime(strtotime($player->join_date)); ?></span>
-			<?php if ($player->status_id == 4) : ?>
-				<span class="col-xs-2 text-center text-danger"><i class="fa fa-times"></i> Removed</span>
-			<?php endif; ?>
-		</a>
-	<?php endforeach; ?>
+<?php if (count($recruits)) : $rctCount = count($recruits); ?>
+	<?php $recruits = arrayToObject($recruits); ?>
+	<div class='panel panel-primary'>
+		<div class='panel-heading'><strong><?php echo $member->forum_name ?>'s Recruiting History</strong> <span class="pull-right"><?php echo $rctCount ?> members</span></div>
+		<div class='list-group'>
+			<?php foreach ($recruits as $player) : ?>
+				<a href="member/<?php echo $player->member_id ?>" class="list-group-item clearfix">
+					<span class="col-xs-5"><?php echo $player->abbr . " " . $player->forum_name ?></span>
+					<span class="col-xs-5 text-muted">Recruited <?php echo formatTime(strtotime($player->join_date)); ?></span>
+					<?php if ($player->status_id == 4) : ?>
+						<span class="col-xs-2 text-center text-danger"><i class="fa fa-times"></i> Removed</span>
+					<?php endif; ?>
+				</a>
+			<?php endforeach; ?>
+		</div>
+	</div>
 <?php else : ?>
 	<span class="list-group-item">Either this player has no recruits, or there are no associations to this player on any existing member's record.</span>
 <?php endif; ?>
--- views/member/sl-personnel.php
+++ views/member/sl-personnel.php
@@ -2,7 +2,7 @@
 	<?php if (Squad::count($member->member_id)) : ?>
 		<div class='panel panel-primary'>
 			<div class='panel-heading'><strong><?php echo $member->forum_name ?>'s Squad</strong> <span class="pull-right"><?php echo Squad::count($member->member_id); ?> members</span></div>
-			<div class='list-group' id='squad'>
+			<div class='list-group'>
 				<?php foreach(Squad::find($member->member_id) as $player) : ?>
 					<a href='member/<?php echo $player->member_id ?>' class='list-group-item'><input type='checkbox' data-id='<?php echo $player->member_id; ?>' class='pm-checkbox'><span class='member-item'><?php echo $player->rank ?> <?php echo $player->forum_name ?></span><small class='pull-right text-<?php echo inactiveClass($player->last_activity); ?>'>Seen <?php echo formatTime(strtotime($player->last_activity)); ?></small></a>
 				<?php endforeach; ?>				
