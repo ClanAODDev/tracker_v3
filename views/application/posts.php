19f43d6d04216315ce861dd7108cc018998c090e
Chris Deaton <dcdeaton@ncsu.edu>
2015-05-04 10:50:32 -0400

    fix posts view

--- views/application/posts.php
+++ views/application/posts.php
@@ -1,10 +1,12 @@
 <?php if (count($posts)) : ?>
 	<?php foreach($posts as $post) : ?>
+		<?php $member_id = Member::findById($post->member_id)->member_id; ?>
+		<?php $member = Member::profileData($member_id); ?>
 		<div class='panel panel-default'>
-			<div class='panel-heading'><?php echo Member::avatar($post->forum_id) . " " .  $post->title; ?></div>
+			<div class='panel-heading'><?php echo Member::avatar($member->member_id) . " " .  $post->title; ?></div>
 			<div class='panel-body'><?php echo $post->content ?></div>
 			<div class='panel-footer text-muted text-right'>
-				<small>Posted <?php echo date("Y-m-d", strtotime($post->date)); ?> by <a href='member/<?php echo $post->forum_id ?>'><?php echo Member::findForumName($post->forum_id) ?></a></small>
+				<small>Posted <?php echo date("Y-m-d", strtotime($post->date)); ?> by <a href='member/<?php echo $member->member_id; ?>'><?php echo Member::findForumName($member->member_id) ?></a></small>
 			</div>
 		</div>
 	<?php endforeach; ?>
