fffbda672aef26595ace5ba65f5348e2b986ca6d
Chris Deaton <dcdeaton@ncsu.edu>
2015-05-27 23:24:22 -0400

    cleanup flagged members with <30 days activity

--- application/cron/cleanup.php
+++ application/cron/cleanup.php
@@ -13,6 +13,9 @@ if (dbConnect()) {
 		// removes flags for members who have already been processed out
 		$pdo->prepare("DELETE FROM inactive_flagged WHERE member_id IN (SELECT member_id FROM member WHERE status_id = 4)")->execute();
 
+		// cleans up flagged members who have posted since being flagged (for inactivty)
+		$pdo->prepare("DELETE FROM inactive_flagged WHERE inactive_flagged.member_id IN (SELECT member_id FROM member WHERE member.last_activity BETWEEN DATE_SUB(NOW(), INTERVAL 30 DAY) AND NOW())")->execute();
+
 		// cleans up activity older than 90 days
 		$pdo->prepare("DELETE FROM activity WHERE datetime < (NOW() - INTERVAL 90 DAY)")->execute();
 
