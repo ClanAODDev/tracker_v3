b7411181b4976cd76a27780f54ee59aeb9e9d654
Chris Deaton <dcdeaton@ncsu.edu>
2015-06-02 21:42:40 -0400

    fix individual issue route view

--- views/issues/issues.php
+++ views/issues/issues.php
@@ -3,13 +3,17 @@
 
 	<?php if (count($issues)): ?>
 		<div class="list-group">
-		<?php foreach($issues as $issue): ?>
-				<a class="list-group-item" href="./issues/<?php echo $issue->getNumber(); ?>">
-					<span class="text-muted">#<?php echo ucwords($issue->getNumber()); ?> </span>
-					<strong><?php echo ucwords($issue->getTitle()); ?></strong>
+			<?php foreach($issues as $issue): ?>
+				<a class="list-group-item" href="./issues/view/<?php echo $issue->getNumber(); ?>">
+					
+					<h4 class="list-group-item-heading"><?php echo ucwords($issue->getTitle()); ?> <span class="text-muted">#<?php echo ucwords($issue->getNumber()); ?> </span></h4>
+
+					<p class="list-group-item-text text-muted"><?php echo excerpt($issue->getBody(), 20); ?></p>
+
 					<?php if ($issue->getComments()): ?>
 						<span class="badge"><i class="fa fa-comment"></i> <?php echo $issue->getComments() ?></span>
 					<?php endif; ?>
+
 				</a>
 			<?php endforeach; ?>
 		</div>
