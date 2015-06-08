e46c11822d37d6eb4802d69646762cd7a37a189e
Nick <fresh.produce@charter.net>
2015-06-08 07:23:41 -0500

    Fixing Github Sync

--- models/MemberHandle.php
+++ models/MemberHandle.php
@@ -5,16 +5,21 @@ class MemberHandle extends Application {
 	public $id;
 	public $member_id;
 	public $handle_type;
+	public $handle_value;
 
 	static $id_field = 'id';
 	static $table_name = 'member_handles';
 
+	public static function find_all($member_id) {
+		return self::find(array('member_id' => $member_id))->many();
+	}
+
 	public static function create($params) {
 		$handle = new self();
-		foreach ($params as $key=>$value) {
-			$handle->$key = $value;
-		}
-		$handle->save($params);
+		$handle->member_id = $params->$member_id;
+		$handle->handle_type = $params->$handle_type;
+		$handle->handle_value = $params->$handle_value;
+		$handle->save();
 	}
 
 	public static function modify($params) {
