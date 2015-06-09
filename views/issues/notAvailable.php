ce405a43042fa0738d82ca441086f9c34a20e88f
Nick <fresh.produce@charter.net>
2015-06-09 08:25:43 -0500

    Issue is not available

---
+++ views/issues/notAvailable.php
@@ -0,0 +1,13 @@
+<div class='container'>
+
+	<ul class='breadcrumb'>
+		<li><a href='./'>Home</a></li>
+		<li><a href='./issues'>Issue Tracker</a></li>
+		<li class='active'>Issue #<?php echo $issue->getNumber() ?></li>
+	</ul>
+
+	<div class='page-header'>
+		<h2><strong>Issue Number <?php echo $issue->getTitle(); ?> Is Not Available To You</strong></h2>
+	</div>
+
+</div>
