5a148b5239c0e2c786aca60aad83274e4b06fc5f
Chris Deaton <dcdeaton@ncsu.edu>
2015-07-17 23:01:32 -0400

    fix visualization of manage platoons

--- views/manage/platoon.php
+++ views/manage/platoon.php
@@ -17,62 +17,72 @@
 
 	<div class="row mod-plt">
 
-		<?php $i=1; foreach ($squads as $squad): ?>
+		<div class="col-xs-8">
 
-		<?php $leader = ($squad->leader_id != 0) ? arrayToObject(Member::findById($squad->leader_id)) : NULL; ?>
-		<?php $leader_name = (!is_null($leader)) ? Rank::convert($leader->rank_id)->abbr . " " . $leader->forum_name : "TBA"; ?>
-		<?php $members = Squad::members($squad->id); ?>
+			<?php $i=1; foreach ($squads as $squad): ?>
 
-		<?php $squad_width = (count((array) $unassignedMembers)) ? 4 : 6; ?>
+			<?php $leader = ($squad->leader_id != 0) ? arrayToObject(Member::findById($squad->leader_id)) : NULL; ?>
+			<?php $leader_name = (!is_null($leader)) ? Rank::convert($leader->rank_id)->abbr . " " . $leader->forum_name : "TBA"; ?>
+			<?php $members = Squad::members($squad->id); ?>
 
-		<div class="col-xs-<?php echo $squad_width ?>">
-			<h3 class="page-header squad-header"><strong><?php echo ordsuffix($i); ?> <?php echo Locality::run('Squad', $division->id); ?></strong> <small><?php echo $leader_name ?></small> <a href="#" class="btn btn-sm btn-default modify-squad"><i class="fa fa-wrench"></i> Edit</a><span class="badge pull-right"><?php echo count((array)$members); ?></span></h3>
+			<div class="col-xs-6">
+				<h3 class="page-header squad-header"><strong><?php echo ordsuffix($i); ?> <?php echo Locality::run('Squad', $division->id); ?></strong> <small><?php echo $leader_name ?></small> <a href="#" class="btn btn-sm btn-default modify-squad"><i class="fa fa-wrench"></i> Edit</a><span class="badge pull-right"><?php echo count((array)$members); ?></span></h3>
 
-			<ul class="list-group sortable" data-squad-id="<?php echo $squad->id ?>" data-platoon-id="<?php echo $platoon->id ?>" data-division-id="<?php echo $division->id ?>">
-				<?php foreach ($members as $member): ?>
-					<?php $rctFlag = (!is_null($leader) && $member->recruiter == $leader->member_id) ? "<sup><i class='fa fa-asterisk text-success'></i></sup>" : NULL; ?>
-					<li class="list-group-item" data-member-id="<?php echo $member->id ?>">
-						<img src="assets/images/grab.svg" class="pull-right" style="width: 8px; opacity: .20;">
-						<?php echo Rank::convert($member->rank_id)->abbr . " " . ucwords($member->forum_name) . $rctFlag; ?>
-					</li>
-				<?php endforeach; ?>
-			</ul>
+				<ul class="list-group sortable" data-squad-id="<?php echo $squad->id ?>" data-platoon-id="<?php echo $platoon->id ?>" data-division-id="<?php echo $division->id ?>">
+					<?php foreach ($members as $member): ?>
+						<?php $rctFlag = (!is_null($leader) && $member->recruiter == $leader->member_id) ? "<sup><i class='fa fa-asterisk text-success'></i></sup>" : NULL; ?>
+						<li class="list-group-item" data-member-id="<?php echo $member->id ?>">
+							<img src="assets/images/grab.svg" class="pull-right" style="width: 8px; opacity: .20;">
+							<?php echo Rank::convert($member->rank_id)->abbr . " " . ucwords($member->forum_name) . $rctFlag; ?>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+
+			</div>
+
+			<?php if ($i % 2 ==0): ?>
+				<div style="clear: both; height: 60px;"></div>
+			<?php endif; ?>
+
+			<?php $i++; endforeach;  ?>
+
+
+			<?php if ($i < (MAX_SQUADS_IN_PLT + 1)): ?>
+				<div class="col-xs-8">
+					<h3 class="page-header text-muted"><strong>New <?php echo Locality::run('Squad', $division->id); ?></strong></h3>
+					<p>Would you like to add a new <?php echo Locality::run('Squad', $division->id); ?>?</p><p><a data-platoon-id="<?php echo $platoon->id ?>" data-division-id="<?php echo $division->id ?>" class="btn btn-success center-block create-squad" href="create/squad"><i class="fa fa-plus"></i> Add</a></p>
+
+				</div>
+			<?php endif; ?>
 
 		</div>
 
-		<?php $i++; endforeach;  ?>
+		<div class="col-xs-4">
 
+			<?php $unassigned = count((array) $unassignedMembers); ?>
 
-		<?php if ($i < (MAX_SQUADS_IN_PLT + 1)): ?>
-			<div class="col-xs-4">
-				<h3 class="page-header text-muted"><strong>New <?php echo Locality::run('Squad', $division->id); ?></strong></h3>
-				<p>Would you like to add a new <?php echo Locality::run('Squad', $division->id); ?>?</p><p><a data-platoon-id="<?php echo $platoon->id ?>" data-division-id="<?php echo $division->id ?>" class="btn btn-success center-block create-squad" href="create/squad"><i class="fa fa-plus"></i> Add</a></p>
+			<?php if ($unassigned): ?>
+				<div class="col-xs-12 genpop pull-right">
+					<h3 class="page-header text-muted"><strong>Unassigned</strong><span class="badge pull-right"><?php echo $unassigned; ?></span></h3>
 
-			</div>
-		<?php endif; ?>
+					<ul class="list-group sortable">
+						<?php foreach ($unassignedMembers as $member): ?>
+							<li class="list-group-item" data-member-id="<?php echo $member->id ?>">
+								<img src="assets/images/grab.svg" class="pull-right" style="width: 8px; opacity: .20;">
+								<?php echo Rank::convert($member->rank_id)->abbr . " " . ucwords($member->forum_name); ?>
+							</li> 
+						<?php endforeach; ?>
+					</ul>
+				</div>
+			<?php endif; ?>
 
-		<?php $unassigned = count((array) $unassignedMembers); ?>
-
-		<?php if ($unassigned): ?>
-			<div class="col-xs-4 genpop">
-				<h3 class="page-header text-muted"><strong>Unassigned</strong><span class="badge pull-right"><?php echo $unassigned; ?></span></h3>
-
-				<ul class="list-group sortable">
-					<?php foreach ($unassignedMembers as $member): ?>
-						<li class="list-group-item" data-member-id="<?php echo $member->id ?>">
-							<img src="assets/images/grab.svg" class="pull-right" style="width: 8px; opacity: .20;">
-							<?php echo Rank::convert($member->rank_id)->abbr . " " . ucwords($member->forum_name); ?>
-						</li> 
-					<?php endforeach; ?>
-				</ul>
-			</div>
-		<?php endif; ?>
+		</div>
 
 	</div>
 
 
 	<hr>
 	<small class="text-muted margin-top-10"><i class='fa fa-asterisk text-success'></i> - Direct recruit</small>
-	
+
 
 </div>
