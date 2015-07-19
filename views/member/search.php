000eda30fd34f9c093118601f7e9508afb3fc898
Chris Deaton <dcdeaton@ncsu.edu>
2015-07-18 22:22:21 -0400

    update search function

--- views/member/search.php
+++ views/member/search.php
@@ -3,7 +3,6 @@
 <?php else : ?>
 	<?php $results = arrayToObject($results); ?>
 	<?php foreach($results as $member) : ?>
-		<?php $battlelog_name = ($member->battlelog_name != "0") ? $member->battlelog_name : "No BL name set"; ?>
-		<a href='member/<?php echo $member->member_id ?>' class='list-group-item'><strong><?php echo ucwords($member->abbr) ?> <?php echo ucwords($member->forum_name) ?></strong><br /><span class="text-muted"><?php echo ucwords($battlelog_name) ?></span></a>	
+		<a href='member/<?php echo $member->member_id ?>' class='list-group-item'><strong><?php echo ucwords($member->abbr) ?> <?php echo ucwords($member->forum_name) ?></strong></a>
 	<?php endforeach; ?>
 <?php endif; ?>
