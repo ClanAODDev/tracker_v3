0c38b2740c5b121353ff4e913e46f549b21de8f0
Chris Deaton <dcdeaton@ncsu.edu>
2015-07-22 12:07:44 -0400

    update cron
    more effective queuing

--- application/cron/bf4_sync.php
+++ application/cron/bf4_sync.php
@@ -9,15 +9,15 @@ if (dbConnect()) {
 	try {
 
 		$next_player = $pdo->query("SELECT value FROM crontab WHERE name = 'bf4_next_player'")->fetch(); 
-		$last_player = $pdo->query("SELECT max(id) as value FROM member")->fetch();
+		$limit = $pdo->query("SELECT max(id) as max, min(id) as min FROM member WHERE game_id = 2 AND status_id = 1")->fetch();
 
-		if ($next_player['value'] > $last_player['value']) {
-			$next_player = 1;
+		if ($next_player['value'] > $limit['max']) {
+			$next_player = $limit['min'];
 		} else {
 			$next_player = $next_player['value'];
 		}
 
-		$params = $pdo->query("SELECT member_id, battlelog_id FROM member WHERE id = {$next_player} AND status_id = 1")->fetch(); 
+		$params = $pdo->query("SELECT member_id, battlelog_id FROM member WHERE id = {$next_player} AND status_id = 1 AND game_id = 2")->fetch(); 
 
 		if (empty($params)) {
 
