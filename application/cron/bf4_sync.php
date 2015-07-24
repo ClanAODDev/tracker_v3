8932bd63a92526fdb78520c1daf3efa3f9c39e4f
Chris Deaton <dcdeaton@ncsu.edu>
2015-07-23 20:36:39 -0400

    fix bf4, bfh crons

--- application/cron/bf4_sync.php
+++ application/cron/bf4_sync.php
@@ -12,7 +12,7 @@ if (dbConnect()) {
 		$next_player = $pdo->query("SELECT value FROM crontab WHERE name = 'bf4_next_player'")->fetch(); 
 
 		// determine player range
-		$limit = $pdo->query("SELECT max(id) as max, min(id) as min FROM member WHERE game_id = 2 AND status_id = 1")->fetch();
+		$limit = $pdo->query("SELECT max(id) as max, min(id) as min FROM member WHERE game_id = 3 AND status_id = 1")->fetch();
 
 		if ($next_player['value'] > $limit['max']) {
 			$next_player = $limit['min'];
@@ -21,7 +21,7 @@ if (dbConnect()) {
 		}
 
 		// fetch battlelog persona id
-		$params = $pdo->query("SELECT m.member_id, h.battlelog_id FROM member m INNER JOIN member_handles h ON m.member_id = m.id WHERE m.id = {$next_player} AND m.status_id = 1 AND m.game_id = 2")->fetch(); 
+		$params = $pdo->query("SELECT m.member_id, h.handle_account_id FROM member m INNER JOIN member_handles h ON h.member_id = m.id WHERE m.id = {$next_player} AND m.status_id = 1 AND m.game_id = 2 AND h.handle_type = 2")->fetch(); 
 
 		if (empty($params)) {
 
@@ -31,7 +31,7 @@ if (dbConnect()) {
 		} else {
 
 			// fetch battlelog data
-			$reports = parse_battlelog_reports($params['battlelog_id'], 'bf4');
+			$reports = parse_battlelog_reports($params['handle_account_id'], 'bf4');
 			newActivity($reports, "bf4", $params['member_id'], $next_player);
 			$pdo->prepare("UPDATE crontab SET value = {$next_player}+1 WHERE name = 'bf4_next_player'")->execute(); 
 
