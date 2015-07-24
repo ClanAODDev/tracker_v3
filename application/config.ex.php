49e3f5f2f3810a79fc26d8bec5a7fb594ff2e860
Chris Deaton <dcdeaton@ncsu.edu>
2015-07-24 12:29:39 -0400

    update config with api keys

--- application/config.ex.php
+++ application/config.ex.php
@@ -14,15 +14,19 @@ Flight::set('root_dir', dirname(__FILE__));
 Flight::set('base_url', '/Division-Tracker/');
 
 // Set database credentials
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
+define('DB_HOST', '');
+define('DB_USER', '');
 define('DB_PASS', '');
+
+define('ARCH_PASS', '');
 
 Flight::register('aod', 'Database', array('aod'));
 
 // defines for website URLs
 define('CLANAOD', 'http://www.clanaod.net/forums/member.php?u=');
-define('BATTLELOG', 'http://battlelog.battlefield.com/bfh/user/');
+define('BATTLELOG', 'http://battlelog.battlefield.com/bf4/user/');
+define('BATTLEREPORT', 'http://battlelog.battlefield.com/bf4/battlereport/show/1/');
+define('BF4DB', 'http://bf4db.com/players/');
 define('PRIVMSG', 'http://www.clanaod.net/forums/private.php?do=newpm&u=');
 define('EMAIL', 'http://www.clanaod.net/forums/sendmessage.php?do=mailmember&u=');
 define('REMOVE', 'http://www.clanaod.net/forums/modcp/aodmember.php?do=remaod&u=');
@@ -35,8 +39,14 @@ define('INACTIVE_MAX', 25);
 
 // global settings
 define('MAX_GAMES_ON_PROFILE', 25);
-define('MAX_SQUADS_IN_PLT', 2);
+define('MAX_SQUADS_IN_PLT', 10);
 
 // gitHub username and password
+// need this for github API integration
 define('GITHUB_USER', '');
 define('GITHUB_PASS', '');
+
+// wargaming API key
+// 10 requests per second
+define('TANKS_NA_KEY', '');
+define('TANKS_EU_KEY', '');
