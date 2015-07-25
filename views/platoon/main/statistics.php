2f3ac0ad71b380c3b1ef4dabc9912bbb561a4f8b
Chris Deaton <dcdeaton@ncsu.edu>
2015-07-25 18:05:59 -0400

    hide forum activity if 0 members

--- views/platoon/main/statistics.php
+++ views/platoon/main/statistics.php
@@ -1,14 +1,16 @@
+<?php $pltCount = Platoon::countPlatoon($platoon->id); ?>
 <div class='panel panel-primary'>
 	<div class='panel-heading'>Total Members</div>
-	<div class='panel-body count-detail-big striped-bg'><span class='count-animated'><?php echo Platoon::countPlatoon($platoon->id); ?></span>
+	<div class='panel-body count-detail-big striped-bg'><span class='count-animated'><?php echo $pltCount; ?></span>
 	</div>
 </div>
-
-<div class='panel panel-primary'>
-	<div class='panel-heading'>Forum Activity</div>
-	<div class='panel-body striped-bg'>
-		<div id="canvas-holder" data-stats="<?php echo htmlentities($activity, ENT_QUOTES, 'UTF-8'); ?>">
-			<canvas id="chart-area" style="filter: drop-shadow(0px 0px 10px rgba(0,0,0,.5));"/>
+<?php if (count($pltCount)): ?>
+	<div class='panel panel-primary'>
+		<div class='panel-heading'>Forum Activity</div>
+		<div class='panel-body striped-bg'>
+			<div id="canvas-holder" data-stats="<?php echo htmlentities($activity, ENT_QUOTES, 'UTF-8'); ?>">
+				<canvas id="chart-area" style="filter: drop-shadow(0px 0px 10px rgba(0,0,0,.5));"/>
+			</div>
 		</div>
 	</div>
-</div>
+<?php endif; ?>
