8ae73728ea539b7f48d127e1c221d17d7f3098f8
Chris Deaton <dcdeaton@ncsu.edu>
2015-07-29 22:45:13 -0400

    update

--- models/Email.php
+++ models/Email.php
@@ -23,11 +23,11 @@ class Email {
 
     $email->to = $user->email;
     $email->subject = "AOD Division Tracker - Email verification";
-    $email->message = "PLEASE DO NOT REPLY TO THIS E-MAIL\r\n\n";
     $email->message .= "<h1><strong>{$user->username}</strong>,</h1>";
     $email->message .= "<p>This email was used by someone with the IP {$_SERVER['REMOTE_ADDR']} to create an account on the AOD Division Tracker. Please verify that it was you by clicking the link provided below, or copy-paste the URL into your browser's address bar.</p>";
     $email->message .= "<p>http://aodwebhost.site.nfoservers.com/tracker/authenticate?id={$user->validation}\r\n\r\n</p>";
-    $email->message .= "<small>If you believe you have received this email in error, or the account was not created by you, please let us know by sending an email to admin@aodwebhost.site.nfoservers.com</small>";
+    $email->message .= "<p><small>If you believe you have received this email in error, or the account was not created by you, please let us know by sending an email to admin@aodwebhost.site.nfoservers.com</small></p>";
+        $email->message = "<p><small>PLEASE DO NOT REPLY TO THIS E-MAIL</small></p>";
     $email->send();
   }
 
