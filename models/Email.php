ed3044be667ccdba961a8aa23a4c3cab4e57b196
Chris Deaton <dcdeaton@ncsu.edu>
2015-07-29 22:46:02 -0400

    Typo

--- models/Email.php
+++ models/Email.php
@@ -27,7 +27,7 @@ class Email {
     $email->message .= "<p>This email was used by someone with the IP {$_SERVER['REMOTE_ADDR']} to create an account on the AOD Division Tracker. Please verify that it was you by clicking the link provided below, or copy-paste the URL into your browser's address bar.</p>";
     $email->message .= "<p>http://aodwebhost.site.nfoservers.com/tracker/authenticate?id={$user->validation}\r\n\r\n</p>";
     $email->message .= "<p><small>If you believe you have received this email in error, or the account was not created by you, please let us know by sending an email to admin@aodwebhost.site.nfoservers.com</small></p>";
-        $email->message = "<p><small>PLEASE DO NOT REPLY TO THIS E-MAIL</small></p>";
+        $email->message .= "<p><small>PLEASE DO NOT REPLY TO THIS E-MAIL</small></p>";
     $email->send();
   }
 
