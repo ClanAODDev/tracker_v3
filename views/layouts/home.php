03807403be0c9e5977248f7790672e6bead73a6b
flashadvocate <flashadvocate@gmail.com>
2015-11-22 22:56:31 -0500

    visual updates to activity

--- views/layouts/home.php
+++ views/layouts/home.php
@@ -47,20 +47,20 @@
 			<div class='col-md-5'>
 				<div class="panel panel-info">
 					<div class="panel-heading"><strong>Recent Activity</strong></div>
-
+					<ul class="activity-list">
 						<?php foreach(UserAction::find_all($division->id,15) as $action) : ?>
 							<?php if ( ! is_null ( $action->target_id ) ): ?>
-								<li class="list-group-item">
+								<li>
+									<i class="<?php echo $action->icon; ?> fa-2x"></i>
 									<div>
 										<?php echo UserAction::humanize($action->type_id, $action->target_id, $action->user_id, $action->verbage); ?>
 										<span><?php echo formatTime(strtotime($action->date)); ?></span>
 									</div>
-									<i class="<?php echo $action->icon; ?> fa-2x"></i>
 								</li>
 							<?php endif; ?>
 						<?php endforeach; ?>
 						<li><div class="pull-right"><a href="activity/">View more</a></div></li>
-
+					</ul>
 				</div>
 			</div>
 		</div>
