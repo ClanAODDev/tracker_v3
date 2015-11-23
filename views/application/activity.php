e1f81e3bd7cc251c2c1f0399cbeed188bc177136
flashadvocate <flashadvocate@gmail.com>
2015-11-22 22:49:32 -0500

    fix debug, filter

--- views/application/activity.php
+++ views/application/activity.php
@@ -1,19 +1,20 @@
-<div class="panel panel-info">
-	<div class="panel-heading"><strong>Recent Activity</strong></div>
-	<ul class="activity-list">
-	<?php $actions = UserAction::find_all($division->id, 15); ?>
-	<?php var_dump(Flight::aod()->last_query); ?>
-		<?php foreach($actions as $action) : ?>
-			<?php if ( ! is_null ( $action->target_id ) ): ?>
-				<li>
-					<i class="<?php echo $action->icon; ?> fa-2x"></i>
-					<div>
-						<?php echo UserAction::humanize($action->type_id, $action->target_id, $action->user_id, $action->verbage); ?>
-						<span><?php echo formatTime(strtotime($action->date)); ?></span>
-					</div>
-				</li>
-			<?php endif; ?>
-		<?php endforeach; ?>
+<div class='container margin-top-20'>
+	<div class="panel panel-info">
+		<div class="panel-heading"><strong>Recent Activity</strong></div>
+		<ul class="activity-list">
+			<?php $actions = UserAction::find_all($division->id, 30); ?>
+			<?php foreach($actions as $action) : ?>
+				<?php if ( ! is_null ( $action->target_id ) ): ?>
+					<li>
+						<i class="<?php echo $action->icon; ?> fa-2x"></i>
+						<div>
+							<?php echo UserAction::humanize($action->type_id, $action->target_id, $action->user_id, $action->verbage); ?>
+							<span><?php echo formatTime(strtotime($action->date)); ?></span>
+						</div>
+					</li>
+				<?php endif; ?>
+			<?php endforeach; ?>
 
-	</ul>
+		</ul>
+	</div>
 </div>
