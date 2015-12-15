83c6c40ef7f40333f2944d80be335d611878af01
Chris Deaton <dcdeaton@ncsu.edu>
2015-12-14 19:33:09 -0500

    track invalid ids and skip

--- application/cron/battlelog_sync.php
+++ application/cron/battlelog_sync.php
@@ -5,7 +5,7 @@ require 'lib.php';
 $members = array();
 
 if (dbConnect()) {
-	$query = $pdo->prepare(" SELECT handle_value FROM member_handles JOIN member ON member.id = member_handles.member_id WHERE handle_account_id = 0 AND handle_type = 2 AND status_id = 1");
+	$query = $pdo->prepare(" SELECT handle_value FROM member_handles JOIN member ON member.id = member_handles.member_id WHERE handle_account_id = 0 AND handle_type = 2 AND status_id = 1 AND invalid = 0");
 	try {
 		$query->execute();
 		$battlelog_names = $query->fetchAll();
@@ -17,11 +17,12 @@ if (dbConnect()) {
 
 			$battlelog_id = getBattlelogId($row['handle_value']);
 			$query = $pdo->prepare("UPDATE member_handles SET handle_account_id = :battlelog_id WHERE handle_value = :battlelog_name");
+			$invalid = $pdo->prepare("UPDATE member_handles SET invalid = 1 WHERE handle_value = :battlelog_name");
 
 			if (!$battlelog_id['error']) {
 				try {
-					$query->bindParam(':battlelog_id', $battlelog_id['id']);
 					$query->bindParam(':battlelog_name', $row['handle_value']);
+					$query->bindParam(':battlelog_id', $battlelog_id['id']);
 					$query->execute();
 					echo "Added ID {$battlelog_id['id']} to {$row['handle_value']}\r\n";
 
@@ -30,6 +31,8 @@ if (dbConnect()) {
 					echo "ERROR: " . $e->getMessage();
 				}
 			} else {
+				$invalid->bindParam(':battlelog_name', $row['handle_value']);
+				$invalid->execute();
 				echo "ERROR: {$row['handle_value']} - {$battlelog_id['message']}\r\n";
 			}
 		}
