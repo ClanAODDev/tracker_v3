b56164475530ca2d6c69423f05554e9594bfc3ac
flashadvocate <flashadvocate@gmail.com>
2016-01-22 18:50:07 -0500

    add slack integration

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
+        Maknz\Slack\SlackServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -203,6 +204,7 @@ return [
         'Route'       => Illuminate\Support\Facades\Route::class,
         'Schema'      => Illuminate\Support\Facades\Schema::class,
         'Session'     => Illuminate\Support\Facades\Session::class,
+        'Slack'       => Maknz\Slack\Facades\Slack::class,
         'Storage'     => Illuminate\Support\Facades\Storage::class,
         'URL'         => Illuminate\Support\Facades\URL::class,
         'Validator'   => Illuminate\Support\Facades\Validator::class,
