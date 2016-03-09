f7e8e6d08fc3760f1f9b7194e4b67b1fff875b8c
Chris Deaton <dcdeaton@ncsu.edu>
2016-03-09 09:20:14 -0500

    convert seeders to factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,5 +17,35 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+
+    ];
+});
+
+$factory->define(App\Member::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->userName,
+        'clan_id' => $faker->randomNumber(6),
+        'rank_id' => $faker->numberBetween(1, 14),
+        'platoon_id' => $faker->numberBetween(1, 3),
+        'position_id' => $faker->numberBetween(1, 11),
+        'squad_id' => $faker->numberBetween(1, 3),
+        'join_date' => $faker->dateTime('now'),
+        'last_forum_login' => $faker->dateTime('now'),
+    ];
+});
+
+$factory->define(App\Platoon::class, function (Faker\Generator $faker) {
+    return [
+        'order' => $faker->numberBetween(1, 3),
+        'name' => $faker->name,
+        'division_id' => 1,
+        'platoon_leader_id' => $faker->randomNumber(5)
+    ];
+});
+
+$factory->define(App\Squad::class, function (Faker\Generator $faker) {
+    return [
+        'platoon_id' => $faker->numberBetween(1, 3),
+        'squad_leader_id' => $faker->randomNumber(5)
     ];
 });
