a4d91a9d952cc9bd013d9bad311720ba51e821c0
Chris Deaton <dcdeaton@ncsu.edu>
2016-03-11 09:45:03 -0500

    consolidate errors in register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,17 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
+
+                @if (count($errors))
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                            <li>
+                                {{ $error }}
+                            </li>
+                        @endforeach
+                    </div>
+                @endif
+
                 <div class="panel panel-primary">
                     <div class="panel-heading">Register</div>
                     <div class="panel-body">
@@ -12,50 +23,22 @@
                             {!! csrf_field() !!}
 
                             <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-
                                 <input type="text" class="form-control" name="name" value="{{ old('name') }}"
                                        placeholder="Username">
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <i class="fa fa-exclamation-circle"></i>
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-
                             </div>
 
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-
                                 <input type="email" class="form-control" name="email" value="{{ old('email') }}"
                                        placeholder="Email Address">
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <i class="fa fa-exclamation-circle"></i>
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
                             </div>
 
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                 <input type="password" class="form-control" name="password" placeholder="Password">
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <i class="fa fa-exclamation-circle"></i>
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                     </span>
-                                @endif
                             </div>
 
                             <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
                                 <input type="password" class="form-control" name="password_confirmation"
                                        placeholder="Confirm Password">
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <i class="fa fa-exclamation-circle"></i>
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
                             </div>
 
                             <div class="form-group">
