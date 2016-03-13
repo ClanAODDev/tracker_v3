957d14dd40a0fff0efe8398e10e6309ae75893f0
Chris Deaton <dcdeaton@ncsu.edu>
2016-03-13 15:59:54 -0400

    extract partials

---
+++ resources/views/division/partials/division_leadership.blade.php
@@ -0,0 +1,17 @@
+<div class="panel panel-info">
+    <div class="panel-heading">Division Command Staff</div>
+    @if (count($division->leaders))
+
+        @foreach ($division->leaders as $leader)
+            <a href="members/{{ $leader->clan_id }}" class="list-group-item">
+                <h5 class="pull-right"><i class="fa fa-shield fa-2x text-muted"></i></h5>
+                <h4 class="list-group-item-heading">
+                    <strong>{{ $leader->rank->abbreviation }} {{ $leader->name }}</strong></h4>
+                <p class="list-group-item-text text-muted"><?php echo $leader->position->name; ?></p>
+            </a>
+        @endforeach
+
+    @else
+        <li class="list-group-item text-muted">This division has no assigned leadership</li>
+    @endif
+</div>
--- resources/views/division/show.blade.php
+++ resources/views/division/show.blade.php
@@ -20,45 +20,13 @@
     </div>
 
     <div class="row">
+
         <div class="col-md-8">
-            <div class="panel panel-primary">
-
-                {{-- locality --}}
-                <div class="panel-heading">Platoons</div>
-                <div class="list-group">
-                    @if (count($division->platoons))
-                        @foreach ($division->platoons as $platoon)
-                            <a href="{{ action('PlatoonController@show', [$platoon->id]) }}"
-                               class=" list-group-item">
-                                <h5 class="pull-right text-muted big-num count-animated">{{ $platoon->members->count() }}</h5>
-                                <h4 class="list-group-item-heading"><strong>{{ $platoon->name }}</strong></h4>
-                                <p class="list-group-item-text text-muted">Leader</p>
-                            </a>
-                        @endforeach
-                    @else
-                        <li class="list-group-item text-muted">No platoons currently exist for this division.</li>
-                    @endif
-
-                </div>
-            </div>
+            @include('division.partials.all_platoons')
         </div>
 
         <div class="col-md-4">
-            <div class="panel panel-info">
-                <div class="panel-heading">Division Command Staff</div>
-                @if (count($division->leaders))
-                    @foreach ($division->leaders as $leader)
-                        <a href="members/{{ $leader->clan_id }}" class="list-group-item">
-                            <h5 class="pull-right"><i class="fa fa-shield fa-2x text-muted"></i></h5>
-                            <h4 class="list-group-item-heading">
-                                <strong>{{ $leader->rank->abbreviation }} {{ $leader->name }}</strong></h4>
-                            <p class="list-group-item-text text-muted"><?php echo $leader->position->name; ?></p>
-                        </a>
-                    @endforeach
-                @else
-                    <li class="list-group-item text-muted">This division has no assigned leadership</li>
-                @endif
-            </div>
+            @include('division.partials.division_leadership')
         </div>
     </div>
 
