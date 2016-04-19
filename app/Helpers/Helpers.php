4da9e0fd8153cea40365d0464a871c7482e91a08
Chris Deaton <dcdeaton@ncsu.edu>
2016-04-19 18:55:57 -0400

    add AOD forum helpers, update member profile view

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -9,17 +9,68 @@ namespace App\Helpers;
  */
 class Helpers
 {
+
+    /**
+     * Returns proper URL for sending an AOD forum PM
+     *
+     * @param $clan_id
+     * @return string
+     */
+    public static function getAODPmUrl($clan_id)
+    {
+        $params = [
+            'do' => 'newpm',
+            'u' => $clan_id,
+        ];
+
+        return "http://www.clanaod.net/forums/private.php?" . http_build_query($params);
+    }
+
+    /**
+     * Returns proper URL for sending an AOD forum email
+     *
+     * @param $clan_id
+     * @return string
+     */
+    public static function getAODEmailUrl($clan_id)
+    {
+        $params = [
+            'do' => 'mailmember',
+            'u' => $clan_id,
+        ];
+
+        return "http://www.clanaod.net/forums/sendmessage.php?" . http_build_query($params);
+    }
+
+    /**
+     * Return gravatar image
+     *
+     * @param $email
+     * @param string $type
+     * @return string
+     */
     public static function avatar($email, $type = "thumb")
     {
         $forum_img = self::GetGravatarUrl($email);
         $unknown = "assets/images/blank_avatar.jpg";
+
         return "<img src='{$forum_img}' class='img-thumbnail' />";
     }
 
+    /**
+     * Generate a gravatar URL
+     *
+     * @param $email
+     * @param int $size
+     * @param string $type
+     * @param string $rating
+     * @return mixed
+     */
     private static function GetGravatarUrl($email, $size = 80, $type = 'retro', $rating = 'pg')
     {
         $gravatar = sprintf('http://www.gravatar.com/avatar/%s?d=%s&s=%d&r=%s',
             md5($email), $type, $size, $rating);
+
         return $gravatar;
     }
 }
--- resources/views/member/show.blade.php
+++ resources/views/member/show.blade.php
@@ -14,11 +14,11 @@
                 <div class="btn-group">
 
                     <a class="btn btn-default btn-xs popup-link"
-                       href="http://www.clanaod.net/forums/private.php?do=newpm&amp;u={{ $member->clan_id }}&amp;url=http://www.clanaod.net/forums/member.php?u={{ $member->clan_id }}"
+                       href="{{ Helpers::getAODPmUrl($member->clan_id) }}"
                        target="_blank"><i class="fa fa-comment"></i> Send PM</a>
 
                     <a class="btn btn-default btn-xs popup-link"
-                       href="http://www.clanaod.net/forums/sendmessage.php?do=mailmember&amp;u={{ $member->clan_id }}&amp;url=http://www.clanaod.net/forums/member.php?u={{ $member->clan_id }}"
+                       href="{{ Helpers::getAODEmailUrl($member->clan_id) }}"
                        target="_blank"><i class="fa fa-envelope"></i> Send Email</a>
 
                 </div>
@@ -27,20 +27,32 @@
 
         <div class="col-xs-6">
 
-            {{--Implement authorization here--}}
-            <div class="btn-group pull-right" data-player-id="31832" data-user-id="31832">
-                <button type="button" class="btn btn-info edit-member"><i class="fa fa-pencil fa-lg"></i> Edit</button>
-                <a href="#"
-                   title="Remove player from AOD"
-                   class="removeMember btn btn-danger"><i class="fa fa-trash fa-lg"></i> Remove<span
-                            class="hidden-sm hidden-xs"> from AOD</span></a>
+            <div class="btn-group pull-right" data-player-id="{{ $member->clan_id }}"
+                 data-user-id="{{ $member->clan_id }}">
+                <button type="button" class="btn btn-info edit-member"><i class="fa fa-pencil fa-lg"></i> Edit
+                </button>
+
+                {{-- if user sgt or higher, show remove button --}}
+                @if ($member->rank_id >= 9)
+                    <a href="#" title="Remove player from AOD" class="removeMember btn btn-danger"><i
+                                class="fa fa-trash fa-lg"></i> Remove<span class="hidden-sm hidden-xs"> from AOD</span></a>
+
+                @else
+                    {{-- else show request removal--}}
+                    <a href="#" title="Remove player from AOD" class="requestRemoval btn btn-warning"><i
+                                class="fa fa-trash fa-lg"></i> Request<span class="hidden-sm hidden-xs"> removal</span></a>
+
+                @endif
             </div>
+
+            {{--Else show request removal button--}}
         </div>
     </div>
     <hr/>
 
+    {{-- Member not primary in any division --}}
     @if (!$member->primaryDivision)
-        <div class="alert alert-danger">This player does not belong to a division supported by the tracker. </div>
+        <div class="alert alert-danger">This player does not belong to a division supported by the tracker.</div>
     @endif
 
 
