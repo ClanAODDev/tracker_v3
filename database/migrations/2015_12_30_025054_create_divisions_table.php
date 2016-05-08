c09e04822483abceddeeae247541f72d8198555a
flashadvocate <flashadvocate@gmail.com>
2016-05-08 00:31:56 -0400

    drop description, add isEnabled column

--- database/migrations/2015_12_30_025054_create_divisions_table.php
+++ database/migrations/2015_12_30_025054_create_divisions_table.php
@@ -13,19 +13,18 @@ class CreateDivisionsTable extends Migration
      */
     public function up()
     {
-
         Schema::create('divisions', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('abbreviation')->unique();
-            $table->text('description');
+            $table->boolean('enabled')->default(false);
             $table->string('division_structure');
             $table->string('welcome_forum');
             $table->mediumInteger('handle_id');
             $table->timestamps();
-
-            $this->populateDivisions();
         });
+
+        $this->populateDivisions();
     }
  
     /**
@@ -46,7 +45,6 @@ class CreateDivisionsTable extends Migration
                 [
                     'name' => 'AOD Racing',
                     'abbreviation' => 'aodr',
-                    'description' => 'Project Cars',
                     'division_structure' => 103832,
                     'welcome_forum' => 544,
                     'handle_id' => 0
@@ -56,7 +54,6 @@ class CreateDivisionsTable extends Migration
                 [
                     'name' => 'ARK',
                     'abbreviation' => 'ark',
-                    'description' => 'ARK: Survival Evolved',
                     'division_structure' => 128577,
                     'welcome_forum' => 533,
                     'handle_id' => 0
@@ -66,7 +63,6 @@ class CreateDivisionsTable extends Migration
                 [
                     'name' => 'Battlefield',
                     'abbreviation' => 'bf',
-                    'description' => 'Battlefield 4',
                     'division_structure' => 73448,
                     'welcome_forum' => 458,
                     'handle_id' => 2
@@ -76,7 +72,6 @@ class CreateDivisionsTable extends Migration
                 [
                     'name' => 'Battlefront',
                     'abbreviation' => 'swb',
-                    'description' => 'Star Wars: Battlefront',
                     'division_structure' => 115653,
                     'welcome_forum' => 574,
                     'handle_id' => 2,
@@ -87,7 +82,6 @@ class CreateDivisionsTable extends Migration
                 [
                     'name' => 'Jedi Knight',
                     'abbreviation' => 'jk',
-                    'description' => 'Jedi Knight: MB II',
                     'division_structure' => 62557,
                     'welcome_forum' => 123,
                     'handle_id' => 0
@@ -97,7 +91,6 @@ class CreateDivisionsTable extends Migration
                 [
                     'name' => 'Jedi Knight',
                     'abbreviation' => 'jk',
-                    'description' => 'Planetside 2',
                     'division_structure' => 65422,
                     'welcome_forum' => 393,
                     'handle_id' => 0
@@ -107,7 +100,6 @@ class CreateDivisionsTable extends Migration
                 [
                     'name' => 'Skyforge',
                     'abbreviation' => 'sf',
-                    'description' => 'Skyforge',
                     'division_structure' => 119785,
                     'welcome_forum' => 566,
                     'handle_id' => 0
@@ -117,7 +109,6 @@ class CreateDivisionsTable extends Migration
                 [
                     'name' => 'Tom Clancy',
                     'abbreviation' => 'tc',
-                    'description' => 'Tom Clancy',
                     'division_structure' => 121653,
                     'welcome_forum' => 495,
                     'handle_id' => 0
@@ -127,7 +118,6 @@ class CreateDivisionsTable extends Migration
                 [
                     'name' => 'Warframe',
                     'abbreviation' => 'wf',
-                    'description' => 'Warframe',
                     'division_structure' => 104706,
                     'welcome_forum' => 514,
                     'handle_id' => 0
@@ -137,7 +127,6 @@ class CreateDivisionsTable extends Migration
                 [
                     'name' => 'Warthunder',
                     'abbreviation' => 'wt',
-                    'description' => 'Warthunder',
                     'division_structure' => 64966,
                     'welcome_forum' => 432,
                     'handle_id' => 0
