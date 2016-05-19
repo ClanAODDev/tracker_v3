9e3cca10b623030c329d0e7592264a7c3514c368
Chris Deaton <dcdeaton@ncsu.edu>
2016-05-18 20:51:00 -0400

    minor cleanup

--- resources/views/member/search.blade.php
+++ resources/views/member/search.blade.php
@@ -1,14 +1,12 @@
 @if (count($members))
     @foreach ($members as $member)
         <a href="{{ action('MemberController@show', $member->clan_id) }}" class="list-group-item">
-            <strong>{{ $member->rankName }}</strong>
+            <strong>{{ $member->present()->rankName }}</strong>
             @if ($member->primaryDivision)
                 <span class="pull-right text-muted">{{ $member->primaryDivision->name }}</span>
             @endif
         </a>
     @endforeach
 @else
-
     <li class="text-muted list-group-item">No results found.</li>
-
 @endif
