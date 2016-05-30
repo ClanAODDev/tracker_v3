38b3543f7d04a1db244a5129ac1cceb8746991e3
Chris Deaton <dcdeaton@ncsu.edu>
2016-05-30 09:21:21 -0400

    division data endpoint for AOD API

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,7 +4,6 @@
 /**
  * Authentication-protected routes
  */
-use App\AOD\DivisionInfo;
 
 Route::group(['middleware' => 'web'], function () {
 
@@ -74,7 +73,7 @@ Route::group(['middleware' => 'slack'], function () {
  */
 Route::group(['prefix' => 'AOD', 'middleware' => 'throttle:5'], function () {
     Route::get('/division-data/{division_name}', function ($division_name) {
-        $info = new DivisionInfo($division_name);
+        $info = new \App\AOD\MemberSync\GetDivisionInfo($division_name)
 
         return response()->json($info->data);
     });
