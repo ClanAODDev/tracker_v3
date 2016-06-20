4ed2ddb5914a45d80e16d7ce9163700b4605c2bd
Chris Deaton <dcdeaton@ncsu.edu>
2016-06-20 19:25:40 -0400

    fix kernel command typo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('memberSync')->hourly();
+        $schedule->command('do:membersync')->hourly();
     }
 }
