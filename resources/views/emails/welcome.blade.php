1807236d02fcb74c0a660e0477f1e6621012a05d
Chris Deaton <dcdeaton@ncsu.edu>
2016-06-22 08:06:47 -0400

    update logo size

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,4 +1,4 @@
-<h1><img src="http://aod-tracker.com/tracker/assets/images/icons/large/tracker.png"/> AOD Tracker v3</h1>
+<h1><img src="http://aod-tracker.com/tracker/assets/images/icons/small/tracker.png"/> AOD Tracker v3</h1>
 <h2>Howdy {{ $user->name }}!</h2>
 <p>You recently created an account on the AOD Division Tracker, a tool for managing members assigned to gaming divisions in the AOD clan.</p>
 <p>Your default role on the tracker is <strong>User</strong>. If you are an NCO in a division, you will need to have someone in your leadership update your account permissions to the appropriate level.</p>
