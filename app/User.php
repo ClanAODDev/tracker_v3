4cb25154fcab0e4e0646afc946a468920b96004f
Laravel Shift <shift@laravelshift.com>
2016-09-16 16:25:21 +0000

    Shift core files

--- app/User.php
+++ app/User.php
@@ -5,9 +5,11 @@ namespace App;
 use App\Settings\UserSettings;
 use Doctrine\Instantiator\Exception\InvalidArgumentException;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+use Notifiable;
 
     /**
      * The attributes that are mass assignable.
