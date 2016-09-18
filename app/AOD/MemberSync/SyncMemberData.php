ee834d7e66d6fe3162244ea2e6f9a93d2c4a2ec7
Chris Deaton <dcdeaton@ncsu.edu>
2016-09-18 14:59:28 -0400

    remove echo, get all divisions during sync

--- app/AOD/MemberSync/SyncMemberData.php
+++ app/AOD/MemberSync/SyncMemberData.php
@@ -17,7 +17,7 @@ class SyncMemberData
      */
     public static function execute()
     {
-        foreach (Division::active()->get() as $division) {
+        foreach (Division::all() as $division) {
             // log activity
             Log::info(date('Y-m-d h:i:s') . " - MEMBER SYNC - fetching {$division->name}");
 
@@ -44,8 +44,6 @@ class SyncMemberData
         $member = Member::firstOrCreate([
             'clan_id' => $item['userid'],
         ]);
-
-        echo $member->clan_id;
 
         // have they been recently promoted?
         if ($member->rank_id < ($item['aodrankval'] - 2) && $member->rank_id > 0) {
