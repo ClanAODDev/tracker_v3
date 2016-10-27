1c925ac64ea434bb0400a27b1e641fcebbe42c62
Chris Deaton <dcdeaton@ncsu.edu>
2016-10-27 01:55:35 -0400

    cleanup

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -89,23 +89,4 @@ class RegisterController extends Controller
             'member_id' => $member->id,
         ]);
     }
-
-//
-//    protected function validator(array $data)
-//    {
-//        return Validator::make($data, [
-//            'name' => 'required|max:255',
-//            'email' => 'required|email|max:255|unique:users',
-//            'password' => 'required|min:6|confirmed',
-//        ]);
-//    }
-//
-//    protected function create(array $data)
-//    {
-//        return User::create([
-//            'name' => $data['name'],
-//            'email' => $data['email'],
-//            'password' => bcrypt($data['password']),
-//        ]);
-//    }
 }
