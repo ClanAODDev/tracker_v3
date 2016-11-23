b41420ed10a377c288fc9776813b14fee598c9e6
Chris Deaton <dcdeaton@ncsu.edu>
2016-11-23 00:21:46 -0500

    add recruit settings migration

---
+++ database/migrations/2016_11_23_051943_add_recruitment_settings.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRecruitmentSettings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $divisions = Division::all();
+
+        foreach ($divisions as $division) {
+            $settings = $division->settings();
+
+            $settings->set('division_structure', '');
+            $settings->set('welcome_area', '');
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
