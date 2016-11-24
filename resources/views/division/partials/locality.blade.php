38a5cf753a30f45740423b2bb9365bd5d8884939
Chris Deaton <dcdeaton@ncsu.edu>
2016-11-24 10:36:02 -0500

    handle empty indexes in locality

--- resources/views/division/partials/locality.blade.php
+++ resources/views/division/partials/locality.blade.php
@@ -1,15 +1,18 @@
 <tr><th>Old String</th> <th>New String</th></tr>
 @foreach($division->settings()->locality as $translation)
 
+    <?php $old_string = ! empty($translation['old-string']) ? $translation['old-string'] : null ?>
+    <?php $new_string = ! empty($translation['new-string']) ? $translation['new-string'] : null ?>
+
     <tr>
         <td class="col-xs-4">
             <input type="text" name="locality[{{ $loop->index }}][old-string]" class="form-control"
-                   value="{{ $translation['old-string'] }}" readonly />
+                   value="{{ $old_string }}" readonly />
         </td>
 
         <td class="col-xs-8">
             <input type="text" name="locality[{{ $loop->index }}][new-string]" class="form-control"
-                   value="{{ $translation['new-string'] }}" required/>
+                   value="{{ $new_string }}" required/>
         </td>
     </tr>
 @endforeach
