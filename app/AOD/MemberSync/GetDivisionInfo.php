b209b02bac7d6f53b59f1c41c0f4e6f950fa438a
Chris Deaton <dcdeaton@ncsu.edu>
2016-12-04 00:35:47 -0500

    add debugging

--- app/AOD/MemberSync/GetDivisionInfo.php
+++ app/AOD/MemberSync/GetDivisionInfo.php
@@ -59,7 +59,7 @@ class GetDivisionInfo
 
         $results = curl_exec($ch);
 
-        $data = json_decode(utf8_encode($results));
+        $data = json_decode($results);
 
         curl_close($ch);
 
