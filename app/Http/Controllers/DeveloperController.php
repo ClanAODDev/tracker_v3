b0975d3a45da5762d58dbba59b3cb0aefac8003f
Chris Deaton <dcdeaton@ncsu.edu>
2016-12-29 16:19:16 -0500

    middleware admin for api credential creation

--- app/Http/Controllers/DeveloperController.php
+++ app/Http/Controllers/DeveloperController.php
@@ -8,7 +8,7 @@ class DeveloperController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth', 'admin']);
     }
 
     public function index()
