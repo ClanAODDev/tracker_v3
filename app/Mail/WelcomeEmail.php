ff561d73781f4d3d6ca99da9726b48bba5717b0f
Chris Deaton <dcdeaton@ncsu.edu>
2017-01-12 21:27:02 -0500

    add mailable WelcomeEmail

---
+++ app/Mail/WelcomeEmail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class WelcomeEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.welcome');
+    }
+}
