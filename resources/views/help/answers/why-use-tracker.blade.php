167b947d7ee65ca0be5a807e15d3ef77ce35b364
Chris Deaton <dcdeaton@ncsu.edu>
2017-02-06 12:10:32 -0500

    break documentation in component

---
+++ resources/views/help/answers/why-use-tracker.blade.php
@@ -0,0 +1,9 @@
+<h3>Why use the tracker?</h3>
+
+<p>The Tracker was created out of a desire to, initially, simplify and speed up the
+    process of recruiting new members. Over time, it evolved into a meaningful way to
+    organize members into platoons and squads, as well as to ensure accurate record of
+    active or inactive members.</p>
+
+<p class="c-white"><strong>It is not a replacement for the forums. It acts as a
+        complement.</strong></p>
--- resources/views/help/index.blade.php
+++ resources/views/help/index.blade.php
@@ -6,35 +6,26 @@
             <div class="col-lg-12">
                 <div class="view-header">
                     <div class="pull-right text-right" style="line-height: 14px">
-                        <small>App Pages<br>Basic<br> <span class="c-white">Support</span></small>
+                        <small>AOD Tracker<br>Documentation<br> <span class="c-white">v3</span></small>
                     </div>
                     <div class="header-icon">
                         <i class="pe page-header-icon pe-7s-help2"></i>
                     </div>
                     <div class="header-title">
-                        <h3>Support</h3>
-                        <small>
-                            Show user data in special designed profile page
-                        </small>
+                        <h3>Documentation</h3>
+                        <small>Tracker support documentation for managing divisions</small>
                     </div>
                 </div>
-                <hr>
+                <hr />
             </div>
         </div>
 
         <div class="row">
-
             <div class="col-md-12">
-
                 <div class="panel panel-filled">
-
                     <div class="panel-body">
-
-                        <h3>Support </h3>
-
-                        <p>
-                            Fill the form and find answer to your question or contact with us on support email.
-                        </p>
+                        <h3>Search</h3>
+                        <p>Search for information about a particular topic, or select from the frequently asked questions provided below.</p>
 
                         <div class="form-group">
                             <input class="form-control" placeholder="What are you looking for ?">
@@ -54,12 +45,10 @@
 
                     <ul class="list-unstyled">
                         <li class="panel panel-filled support-question active">
-                            <a href="#answer1" data-toggle="tab">
+                            <a href="#why-use-tracker" data-toggle="tab">
                                 <div class="panel-body">
-                                    <p class="font-bold c-white">Support question 1</p>
-                                    <p>
-                                        Various versions have evolved over the years, sometimes by accident, sometimes on purpose.
-                                    </p>
+                                    <p class="font-bold c-white">Why use the tracker?</p>
+                                    <p>My division has used spreadsheets for years. Why should we use the tracker instead?</p>
                                 </div>
                             </a>
                         </li>
@@ -67,9 +56,7 @@
                             <a href="#answer2" data-toggle="tab">
                                 <div class="panel-body">
                                     <p class="font-bold c-white">Support question 2</p>
-                                    <p>
-                                        Have evolved over the years, sometimes by accident, sometimes.
-                                    </p>
+                                    <p>Have evolved over the years, sometimes by accident, sometimes.</p>
                                 </div>
                             </a>
                         </li>
@@ -77,9 +64,7 @@
                             <a href="#answer3" data-toggle="tab">
                                 <div class="panel-body">
                                     <p class="font-bold c-white">Support question 3</p>
-                                    <p>
-                                        Various have evolved over the years, sometimes by accident.
-                                    </p>
+                                    <p>Various have evolved over the years, sometimes by accident.</p>
                                 </div>
                             </a>
                         </li>
@@ -87,9 +72,7 @@
                             <a href="#answer4" data-toggle="tab">
                                 <div class="panel-body">
                                     <p class="font-bold c-white">Support question 4</p>
-                                    <p>
-                                        Versions have evolved over the years, sometimes by accident, sometimes on purpose
-                                    </p>
+                                    <p>Versions have evolved over the years, sometimes by accident, sometimes on purpose</p>
                                 </div>
                             </a>
                         </li>
@@ -103,120 +86,26 @@
                 <div class="panel">
                     <div class="panel-body">
                         <div class="tab-content">
-                            <div id="answer1" class="tab-pane active">
-                                <h3>
-                                    Question 1
-                                </h3>
-                                <p>
-                                    Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-                                </p>
-                                <ul>
-                                    <li>Lorem ipsum dolor sit amet, comes from</li>
-                                    <li>There are many variations of passages of Lorem Ipsum available</li>
-                                    <li>All the Lorem Ipsum generators on the Internet</li>
-                                </ul>
-                                <p class="font-bold c-white">
-                                    Contrary to popular belief, Lorem Ipsum is not
-                                </p>
-                                <p>
-                                    McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
-                                </p>
-                                <p>
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
-                                </p>
-                                <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                                </p>
 
+                            <div id="why-use-tracker" class="tab-pane active animated fadeInUp">
+                                @include('help.answers.why-use-tracker')
                             </div>
 
-                            <div id="answer2" class="tab-pane">
-                                <h3>
-                                    Question 2
-                                </h3>
-                                <p>
-                                    Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-                                </p>
-                                <ul>
-                                    <li>Lorem ipsum dolor sit amet, comes from</li>
-                                    <li>There are many variations of passages of Lorem Ipsum available</li>
-                                    <li>All the Lorem Ipsum generators on the Internet</li>
-                                    <li>Lorem ipsum dolor sit amet, comes from</li>
-                                    <li>All the Lorem Ipsum generators on the Internet</li>
-                                    <li>There are many variations of passages of Lorem Ipsum</li>
-                                    <li>All the Lorem Ipsum generators on the Internet</li>
-                                </ul>
-                                <p class="font-bold c-white">
-                                    Contrary to popular belief, Lorem Ipsum is not
-                                </p>
-                                <p>
-                                    McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
-                                </p>
-                                <p>
-                                    McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
-                                </p>
-
+                            <div id="answer2" class="tab-pane animated fadeInUp">
+                                @include('help.answers.why-use-tracker')
                             </div>
-                            <div id="answer3" class="tab-pane">
-                                <h3>
-                                    Question 3
-                                </h3>
-                                <p>
-                                    Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-                                </p>
-                                <ul>
-                                    <li>Lorem ipsum dolor sit amet, comes from</li>
-                                    <li>There are many variations of passages of Lorem Ipsum available</li>
-                                    <li>All the Lorem Ipsum generators on the Internet</li>
-                                </ul>
-                                <p class="font-bold c-white">
-                                    Contrary to popular belief, Lorem Ipsum is not
-                                </p>
-                                <p>
-                                    McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
-                                </p>
-                                <p>
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
-                                </p>
-                                <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-                                </p>
 
+                            <div id="answer3" class="tab-pane animated fadeInUp">
+                                @include('help.answers.why-use-tracker')
                             </div>
-                            <div id="answer4" class="tab-pane">
-                                <h3>
-                                    Question 4
-                                </h3>
-                                <p>
-                                    Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-                                </p>
-                                <ul>
-                                    <li>Lorem ipsum dolor sit amet, comes from</li>
-                                    <li>There are many variations of passages of Lorem Ipsum available</li>
-                                    <li>All the Lorem Ipsum generators on the Internet</li>
-                                    <li>Lorem ipsum dolor sit amet, comes from</li>
-                                    <li>All the Lorem Ipsum generators on the Internet</li>
-                                    <li>There are many variations of passages of Lorem Ipsum</li>
-                                    <li>All the Lorem Ipsum generators on the Internet</li>
-                                </ul>
-                                <p class="font-bold c-white">
-                                    Contrary to popular belief, Lorem Ipsum is not
-                                </p>
-                                <p>
-                                    McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
-                                </p>
-                                <p>
-                                    McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
-                                </p>
 
+                            <div id="answer4" class="tab-pane animated fadeInUp">
+                                @include('help.answers.why-use-tracker')
                             </div>
+
                         </div>
-
-
-
-
                     </div>
-
                 </div>
-
             </div>
         </div>
     </div>
