94f4b1a472b5afc38f87629d24cf31cc047d4cd5
Chris Deaton <dcdeaton@ncsu.edu>
2017-02-08 10:29:09 -0500

    add proper name to divisions, use if provided (like floater)

--- app/Division/HasCustomAttributes.php
+++ app/Division/HasCustomAttributes.php
@@ -2,5 +2,26 @@
 
 namespace App\Division;
 
+use App;
+
 trait HasCustomAttributes
-{}
+{
+    /**
+     * Use proper name if it's provided
+     * @param $value
+     * @return string
+     */
+    public function getNameAttribute($value)
+    {
+        // avoid breaking reliance in data sync
+        if (App::runningInConsole()) {
+            return $value;
+        }
+
+        if ($this->proper_name) {
+            return $this->proper_name;
+        }
+
+        return $value;
+    }
+}
