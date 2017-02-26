caf3c9cd819ba1b7cc6d2a25a85a3ec2aec63ffc
Chris Deaton <dcdeaton@ncsu.edu>
2017-02-26 01:46:41 -0500

    add new logo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,53 +15,52 @@
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
+        @if (count($errors))
+            <div class="alert alert-danger">
+                @foreach ($errors->all() as $error)
+                    <li>
+                        {{ $error }}
+                    </li>
+                @endforeach
+            </div>
+        @endif
 
-                @if (count($errors))
-                    <div class="alert alert-danger">
-                        @foreach ($errors->all() as $error)
-                            <li>
-                                {{ $error }}
-                            </li>
-                        @endforeach
+        <div class="panel panel-filled">
+            <div class="panel-body">
+
+                <form role="form" method="POST" action="{{ url('/register') }}">
+                    {!! csrf_field() !!}
+
+                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <label class="control-label" for="name">Username</label>
+                        <input type="text" class="form-control" name="name" value="{{ old('name') }}"
+                               placeholder="Username">
                     </div>
-                @endif
 
-                <div class="panel panel-filled">
-                    <div class="panel-body">
-
-                        <form role="form" method="POST" action="{{ url('/register') }}">
-                            {!! csrf_field() !!}
-
-                            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                <input type="text" class="form-control" name="name" value="{{ old('name') }}"
-                                       placeholder="Username">
-                            </div>
-
-                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}"
-                                       placeholder="Email Address">
-                            </div>
-
-                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <input type="password" class="form-control" name="password" placeholder="Password">
-                            </div>
-
-                            <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                                <input type="password" class="form-control" name="password_confirmation"
-                                       placeholder="Confirm Password">
-                            </div>
-
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-accent btn-block">
-                                    Register
-                                </button>
-                            </div>
-
-                        </form>
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label class="control-label" for="email">E-mail</label>
+                        <input type="email" class="form-control" name="email" value="{{ old('email') }}"
+                               placeholder="Email Address">
                     </div>
-                </div>
+
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label class="control-label" for="email">Password</label>
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                    </div>
+
+                    <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                        <label class="control-label" for="email">Confirm Password</label>
+                        <input type="password" class="form-control" name="password_confirmation"
+                               placeholder="Confirm Password">
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-accent btn-block">
+                            Register
+                        </button>
+                    </div>
+
+                </form>
             </div>
         </div>
     </div>
