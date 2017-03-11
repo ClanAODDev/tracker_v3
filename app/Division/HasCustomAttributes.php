92d337daf2e508e06d5d173e8e47b4f8fed4da48
Chris Deaton <dcdeaton@ncsu.edu>
2017-03-11 16:14:37 -0500

    rollback proper_name migration

--- app/Division/HasCustomAttributes.php
+++ app/Division/HasCustomAttributes.php
@@ -6,22 +6,5 @@ use App;
 
 trait HasCustomAttributes
 {
-    /**
-     * Use proper name if it's provided
-     * @param $value
-     * @return string
-     */
-    public function getNameAttribute($value)
-    {
-        // avoid breaking reliance in data sync
-        if (App::runningInConsole()) {
-            return $value;
-        }
 
-        if ($this->proper_name) {
-            return $this->proper_name;
-        }
-
-        return $value;
-    }
 }
