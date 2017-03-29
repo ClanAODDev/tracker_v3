59548e72f54dff192e4699f4a56ea10a74463482
Chris Deaton <dcdeaton@ncsu.edu>
2017-03-29 18:13:21 -0400

    add dependency

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use App\Mail\RoleAssigned;
 use App\Settings\UserSettings;
+use Illuminate\Support\Facades\Mail;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
