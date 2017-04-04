e57ba74fab903b806f752b770bca581641a3b35c
Chris Deaton <flashadvocate@gmail.com>
2017-04-04 17:51:48 -0400

    add mail config

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'tracker@clanaod.net'),
-        'name' => env('MAIL_FROM_NAME', 'Admin'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
