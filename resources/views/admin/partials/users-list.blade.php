c751d16149611d8a81c7566e0ca4e6882ed09a07
Chris Deaton <dcdeaton@ncsu.edu>
2017-04-11 07:52:09 -0400

    slight appearance change to user roles

--- resources/views/admin/partials/users-list.blade.php
+++ resources/views/admin/partials/users-list.blade.php
@@ -30,7 +30,7 @@
                     <td>{{ $user->email }}</td>
                     <td>
                         {{ $user->role->id }}
-                        <small class="slight text-muted">{!! $user->role->present()->roleLabelColored !!}</small>
+                        <small class="slight">{!! $user->role->present()->roleLabelColored !!}</small>
                     </td>
                     <td class="text-uppercase">
                         <small class="slight text-{{ ($user->isDeveloper()) ? 'success' : 'muted' }}">
