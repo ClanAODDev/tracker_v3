51bbc925fd19f50fbd096469147bc31b0e8442c2
Chris Deaton <dcdeaton@ncsu.edu>
2017-04-29 00:52:54 -0400

    add missing dependency

--- app/Division.php
+++ app/Division.php
@@ -5,6 +5,7 @@ namespace App;
 use App\Activities\RecordsActivity;
 use App\Presenters\DivisionPresenter;
 use App\Settings\DivisionSettings;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
