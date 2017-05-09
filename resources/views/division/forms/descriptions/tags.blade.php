b8392af2717c00dae2d7a3ec2e5faee2f964c088
Chris Deaton <dcdeaton@ncsu.edu>
2017-05-09 14:59:44 -0400

    show default tags

--- resources/views/division/forms/descriptions/tags.blade.php
+++ resources/views/division/forms/descriptions/tags.blade.php
@@ -2,3 +2,8 @@
 <p>In addition to the default tags, divisions can define specific custom tags for leadership to use when annotating member profiles.</p>
 
 <p>Deleted tags that are currently in use on a member note will be removed from that note.</p>
+
+<h5 class="m-t-lg">Default Tags</h5>
+@foreach ($defaultTags as $tag)
+    <span class="badge">{{ $tag->name }}</span>
+@endforeach
