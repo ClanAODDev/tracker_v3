46db0c7b67d497b80d8727109994954800012b3d
Chris Deaton <dcdeaton@ncsu.edu>
2017-05-11 10:45:24 -0400

    cleaner interface for managing tags

--- resources/views/division/partials/division-tags.blade.php
+++ resources/views/division/partials/division-tags.blade.php
@@ -1,24 +1,28 @@
-@forelse($division->tags as $tag)
-    <div class="row form-group" data-repeater-item>
-        <div class="col-xs-11">
-            <input type="text" name="tags[{{ $loop->index+1 }}][tag]"
-                   class="form-control" placeholder="Enter a tag..." value="{{ $tag->name }}" required />
+<div class="row">
+    @forelse ($division->tags as $tag)
+        <div class="col-xs-3" data-repeater-item>
+            <div class="input-group form-group">
+                <input type="text" name="tags[{{ $loop->index+1 }}][tag]"
+                       class="form-control" placeholder="Enter a tag..." value="{{ $tag->name }}" required />
+                <span class="input-group-btn">
+                    <button data-repeater-delete class="btn-xs btn" type="button" style="margin-left: -30px">
+                        <i class="fa fa-times fa-lg"></i>
+                    </button>
+                </span>
+            </div>
         </div>
-        <div class="col-xs-1">
-            <button type="button" data-repeater-delete class="btn btn-danger">
-                <i class="fa fa-trash-o fa-lg"></i></button>
+    @empty
+        <div class="col-xs-3" data-repeater-item>
+            <div class="input-group form-group">
+                <input type="text" name="tags[0][tag]"
+                       class="form-control" placeholder="Enter a tag..." required />
+                <span class="input-group-btn">
+                    <button data-repeater-delete class="btn-xs btn" type="button" style="margin-left: -30px">
+                        <i class="fa fa-times fa-lg"></i>
+                    </button>
+                </span>
+            </div>
         </div>
-    </div>
-@empty
-    <div class="row form-group" data-repeater-item>
-        <div class="col-xs-11">
-            <input type="text" name="tags[0][tag]"
-                   class="form-control" placeholder="Enter a tag..." required />
-        </div>
-        <div class="col-xs-1">
-            <button type="button" data-repeater-delete class="btn btn-danger">
-                <i class="fa fa-trash-o fa-lg"></i></button>
-        </div>
-    </div>
-@endforelse
+    @endforelse
+</div>
 
