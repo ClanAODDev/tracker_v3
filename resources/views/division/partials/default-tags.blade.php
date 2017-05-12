24ebc549366269ab32d4faf11a366435513bc60b
Chris Deaton <dcdeaton@ncsu.edu>
2017-05-12 13:34:00 -0400

    fix tag layout for divisions

--- resources/views/division/partials/default-tags.blade.php
+++ resources/views/division/partials/default-tags.blade.php
@@ -1,6 +1,6 @@
 @foreach($defaultTags as $tag)
-    <div class="col-xs-3">
-        <div class="input-group form-group">
+    <div class="col-xs-6">
+        <div class="form-group">
             <input type="text" class="form-control" value="{{ $tag->name }}" disabled />
         </div>
     </div>
