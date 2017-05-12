f02dd50377c283e0934198191ebfbe879b398a54
Chris Deaton <dcdeaton@ncsu.edu>
2017-05-12 13:39:08 -0400

    expand size of tag input

--- resources/views/admin/partials/tags.blade.php
+++ resources/views/admin/partials/tags.blade.php
@@ -1,13 +1,11 @@
-@foreach($defaultTags as $tag)
-    <div class="col-sm-4 col-xs-6" data-repeater-item>
-        <div class="input-group form-group">
-            <input type="text" name="tags[{{ $loop->index }}][tag]"
+@foreach ($defaultTags as $tag)
+    <div class="col-xs-6" data-repeater-item>
+        <div class="form-group">
+            <input type="text" name="tags[{{ $loop->index }}][tag]" maxlength="16"
                    class="form-control" placeholder="Enter a tag..." value="{{ $tag->name }}" required />
-            <span class="input-group-btn">
-                    <button data-repeater-delete class="btn-xs btn" type="button" style="margin-left: -30px">
-                        <i class="fa fa-times fa-lg"></i>
-                    </button>
-                </span>
+            <button data-repeater-delete class="btn-xs btn" type="button" style="position: absolute; top: 5px; right: 10px;">
+                <i class="fa fa-times fa-lg"></i>
+            </button>
         </div>
     </div>
 @endforeach
