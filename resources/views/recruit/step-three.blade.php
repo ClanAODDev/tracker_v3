6a36f7f5dd3b1a67290c80a10ed738958534760f
Chris Deaton <dcdeaton@ncsu.edu>
2017-05-20 20:32:32 -0400

    fix undefined route

--- resources/views/recruit/step-three.blade.php
+++ resources/views/recruit/step-three.blade.php
@@ -1,7 +1,28 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dcdeaton
- * Date: 5/20/17
- * Time: 8:20 PM
- */
+@extends('application.base')
+
+@section('content')
+
+    @component ('application.components.division-heading', [$division])
+        @slot ('icon')
+            <img src="{{ getDivisionIconPath($division->abbreviation) }}" class="division-icon-large" />
+        @endslot
+        @slot ('heading')
+            <span class="hidden-xs">{{ $division->name }}</span>
+            <span class="visible-xs">{{ $division->abbreviation }}</span>
+        @endslot
+        @slot ('subheading')
+            Recruit New Member
+        @endslot
+    @endcomponent
+
+    <div class="container-fluid">
+        <h4><i class="fa fa-pencil-square-o"></i> Step 2: Member Agreement</h4>
+        <hr />
+
+    </div>
+
+@stop
+
+@section('footer_scripts')
+    <script src="{!! asset('/js/recruiting.js') !!}"></script>
+@stop
