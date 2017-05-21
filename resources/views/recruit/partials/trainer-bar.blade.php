cbda27714a9c8bb536b6279c59090e8894df414b
Chris Deaton <dcdeaton@ncsu.edu>
2017-05-21 11:51:26 -0400

    training mode for recruiting - change button label

--- resources/views/recruit/partials/trainer-bar.blade.php
+++ resources/views/recruit/partials/trainer-bar.blade.php
@@ -1,5 +1,5 @@
 <div class="alert text-center">
     <i class="fa fa-question-circle text-accent"></i>
     Training, or just want to look around?
-    <button type="button" name="trngMode" class="btn btn-rounded btn-accent btn-sm">Enable Training Mode</button>
+    <button type="button" name="trngMode" class="btn btn-rounded btn-accent btn-sm">Use Dummy Data</button>
 </div>
