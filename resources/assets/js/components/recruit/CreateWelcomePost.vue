4352d1b34bf20e04ea2c91a9c069c2f04cea81fc
Chris Deaton <dcdeaton@ncsu.edu>
2017-05-26 15:22:42 -0400

    fix welcome area handling

--- resources/assets/js/components/recruit/CreateWelcomePost.vue
+++ resources/assets/js/components/recruit/CreateWelcomePost.vue
@@ -6,7 +6,7 @@
             <p>This division uses a welcome thread for all new recruit introductions. Click the button below to create a post and introduce your new recruit to the other members of the division.</p>
 
             <div class="text-center p-lg bs-example">
-                <a :href="'https://www.clanaod.net/forums/newreply.php?do=postreply&t=' + store.division.settings.welcome_area"
+                <a :href="'https://www.clanaod.net/forums/showthread.php?goto=newpost&t=' + store.division.settings.welcome_area
                    target="_blank" class="btn btn-accent">
                     <i class="fa fa-external-link text-accent" aria-hidden="true"></i> Create Post
                 </a>
@@ -18,7 +18,7 @@
             <p>This division uses a welcome forum for all new recruit introductions. Click the button below to create a thread and introduce your new recruit to the other members of the division.</p>
 
             <div class="text-center p-lg bs-example">
-                <a :href="'https://www.clanaod.net/forums/showthread.php?goto=newpost&t=' + store.division.settings.welcome_area"
+                <a :href="'https://www.clanaod.net/forums/newthread.php?do=newthread&f=' + store.division.settings.welcome_area"
                    target="_blank" class="btn btn-accent">
                     <i class="fa fa-external-link text-accent" aria-hidden="true"></i> Create Thread
                 </a>
