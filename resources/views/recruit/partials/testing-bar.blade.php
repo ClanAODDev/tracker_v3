75f893a6942aa7ef7d46efaa8aab19bc19153504
Chris Deaton <dcdeaton@ncsu.edu>
2017-05-26 13:22:06 -0400

    cleanup old recruiting views

--- resources/views/recruit/partials/testing-bar.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="panel panel-c-info panel-filled">
-    <div class="panel-body text-uppercase slight">
-        <i class="fa fa-wrench text-info"></i> Training Mode
-        <span class="text-muted pull-right">Data will not be saved</span>
-    </div>
-</div>
