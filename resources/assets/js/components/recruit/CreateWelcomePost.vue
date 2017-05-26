dd66c378fdd49cb923427f0bf731363617dc70f8
Chris Deaton <dcdeaton@ncsu.edu>
2017-05-26 15:24:15 -0400

    fix typo

--- resources/assets/js/components/recruit/CreateWelcomePost.vue
+++ resources/assets/js/components/recruit/CreateWelcomePost.vue
@@ -6,7 +6,7 @@
             <p>This division uses a welcome thread for all new recruit introductions. Click the button below to create a post and introduce your new recruit to the other members of the division.</p>
 
             <div class="text-center p-lg bs-example">
-                <a :href="'https://www.clanaod.net/forums/showthread.php?goto=newpost&t=' + store.division.settings.welcome_area
+                <a :href="'https://www.clanaod.net/forums/showthread.php?goto=newpost&t=' + store.division.settings.welcome_area"
                    target="_blank" class="btn btn-accent">
                     <i class="fa fa-external-link text-accent" aria-hidden="true"></i> Create Post
                 </a>
