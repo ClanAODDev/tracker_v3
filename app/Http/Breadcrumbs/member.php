f1183558e8993d9167929ac72feb94d114908195
Chris Deaton <dcdeaton@ncsu.edu>
2017-06-05 11:01:09 -0400

    no platoon, squad check if no relationship exists

--- app/Http/Breadcrumbs/member.php
+++ app/Http/Breadcrumbs/member.php
@@ -7,14 +7,14 @@ Breadcrumbs::register('member', function ($breadcrumbs, $member, $division) {
         $breadcrumbs->push($division->name, route('division', $division->abbreviation));
     }
 
-    if ($member->platoon_id) {
+    if ($member->platoon_id !== 0) {
         $breadcrumbs->push(
             ucwords($member->platoon->name),
             route('platoon', [$division->abbreviation, $member->platoon->id])
         );
     }
 
-    if ($member->squad_id) {
+    if ($member->squad_id !== 0) {
         $breadcrumbs->push(ucwords($member->squad->name));
     }
 
