5645946bb76863e2e36894e2dd75cd365176a1e5
Chris Deaton <dcdeaton@ncsu.edu>
2017-06-06 11:55:22 -0400

    mail should use sender values from environment config

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', env('MAIL_SENDER')),
+        'name' => env('MAIL_FROM_NAME', env('MAIL_SENDER_NAME')),
     ],
 
     /*
