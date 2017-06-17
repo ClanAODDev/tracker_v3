bb3d5bf2ae68fb33f3085661c6919c539f1ccdb3
Chris Deaton <dcdeaton@ncsu.edu>
2017-06-17 09:36:24 -0400

    add more documentation

--- resources/views/help/division-structures.blade.php
+++ resources/views/help/division-structures.blade.php
@@ -18,9 +18,16 @@
     @endcomponent
 
     <div class="container-fluid">
+        <div class="btn-group m-b-xl">
+            <a href="#basic-syntax" class="btn btn-default">Basic syntax</a>
+            <a href="#loops" class="btn btn-default">Loops and nesting</a>
+            <a href="#properties" class="btn btn-default">Available properties</a>
+        </div>
+
         <p>Divisions can customize their division structures with ease by making use of Twig, a templating engine, in conjunction with normal bb-code for styling. Read the following documentation on how to get started using Twig and what data is available for divisions to use.</p>
 
-        <h3 class="m-t-xl">Basic Syntax</h3>
+        <h3 class="m-t-xl" id="basic-syntax">Basic Syntax</h3>
+        <hr />
         <p>Twig makes use of three types of syntax: <code>@{{ output statements }}</code>,
             <code>{% control structures %}</code>, and
             <code>{# comments #}</code>. Output statements are used to echo strings, like name, or rank abbreviation, or position name. Control structures allow you to iterate over items, or operate on conditionals. An example of this would be:
@@ -31,7 +38,8 @@
         <p>Control structures typically have a beginning element and an ending element. <code>for</code> and
             <code>if</code> are the primary types of control structures.</p>
 
-        <h3 class="m-t-xl">Loops and nesting</h3>
+        <h3 class="m-t-xl" id="loops">Loops and nesting</h3>
+        <hr />
         <p>In order to properly build a division structure with tracker data, nested loops are required. A loop is a control structure that iterates over a series of items. Nesting loops entails using a control structure inside of another control structure. In order to access a division's squads, you must first iterate over the platoons. Consider the following example:</p>
 
         <pre><code class="language-twig">@include('help.snippets.loops')</code></pre>
@@ -43,7 +51,8 @@
         <p class="m-t-lg m-b-lg">Ultimately, we will want to output the squad members of each squad. As you might have guessed, this requires one additional nested control structure. Note the added bb-code for context.</p>
         <pre><code class="language-twig">@include('help.snippets.list')</code></pre>
 
-        <h3 class="m-t-xl">Available Properties</h3>
+        <h3 class="m-t-xl" id="properties">Available Properties</h3>
+        <hr />
         <h4 class="m-t-xl">Division</h4>
         @include('help.partials.division-properties')
 
