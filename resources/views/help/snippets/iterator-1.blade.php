e739dbd2885b0435d5451cb8d929c721d86aa147
Chris Deaton <dcdeaton@ncsu.edu>
2017-06-17 14:40:27 -0400

    update syntax

--- resources/views/help/snippets/iterator-1.blade.php
+++ resources/views/help/snippets/iterator-1.blade.php
@@ -3,7 +3,7 @@
 	{# outputs "Squad Name - Squad #1 #}
 	@{{ squad.name }} - Squad #@{{ loop.index }}
 
-	{% if loop.index is divisibleBy(2) %}
+	{% if loop.index is divisible by(2) %}
 		{# end the current row and start a new one #}
 		[/tr][tr]
 	{% endif %}
