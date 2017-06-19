6654bd19d005c41ff41d0aeb3521153a65a4244d
Chris Deaton <dcdeaton@ncsu.edu>
2017-06-18 22:59:32 -0400

    add alert link class

--- resources/views/member/partials/notices.blade.php
+++ resources/views/member/partials/notices.blade.php
@@ -2,7 +2,7 @@
     <div class="alert alert-warning">
         The {{ $primaryDivision->name }} division requires a(n)
         <code>{{ $primaryDivision->handle->label }}</code> handle, but {{ $member->name }} does not have one. You should
-        <a href="#handles">add it now</a>.
+        <a href="#handles" class="alert-link">add it now</a>.
     </div>
 @endif
 
