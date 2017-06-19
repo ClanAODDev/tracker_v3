c4f1f46ca37dcce6f5ae1e8b9a31f8d4c3487c28
Chris Deaton <dcdeaton@ncsu.edu>
2017-06-19 16:20:57 -0400

    fix platoons

--- resources/views/division/partials/platoons.blade.php
+++ resources/views/division/partials/platoons.blade.php
@@ -17,7 +17,21 @@
                 <div class="col-md-9"></div>
                 <div class="col-md-3"></div>
             </div>
+            <a href="{{ route('platoon', [$division->abbreviation, $platoon->id]) }}"
+               class="panel panel-filled">
+                <div class="panel-body">
+                    <h4 class="m-b-none">
+                        {{ $platoon->name }}
+                        <label class="badge pull-right">{{ $platoon->members_count }} Assigned</label>
+                    </h4>
 
+                    @if ($platoon->leader)
+                        <p class="list-group-item-text">{{ $platoon->leader->present()->rankName }}</p>
+                    @else
+                        <p class="list-group-item-text">Unfilled</p>
+                    @endif
+                </div>
+            </a>
         </div>
     @empty
         <div class="col-md-12">
