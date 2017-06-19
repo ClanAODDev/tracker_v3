ef8e3e330fab5785bf8e44f0d2ca9b4656685280
Chris Deaton <dcdeaton@ncsu.edu>
2017-06-18 22:51:37 -0400

    add handles url

--- resources/views/member/partials/notices.blade.php
+++ resources/views/member/partials/notices.blade.php
@@ -1,7 +1,8 @@
 @if (! $member->handles->contains($primaryDivision->handle) && $primaryDivision->handle)
     <div class="alert alert-warning">
         The {{ $primaryDivision->name }} division requires a
-        <code>{{ $primaryDivision->handle->label }}</code> handle, but {{ $member->name }} does not have one. You should add it now.
+        <code>{{ $primaryDivision->handle->label }}</code> handle, but {{ $member->name }} does not have one. You should
+        <a href="#handles">add it now</a>.
     </div>
 @endif
 
