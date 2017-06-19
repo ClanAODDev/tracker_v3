8ae1af790e90871af0b06618102687f3a83e6ca4
Chris Deaton <dcdeaton@ncsu.edu>
2017-06-19 06:56:52 -0400

    documentation fix

--- resources/views/help/partials/general.blade.php
+++ resources/views/help/partials/general.blade.php
@@ -1,5 +1,3 @@
-<h3 class="m-t-lg">General Information</h3>
-<hr />
 <p>The Tracker is an application intended to complement the AOD forums - not replace them - and help clan and division leadership do the things they do on an every-day basis easier and more effectively.</p>
 <p>Originally, the Tracker's sole mission was to solve the problem of recruiting, which for many was an arduous and chaotic process. The solution was to provide all the necessary information consolidated in one place. A few automated processes, like the ability to quickly scan for recruit posts in required response threads (R&U), made the process even faster.</p>
 <p>Today, the Tracker has evolved into much more. Recruiting is still one of its primary functions, but now, leaders can track member activity in the context of the rest of the clan. Weekly censuses provide a snapshot of population vs. weekly activity, allowing leaders to spot trends in division activity.</p>
