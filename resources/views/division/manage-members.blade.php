9b0c2d7e4ed4b3c924936fcd2eb683953feaa553
Chris Deaton <dcdeaton@ncsu.edu>
2017-07-07 06:43:54 -0400

    move droppable to partial

--- resources/views/division/manage-members.blade.php
+++ resources/views/division/manage-members.blade.php
@@ -39,26 +39,7 @@
 
             <div class="row mod-plt sortable-squad">
                 @foreach ($platoon->squads as $squad)
-                    <div class="col-md-3">
-                        <h5 class="grabbable"><i class="fa fa-drag-handle text-muted"></i>
-                            <strong>{{ $squad->name or "Untitled" }}</strong>
-                            @if ($squad->leader)
-                                <small>{{ $squad->leader->present()->rankName }}</small>
-                            @else
-                                <small>TBA</small>
-                            @endif
-                            <span class="pull-right badge badge-default count">{{ count($squad->members) }}</span>
-                        </h5>
-                        <hr />
-                        <ul class="sortable" data-squad-id="{{ $squad->id }}" style="min-height: 50px;">
-                            @foreach ($squad->members as $member)
-                                <li class="list-group-item grabbable" data-member-id="{{ $member->id }}">
-                                    <i class="fa fa-drag-handle text-muted pull-right"></i>
-                                    <span class="no-select">{{ $member->present()->rankName }}</span>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
+                    @include('platoon.partials.droppable-member')
                 @endforeach
             </div>
         </div>
