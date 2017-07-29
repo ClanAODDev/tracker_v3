64a1d86528f10959474b629996019e0cd75a700a
Chris Deaton <dcdeaton@ncsu.edu>
2017-07-29 15:44:03 -0400

    update views inactivity

--- resources/views/division/partials/filter-inactive.blade.php
+++ resources/views/division/partials/filter-inactive.blade.php
@@ -1,26 +1,21 @@
-<div class="panel panel-filled m-b-xl panel-c-info">
-    <div class="panel-body">
-        <div class="row m-t-xs">
-            <form method="get">
-                <div class="col-md-3">
-                    <h5><i class="fa fa-filter text-info"></i> Filter by {{ $division->locality('platoon') }}</h5>
-                </div>
-                <div class="col-md-5">
-
-                    <select name="platoon" id="platoon" class="form-control"
-                            onChange="top.location.href=this.options[this.selectedIndex].value">
-                        <option value="">None selected</option>
-                        @foreach ($division->platoons as $platoon)
-                            <option {{ (request()->platoon && request()->platoon->id == $platoon->id ? 'selected' : null) }} value="{{ route('division.inactive-members', [$division->abbreviation, $platoon->id]) }}">{{ $platoon->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="col-md-3 text-right">
-                    <a href="{{ route('division.inactive-members', $division->abbreviation) }}"
-                       class="btn btn-default">Reset Filter</a>
-                </div>
-            </form>
+<div class="row m-t-xs m-b-lg text-center">
+    <form method="get">
+        <div class="col-md-3">
+            <h5><i class="fa fa-filter text-info"></i> Filter by {{ $division->locality('platoon') }}</h5>
         </div>
+        <div class="col-md-5">
 
-    </div>
+            <select name="platoon" id="platoon" class="form-control"
+                    onChange="top.location.href=this.options[this.selectedIndex].value">
+                <option value="">None selected</option>
+                @foreach ($division->platoons as $platoon)
+                    <option {{ (request()->platoon && request()->platoon->id == $platoon->id ? 'selected' : null) }} value="{{ route('division.inactive-members', [$division->abbreviation, $platoon->id]) }}">{{ $platoon->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-md-3 text-right">
+            <a href="{{ route('division.inactive-members', $division->abbreviation) }}"
+               class="btn btn-default">Reset Filter</a>
+        </div>
+    </form>
 </div>
