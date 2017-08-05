0af84b8ce33cc3b976ace35ba13c3f7005f0790e
Chris Deaton <dcdeaton@ncsu.edu>
2017-08-04 22:46:33 -0400

    fix welcome email

--- app/Notifications/WelcomeUser.php
+++ app/Notifications/WelcomeUser.php
@@ -39,11 +39,9 @@ class WelcomeUser extends Notification
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-            ->line('You recently created an account on the AOD Division Tracker, a tool for managing members assigned to gaming divisions in the AOD clan.')
-            ->line("Your default role on the tracker is: **Member**. If you are an NCO in your division, you will need to have someone in your leadership update your account permissions to the appropriate level before you can access the member recruitment tools and other management areas.")
-            ->action('Visit the Tracker', route('home'))
-            ->line("If you have any questions, visit the [Tracker Documentation](" . route('help') . ") section.");
+        return (new MailMessage)->view(
+            'emails.welcome'
+        );
     }
 
     /**
