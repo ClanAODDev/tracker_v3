f1f65c946e92637e460ccd5f2a59301ec78fe7ce
Chris Deaton <dcdeaton@ncsu.edu>
2017-08-05 12:36:26 -0400

    slight style adjustment

--- resources/views/application/partials/primaryHeader.blade.php
+++ resources/views/application/partials/primaryHeader.blade.php
@@ -20,7 +20,8 @@
         </div>
 
         <div class="navbar-form navbar-left">
-            <img src="{{ asset('images/loading_2.gif') }}" alt="Results loading..." class="hidden results-loader">
+            <img src="{{ asset('images/loading_2.gif') }}" alt="Results loading..."
+                 class="hidden results-loader" style="opacity: .5" />
             <input type="text" class="form-control" id="member-search" name="search"
                    placeholder="Search for a player..." />
             <span id="searchclear" class="fa fa-times-circle fa-2x text-muted pull-right"></span>
