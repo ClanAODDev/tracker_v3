6b8c835e8d014b65530ada235139ad8ffba1bf2e
Chris Deaton <dcdeaton@ncsu.edu>
2017-08-09 15:36:12 -0400

    temp fix for activity log

--- resources/views/activity/types/created_squad.blade.php
+++ resources/views/activity/types/created_squad.blade.php
@@ -1,6 +1,6 @@
 <td>
     <i class="fa fa-cubes text-success"></i>
-    {{ $event->user->name }} created squad {{ $event->subject->name or $event->subject->id }}
+    {{ $event->user->name or "Somebody" }} created squad {{ $event->subject->name or $event->subject->id }}
 </td>
 <td>
     <span class="pull-right text-muted">{{ $event->created_at->diffForHumans() }}</span>
