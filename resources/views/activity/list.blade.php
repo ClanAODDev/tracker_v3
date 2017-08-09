1555fc037967dc0696c1cd5bc96f028b55904c54
Chris Deaton <dcdeaton@ncsu.edu>
2017-08-09 15:56:43 -0400

    wip

--- resources/views/activity/list.blade.php
+++ resources/views/activity/list.blade.php
@@ -2,14 +2,8 @@
     <table class="table table-hover">
         @forelse ($activity as $event)
             <tr title="{{ $event->created_at }}">
-                @if (view()->exists("activity.types.{$event->name}"))
-                    @include ("activity.types.{$event->name}")
-                @else
-                    <td>
-                        <i class="fa fa-times-rectangle text-danger"></i>
-                        Missing activity template for {{ $event->name }}
-                    </td>
-                @endif
+                <td>{{ $event->user->name or $event->user_id }} {{ $event->name }}</td>
+                <td class="text-right">{{ $event->created_at->diffForHumans() }}</td>
             </tr>
         @empty
             <tr>
