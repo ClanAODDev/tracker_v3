523e9441be8683e0ae81f81acdf9e3e425ab25fb
Chris Deaton <dcdeaton@ncsu.edu>
2017-08-10 14:39:14 -0400

    force authentication on activity log

--- app/Http/Controllers/ActivitiesController.php
+++ app/Http/Controllers/ActivitiesController.php
@@ -7,6 +7,11 @@ use App\User;
 
 class ActivitiesController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth']);
+    }
+
     public function byUser(User $user)
     {
         $division = $user->member->division;
