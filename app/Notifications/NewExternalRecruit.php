ebfcca89d12bceb2d179f9914e6db1036a0787ff
Chris Deaton <dcdeaton@ncsu.edu>
2017-08-09 20:29:10 -0400

    slack alert notify instead of email

--- app/Notifications/NewExternalRecruit.php
+++ app/Notifications/NewExternalRecruit.php
@@ -40,21 +40,7 @@ class NewExternalRecruit extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
+        return ['slack'];
     }
 
     /**
