a087c75b3aeab89bf02919fcd414648459293394
Chris Deaton <dcdeaton@ncsu.edu>
2017-08-14 18:03:42 -0400

    wip

---
+++ resources/views/platoon/partials/members-table-toggle.blade.php
@@ -0,0 +1,9 @@
+<div class="text-center">
+    <div class="m-t-md btn-group">
+        <a class="toggle-vis btn btn-default" href="#" data-column="9">Handle</a><a
+                class="toggle-vis btn btn-default" href="#" data-column="3">Rank</a><a
+                class="toggle-vis btn btn-default" href="#" data-column="4">Join Date</a><a
+                class="toggle-vis btn btn-default" data-column="5">Forum Activity</a><a
+                class="toggle-vis btn btn-default" data-column="6">TS Activity</a>
+    </div>
+</div>
--- resources/views/squad/partials/squad-members.blade.php
+++ resources/views/squad/partials/squad-members.blade.php
@@ -1,8 +1,6 @@
 <div class='panel-body border-bottom'>
     <div id='playerFilter'></div>
-    <div class="m-t-md btn-group">
-        <a class="toggle-vis btn btn-default" href="#" data-column="9">Handle</a><a class="toggle-vis btn btn-default" href="#" data-column="3">Rank</a><a class="toggle-vis btn btn-default" href="#" data-column="4">Join Date</a><a class="toggle-vis btn btn-default" data-column="5">Forum Activity</a><a class="toggle-vis btn btn-default" data-column="6">TS Activity</a>
-    </div>
+    @include ('platoon.partials.members-table-toggle')
 </div>
 <div class="table-responsive">
 
