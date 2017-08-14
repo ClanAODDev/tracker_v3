1f0cfb44a8521cb288d323c8632faabe60596cbb
Chris Deaton <dcdeaton@ncsu.edu>
2017-08-14 18:16:58 -0400

    wip

--- resources/views/platoon/partials/squads.blade.php
+++ resources/views/platoon/partials/squads.blade.php
@@ -1,10 +1,17 @@
-<div class="panel panel-filled text-center">
+<div class="panel panel-filled">
     <div class="panel-heading">Squads</div>
-    <div class="panel-body btn-group-vertical">
+    <div class="panel-body btn-group-vertical center-block">
         @foreach ($platoon->squads as $squad)
             <a class="btn btn-default"
                href="{{ route('squad.show', [$division->abbreviation, $platoon, $squad]) }}">
-                {{ $squad->name }}
+                {{ $squad->name }} - #{{ $squad->id }}<br />
+                <small class="slight">
+                    @if ($squad->leader)
+                        {{ $squad->leader->present()->rankName }}
+                    @else
+                        TBA
+                    @endif
+                </small>
             </a>
         @endforeach
     </div>
