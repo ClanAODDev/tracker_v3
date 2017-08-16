0d0c5251ad326771d5c3afaaf733457381b7e532
Chris Deaton <dcdeaton@ncsu.edu>
2017-08-16 01:29:49 -0400

    add mass pm to inactives

--- resources/views/division/inactive-members.blade.php
+++ resources/views/division/inactive-members.blade.php
@@ -28,25 +28,6 @@
 
         <hr />
 
-        <div class="panel">
-            <div class="panel-body">
-                <strong class="c-white">
-                    @if ($queryingTsInactives)
-                        Teamspeak Inactivity
-                    @else
-                        Forum Inactivity
-                    @endif
-                </strong>
-                <span class="pull-right">
-                    Filter by:
-                    <a href="{{ route('division.inactive-members', $division->abbreviation) }}"
-                       class="btn btn-info {{ set_active(['divisions/*/inactive-members/*', 'divisions/*/inactive-members']) }}">By Forum Activity</a>
-                    <a href="{{ route('division.inactive-members-ts', $division->abbreviation) }}"
-                       class="btn btn-info {{ set_active(['divisions/*/inactive-members-ts/*', 'divisions/*/inactive-members-ts']) }}">By TS Activity</a>
-                </span>
-            </div>
-        </div>
-
         <div class="tabs-container">
             <ul class="nav nav-tabs">
                 <li class="active">
@@ -55,7 +36,22 @@
                 <li>
                     <a data-toggle="tab" href="#flagged"
                        aria-expanded="false">Flagged <span class="badge">{{ count($flaggedMembers) }}</span>
-                    </a></li>
+                    </a>
+                </li>
+                <li class="text-center">
+                </li>
+                <li class="pull-right">
+                    <span class="btn-group-sm btn-group">
+                    <a href="{{ route('division.inactive-members', $division->abbreviation) }}"
+                       class="btn btn-default {{ set_active(['divisions/*/inactive-members/*', 'divisions/*/inactive-members']) }}">Filter By Forum Activity</a>
+                    <a href="{{ route('division.inactive-members-ts', $division->abbreviation) }}"
+                       class="btn btn-default {{ set_active(['divisions/*/inactive-members-ts/*', 'divisions/*/inactive-members-ts']) }}">Filter By TS Activity</a>
+                    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal">
+                        <i class="fa fa-bullhorn text-accent"></i> <span
+                                class="hidden-xs hidden-sm">Mass PM Inactives</span>
+                    </button>
+                        </span>
+                </li>
             </ul>
             <div class="tab-content">
                 <div id="inactive" class="tab-pane active">
@@ -74,4 +70,21 @@
 
         @include('division.partials.inactivity-log')
     </div>
+
+    @component('application.components.modal', ['showSaveButton' => false])
+        @slot('title')
+            Mass Forum PM ({{ count($inactiveMembers) }})
+        @endslot
+        @slot('body')
+            <p>The Clan AOD forums has a maximum number of 20 recipients per PM. To assist with this limitation, members have been chunked into groups for your convenience.</p>
+            <p class="m-t-md">
+                @foreach ($inactiveMembers->chunk(20) as $chunk)
+                    <a href="{{ doForumFunction($chunk->pluck('clan_id')->toArray(), 'pm') }}"
+                       target="_blank" class="btn btn-default">
+                        <i class="fa fa-link text-accent"></i> Group {{ $loop->iteration }}
+                    </a>
+                @endforeach
+            </p>
+        @endslot
+    @endcomponent
 @stop
