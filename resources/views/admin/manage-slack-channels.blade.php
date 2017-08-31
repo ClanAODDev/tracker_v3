70f18e97cdbfe7b8db2e90b8bf4cf9737932a1b4
Chris Deaton <dcdeaton@ncsu.edu>
2017-08-31 09:19:22 -0400

    replace ampersands with blank

--- resources/views/admin/manage-slack-channels.blade.php
+++ resources/views/admin/manage-slack-channels.blade.php
@@ -67,12 +67,12 @@
 
       function slugify (text) {
         return text.toString().toLowerCase().trim()
-          .normalize('NFD') 				 // separate accent from letter
-          .replace(/[\u0300-\u036f]/g, '') // remove all separated accents
-          .replace(/\s+/g, '-')            // replace spaces with -
-          .replace(/&/g, '-and-')          // replace & with 'and'
-          .replace(/[^\w\-]+/g, '')        // remove all non-word chars
-          .replace(/\-\-+/g, '-');          // replace multiple '-' with single '-'
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')
+          .replace(/\s+/g, '-')
+          .replace(/&/g, '')
+          .replace(/[^\w\-]+/g, '')
+          .replace(/\-\-+/g, '-');
       }
     </script>
 
