094b64a922f15e3e3b4a44bc09abf08236d16e79
Chris Deaton <dcdeaton@ncsu.edu>
2017-09-05 10:50:05 -0400

    adjust division census to match clan stats

--- resources/views/division/forms/census.blade.php
+++ resources/views/division/forms/census.blade.php
@@ -18,18 +18,18 @@
             <tr>
                 <td>{{ $census->created_at->format('m/d/Y') }}</td>
                 <td class="text-center">{{ $census->count }}</td>
-                <td class="text-center">
-                    {{ $census->weekly_active_count }}
+                <td class="text-center slight">
+                    {{ number_format($census->weekly_active_count / $census->count * 100, 1) }}%
                     <span class="census-pie"
                           data-colors="{{ json_encode(['#404652', '#1bbf89']) }}"
                           data-counts="{{ json_encode([$popMinus, $census->weekly_active_count]) }}">
                     </span>
                 </td>
-                <td class="text-center">
-                    {{ $census->weekly_ts_count }}
+                <td class="text-center slight">
+                    {{ number_format($census->weekly_ts_active / $census->count * 100, 1) }}%
                     <span class="census-pie"
-                          data-colors="{{ json_encode(['#404652', '#f7af3e']) }}"
-                          data-counts="{{ json_encode([$popMinus, $census->weekly_ts_count ]) }}">
+                          data-colors="{{ json_encode(['#404652', '#56C0E0']) }}"
+                          data-counts="{{ json_encode([$popMinus, $census->weekly_ts_active]) }}">
                     </span>
                 </td>
                 {{--<td>{{ $census->notes }}</td>--}}
