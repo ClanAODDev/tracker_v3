c62f2d7827ed8910d48a669bc6638c986b17939a
Chris Deaton <dcdeaton@ncsu.edu>
2017-09-17 22:55:58 -0400

    add default select option to promo report fixes #87

--- resources/views/division/partials/filter-promotions.blade.php
+++ resources/views/division/partials/filter-promotions.blade.php
@@ -3,6 +3,7 @@
         <label for="promotion-periods">Filter by promotion period</label>
         <select name="promotion-periods" id="promotion-periods" class="form-control"
                 onChange="top.location.href=this.options[this.selectedIndex].value;">
+            <option value="" disabled selected>Select a period</option>
             @foreach ($promotionPeriods as $period)
                 <option value="{{ route('division.promotions', [$division->abbreviation, $period->month, $period->year]) }}">
                     {{ $period->month }} {{ $period->year }}
