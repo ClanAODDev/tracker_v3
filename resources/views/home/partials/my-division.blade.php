ddf97b7b5aaa86d84d2d22840fdc1144038b6314
Chris Deaton <dcdeaton@ncsu.edu>
2017-10-01 10:16:09 -0400

    force cache bust of headers

--- resources/views/home/partials/my-division.blade.php
+++ resources/views/home/partials/my-division.blade.php
@@ -1,5 +1,5 @@
 <div class="panel panel-c-accent panel-filled division-header"
-     style="background-image: url({{ asset('images/headers/' . $myDivision->abbreviation . ".png") }})">
+     style="background-image: url({{ asset('images/headers/' . $myDivision->abbreviation . ".png?") }})">
     <div class="panel-heading">
 
         <style>
