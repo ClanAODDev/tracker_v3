93948d2dded3a1cef280687c5b6f53119b054ba3
Chris Deaton <dcdeaton@ncsu.edu>
2017-10-08 20:53:06 -0400

    provide default squad names

--- resources/views/platoon/partials/squads.blade.php
+++ resources/views/platoon/partials/squads.blade.php
@@ -2,7 +2,7 @@
     @foreach ($platoon->squads as $squad)
         <a class="list-group-item"
            href="{{ route('squad.show', [$division->abbreviation, $platoon, $squad]) }}">
-            {{ str_limit($squad->name, 23) }}<br />
+            {{ str_limit($squad->name or "Squad" . $loop->iteration, 23) }}<br />
             <span class="slight text-muted">
                 @if ($squad->leader)
                     {{ $squad->leader->present()->rankName }}
