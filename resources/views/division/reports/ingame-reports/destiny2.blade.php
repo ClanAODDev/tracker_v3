74fcff1200833ddc724aa148fe906f59534b0630
Chris Deaton <dcdeaton@ncsu.edu>
2017-11-11 16:54:51 -0500

    support tabs

--- resources/views/division/reports/ingame-reports/destiny2.blade.php
+++ resources/views/division/reports/ingame-reports/destiny2.blade.php
@@ -1,45 +1,54 @@
 <h4>Ingame Clan Members</h4>
 <hr />
 
-@foreach ($data as $clan)
+<!-- Nav tabs -->
+<ul class="nav nav-tabs" role="tablist">
+    @foreach ($data as $clan)
+        <li role="presentation">
+            <a href="#clan-{{ $clan['clan-info']->detail->groupId }}" aria-controls="home" role="tab"
+               data-toggle="tab">{{ $clan['clan-info']->detail->motto }} ({{ count($clan['clan-members']) }})</a>
+        </li>
+    @endforeach
+</ul>
 
-    <h4>{{ $clan['clan-info']->detail->name }}
-        <small>{{ $clan['clan-info']->detail->motto }}</small>
-        <span class="pull-right">{{ count($clan['clan-members']) }} Members</span>
-    </h4>
-    <div class="panel panel-filled">
-        <table class="table table-hover basic-datatable">
-            <thead>
-            <tr>
-                <th>Ingame Name</th>
-                <th>Bungie Id</th>
-                <th>Online</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach ($clan['clan-members'] as $member)
-                <tr>
-                    <td>{{ $member->destinyUserInfo->displayName }}</td>
-                    <td>
-                        @if (isset($member->bungieNetUserInfo->membershipId))
-                            <a href="https://www.bungie.net/en/Profile/{{ $member->bungieNetUserInfo->membershipId }}">
-                                <code>{{ $member->bungieNetUserInfo->membershipId }}</code>
-                            </a>
-                        @else
-                            <span class='text-danger'>No Bungie Account</span>
-                        @endif
-                    </td>
-                    <td>
-                        @if ($member->isOnline)
-                            <i class="fa fa-check text-success"></i>
-                        @endif
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-    </div>
-
-    <hr>
-
-@endforeach
+<!-- Tab panes -->
+<div class="tab-content">
+    @foreach ($data as $clan)
+        <div role="tabpanel" class="tab-pane" id="clan-{{ $clan['clan-info']->detail->groupId }}">
+            <div class="panel panel-filled">
+                <div class="panel-body">
+                    <table class="table table-hover adv-datatable">
+                        <thead>
+                        <tr>
+                            <th>Ingame Name</th>
+                            <th>Bungie Id</th>
+                            <th>Online</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($clan['clan-members'] as $member)
+                            <tr>
+                                <td>{{ $member->destinyUserInfo->displayName }}</td>
+                                <td>
+                                    @if (isset($member->bungieNetUserInfo->membershipId))
+                                        <a href="https://www.bungie.net/en/Profile/{{ $member->bungieNetUserInfo->membershipId }}">
+                                            <code>{{ $member->bungieNetUserInfo->membershipId }}</code>
+                                        </a>
+                                    @else
+                                        <span class='text-danger'>No Bungie Account</span>
+                                    @endif
+                                </td>
+                                <td>
+                                    @if ($member->isOnline)
+                                        <i class="fa fa-check text-success"></i>
+                                    @endif
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
