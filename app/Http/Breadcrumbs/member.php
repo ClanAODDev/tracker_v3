d4abf57fece272595582b001c465b3cd77cbd1a1
Chris Deaton <dcdeaton@ncsu.edu>
2018-02-26 18:15:28 -0500

    only push navigation items for active members

--- app/Http/Breadcrumbs/member.php
+++ app/Http/Breadcrumbs/member.php
@@ -5,16 +5,19 @@ Breadcrumbs::register('member', function ($breadcrumbs, $member, $division) {
 
     if ($division) {
         $breadcrumbs->push($division->name, route('division', $division->abbreviation));
-    }
 
-    if ($member->platoon_id !== 0 && $member->platoon) {
-        $breadcrumbs->push($member->platoon->name, route('platoon', [$division->abbreviation, $member->platoon->id]));
-    }
 
-    if ($member->squad_id !== 0 && $member->squad) {
-        $breadcrumbs->push($member->squad->name ?: "Untitled",
-            route('squad.show', [$division->abbreviation, $member->platoon->id, $member->squad])
-        );
+        if ($member->platoon_id !== 0 && $member->platoon) {
+            $breadcrumbs->push($member->platoon->name,
+                route('platoon', [$division->abbreviation, $member->platoon->id]));
+        }
+
+        if ($member->squad_id !== 0 && $member->squad) {
+            $breadcrumbs->push($member->squad->name ?: "Untitled",
+                route('squad.show', [$division->abbreviation, $member->platoon->id, $member->squad])
+            );
+        }
+
     }
 
     $breadcrumbs->push('View profile');
