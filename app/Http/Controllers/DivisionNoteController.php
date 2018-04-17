991550b617652e446c5a39b3556350cc09ea7629
Chris Deaton <dcdeaton@ncsu.edu>
2018-04-17 09:05:26 -0400

    add auth middleware to division notes

--- app/Http/Controllers/DivisionNoteController.php
+++ app/Http/Controllers/DivisionNoteController.php
@@ -8,6 +8,15 @@ use Illuminate\Http\Request;
 
 class DivisionNoteController extends Controller
 {
+
+    /**
+     * DivisionNoteController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
