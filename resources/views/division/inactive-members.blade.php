bd6ec8afe1a92fd860f9b996c6ff72253c8af62b
Chris Deaton <dcdeaton@ncsu.edu>
2018-08-13 07:50:22 -0400

    update verbiage on inactive page

--- resources/views/division/inactive-members.blade.php
+++ resources/views/division/inactive-members.blade.php
@@ -19,10 +19,11 @@
 
         {!! Breadcrumbs::render('inactive-members', $division) !!}
 
-        <p>Members listed here have activity that has reached or exceeded the number of days defined by the division leadership. Use this page to attempt to communicate with inactiveMembers members, and also to process their removal from the clan. Members who have an
-            <strong>active leave of absence</strong> are <strong>not</strong> listed here.</p>
+        <p>Members listed here have activity that has reached or exceeded the number of days defined by the division leadership. Use this page to attempt to communicate with inactive members, and also to process their removal from the clan. Members who have an active leave of absence are omitted.</p>
 
-        <p>{{ $division->name }} division inactivity set to
+        <p class="text-info">Members should be PMed before they are flagged for removal</p>
+
+        <p><strong>{{ $division->name }}</strong> division inactivity set to
             <code>{{ $division->settings()->inactivity_days }} days</code>
         </p>
 
