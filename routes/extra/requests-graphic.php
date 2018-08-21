7c4c026f63a22825b9dd01f8b40966d1d6ad6def
Chris Deaton <dcdeaton@ncsu.edu>
2018-08-21 12:14:25 -0400

    smaller request count image
    - also right align the count

--- routes/extra/requests-graphic.php
+++ routes/extra/requests-graphic.php
@@ -4,18 +4,18 @@ Route::get('requests-count.png', function () {
     header('Content-Type: image/png');
     date_default_timezone_set('America/New_York');
 
-    $im = imagecreatetruecolor(960, 330);
-    $orange = imagecolorallocate($im, 255, 108, 0);
-    $im = imagecreatefromjpeg(asset('images/pending-requests.jpg'));
-
     $tinyfont = public_path('fonts/copy0855.ttf');
     $tinyboldfont = public_path('fonts/copy0866.ttf');
     $bigfont = public_path('fonts/din-black.otf');
+    $im = imagecreatetruecolor(960, 330);
+    $orange = imagecolorallocate($im, 255, 108, 0);
+    $im = imagecreatefromjpeg(asset('images/dynamic-images/bgs/pending-requests.jpg'));
+    $requestsCount = \App\MemberRequest::pending()->count();
+    $dimensions = imagettfbbox(20, 0, $bigfont, $requestsCount);
+    $textWidth = abs($dimensions[4] - $dimensions[0]);
+    $x = imagesx($im) - $textWidth;
 
-    $requests = \App\MemberRequest::pending();
-
-    imagettftext($im, 30, 0, 230, 40, $orange, $bigfont, $requests->count());
-
+    imagettftext($im, 20, 0, $x-10, 25, $orange, $bigfont, $requestsCount);
     imagepng($im);
     imagedestroy($im);
 });
