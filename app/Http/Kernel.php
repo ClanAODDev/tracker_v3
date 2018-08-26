c8563b82f528a17cc2ebe73a725e8b250eef9503
Laravel Shift <shift@laravelshift.com>
2018-08-26 19:22:22 +0000

    Shift Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel
         'forumAuth' => \App\Http\Middleware\ClanForumAuthentication::class,
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
