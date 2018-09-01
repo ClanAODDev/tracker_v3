5046c6775c5ca419e557e06e2b4260ee5cc26ef5
Chris Deaton <dcdeaton@ncsu.edu>
2018-09-01 16:14:26 -0400

    fix member id error appearance

--- resources/assets/js/components/recruit/StepOne.vue
+++ resources/assets/js/components/recruit/StepOne.vue
@@ -46,7 +46,7 @@
                 <div class="row">
 
                     <div class="col-md-4 form-group"
-                         :class="{'input': true, 'has-warning': errors.has('member_id') }">
+                         :class="{'input': true, 'has-warning': !store.validMemberId }">
                         <label for="member_id">Forum Member Id</label>
                         <input type="number" class="form-control" name="member_id"
                                v-model="store.member_id"
