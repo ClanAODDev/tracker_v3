d7a12eb10480ffc9d9baa4dcf54e2884dff87dd0
Chris Deaton <dcdeaton@ncsu.edu>
2018-09-09 19:32:01 -0400

    hide count if none

--- routes/extra/requests-graphic.php
+++ routes/extra/requests-graphic.php
@@ -9,7 +9,7 @@ Route::get('requests-count.png', function () {
     $bigfont = public_path('fonts/din-black.otf');
     $im = imagecreatefromjpeg(asset('images/dynamic-images/bgs/pending-requests.jpg'));
     $orange = imagecolorallocate($im, 255, 108, 0);
-    $red = imagecolorallocate($im, 153,26,34);
+    $red = imagecolorallocate($im, 153, 26, 34);
     $requestsCount = \App\MemberRequest::pending()->count();
     $errors = \App\MemberRequest::errors()->count();
     $dimensions = imagettfbbox(20, 0, $bigfont, $requestsCount);
@@ -17,7 +17,11 @@ Route::get('requests-count.png', function () {
     $x = imagesx($im) - $textWidth;
 #991A22
     imagettftext($im, 20, 0, $x - 10, 25, $orange, $bigfont, $requestsCount);
-    imagettftext($im, 6, 0, $x - 50, 20, $red, $tinyfont, "({$errors} ERR)");
+
+    if ($errors > 0) {
+        imagettftext($im, 6, 0, $x - 50, 20, $red, $tinyfont, "({$errors} ERR)");
+    }
+    
     imagepng($im);
     imagedestroy($im);
 });
