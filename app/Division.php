a53d51677089369c7c0bb3b87b91695e6505be18
Chris Deaton <dcdeaton@ncsu.edu>
2018-10-29 11:41:41 -0400

    configure ssl for curl
    don't verify ssl certs for thread check

--- app/Division.php
+++ app/Division.php
@@ -142,9 +142,13 @@ class Division extends Model
     public static function threadCheck($string, $thread)
     {
         $ch = curl_init();
+
         curl_setopt($ch, CURLOPT_URL, $thread . "&goto=newpost");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
         $getPosts = curl_exec($ch);
         $countPosts = stripos($getPosts, $string);
 
