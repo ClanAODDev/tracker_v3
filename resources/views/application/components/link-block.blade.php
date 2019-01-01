0a0a2e1c906e4d0176a009bb2a8d9fd435baf70f
Chris Deaton <dcdeaton@ncsu.edu>
2019-01-01 14:11:44 -0500

    linkify trainer block

--- resources/views/application/components/link-block.blade.php
+++ resources/views/application/components/link-block.blade.php
@@ -1,6 +1,7 @@
 <div class="col-md-4 col-xs-12">
-    <a href="{{ $link }}" class="panel panel-filled {{ $color or null }}">
+    <a href="{{ $link }}" class="panel panel-filled {{ $color or null }} panel-c-success">
         <div class="panel-body">
+            <span class="pull-right"><i class="fa fa-link"></i></span>
             <h4 class="m-b-sm">
                 {{ $data }}
             </h4>
