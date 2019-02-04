fe18de258492ec3c124d6fb23f343fde8b725732
Chris Deaton <dcdeaton@ncsu.edu>
2019-02-04 15:25:44 -0500

    prevent double-quotes in recruiting tasks

--- resources/views/division/partials/recruitingTasks.blade.php
+++ resources/views/division/partials/recruitingTasks.blade.php
@@ -7,6 +7,8 @@
                 <i class="fas fa-grip-vertical input-group-addon text-muted" style="cursor: move"></i>
                 <input type="text" name="tasks[{{ $loop->index }}][task_description]"
                        class="form-control" placeholder="Task description"
+                       title="Do not use double-quotes in task"
+                       pattern="[^\x22]*$"
                        value="{{ $task['task_description'] }}" required />
             </div>
 
