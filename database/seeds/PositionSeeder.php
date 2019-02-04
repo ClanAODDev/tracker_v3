8898d0f9060e8894f5c2ff6a457f78535ef5fee2
Chris Deaton <dcdeaton@ncsu.edu>
2019-02-04 10:50:37 -0500

    update position seeder

--- database/seeds/PositionSeeder.php
+++ database/seeds/PositionSeeder.php
@@ -14,12 +14,12 @@ class PositionSeeder extends Seeder
         DB::table('positions')->insert(
             [
                 ['name' => 'Member', 'icon' => '', 'class' => 'text-default', 'order' => 0],
-                ['name' => 'Squad Leader', 'icon' => 'fa fa-shield', 'class' => 'text-info', 'order' => 0],
-                ['name' => 'Platoon Leader', 'icon' => 'fa fa-circle', 'class' => 'text-warning', 'order' => 0],
+                ['name' => 'Squad Leader', 'icon' => 'fas fa-shield-alt', 'class' => 'text-info', 'order' => 0],
+                ['name' => 'Platoon Leader', 'icon' => 'fas fa-dot-circle', 'class' => 'text-warning', 'order' => 0],
                 ['name' => 'General Sergeant', 'icon' => 'fa', 'class' => 'text-default', 'order' => 0],
-                ['name' => 'Executive Officer', 'icon' => 'fa fa-dot-circle-o', 'class' => 'text-danger', 'order' => 0],
-                ['name' => 'Commanding Officer', 'icon' => 'fa fa-circle', 'class' => 'text-danger', 'order' => 0],
-                ['name' => 'Clan Admin', 'icon' => 'fa fa-square', 'class' => 'text-danger', 'order' => 0],
+                ['name' => 'Executive Officer', 'icon' => 'fas fa-circle-notch', 'class' => 'text-danger', 'order' => 0],
+                ['name' => 'Commanding Officer', 'icon' => 'fas fa-circle', 'class' => 'text-danger', 'order' => 0],
+                ['name' => 'Clan Admin', 'icon' => 'fas fa-square', 'class' => 'text-danger', 'order' => 0],
             ]
         );
     }
