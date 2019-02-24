41bf625ad9fdb4eb2c01605c3c557c1e289e48ec
Chris Deaton <dcdeaton@ncsu.edu>
2019-02-24 12:32:13 -0500

    add discord information to recruiting

--- resources/assets/js/components/recruit/StepOne.vue
+++ resources/assets/js/components/recruit/StepOne.vue
@@ -9,6 +9,7 @@
         <p>
             <a href="https://www.clanaod.net/forums/showthread.php?t=97502" target="_blank"
                class="btn btn-default">Teamspeak Information</a>
+            <a href="https://www.clanaod.net/forums/showthread.php?t=208233">Discord Information</a>
             <a class="btn btn-default m-l-sm" target="_blank"
                href="https://www.clanaod.net/forums/memberlist.php?order=desc&sort=joindate&pp=30">New Forum Members</a>
         </p>
