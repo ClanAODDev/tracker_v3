209178bd6d33d9b2aabdccd058b53ea9fcf4479c
Chris Deaton <dcdeaton@ncsu.edu>
2019-06-07 16:19:04 -0400

    icon update

--- resources/views/member/edit-member.blade.php
+++ resources/views/member/edit-member.blade.php
@@ -6,7 +6,7 @@
             @if ($division)
                 <img src="{{ getDivisionIconPath($division->abbreviation) }}" />
             @else
-                <img src="{{ asset('images/logo_v2.svg') }}" class="division-icon-large" />
+                <img class="division-icon-large" src="{{ asset('images/logo_v2.svg') }}" />
             @endif
         @endslot
         @slot ('heading')
