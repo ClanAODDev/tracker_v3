ef5c71e1f38dd605425bb535f50f749ee49fff2c
Chris Deaton <dcdeaton@ncsu.edu>
2019-06-07 16:19:04 -0400

    icon update

--- resources/views/application/components/division-heading.blade.php
+++ resources/views/application/components/division-heading.blade.php
@@ -1,7 +1,6 @@
 <div class="division-header">
     <div class="header-icon">
         {{ $icon }}
-        asdf
     </div>
     <div class="header-title">
         <h3 class="m-b-xs text-uppercase">
