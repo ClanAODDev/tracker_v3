27dbccd7a78ac19f2cb7893cac766182383df957
Chris Deaton <dcdeaton@ncsu.edu>
2019-07-07 08:48:13 -0400

    implement name validation

--- resources/assets/js/components/recruit/store.js
+++ resources/assets/js/components/recruit/store.js
@@ -34,24 +34,25 @@ store.forum_name = '';
 store.platoon = '';
 store.squad = '';
 store.validMemberId = false;
+store.validMemberName = false;
 
 // division data
 store.handleName = '';
 store.division = {
-  abbreviation: '',
-  settings: [],
-  platoons: [],
-  squads: [],
-  threads: [],
-  tasks: []
+    abbreviation: '',
+    settings: [],
+    platoons: [],
+    squads: [],
+    threads: [],
+    tasks: []
 };
 
 // locality data
 store.locality = {
-  platoon: 'Platoon',
-  platoons: 'Platoons',
-  squad: 'Squad',
-  squads: 'Squads',
+    platoon: "Platoon",
+    platoons: "Platoons",
+    squad: "Squad",
+    squads: "Squads",
 };
 
 /**
@@ -60,14 +61,14 @@ store.locality = {
  * @param division
  */
 store.getPlatoons = (division) => {
-  axios.get(store.base_url + '/division-platoons/' + division)
-    .then(function (response) {
-      store.division.platoons = response.data.data.platoons;
-      store.division.settings = response.data.data.settings;
-    })
-    .catch(function (error) {
-      toastr.error(error, 'Something went wrong while fetching division platoons');
-    });
+    axios.get(store.base_url + '/division-platoons/' + division)
+        .then(function (response) {
+            store.division.platoons = response.data.data.platoons;
+            store.division.settings = response.data.data.settings;
+        })
+        .catch(function (error) {
+            toastr.error(error, 'Something went wrong while fetching division platoons');
+        });
 };
 
 /**
@@ -76,13 +77,13 @@ store.getPlatoons = (division) => {
  * @param threads
  */
 store.checkIfIncomplete = function (threads) {
-  threads.forEach(function (thread) {
-    if (!thread.status) {
-      store.threadsIncomplete = true;
-      return;
-    }
-    store.threadsIncomplete = false;
-  });
+    threads.forEach(function (thread) {
+        if (!thread.status) {
+            store.threadsIncomplete = true;
+            return;
+        }
+        store.threadsIncomplete = false;
+    });
 };
 
 /**
@@ -91,18 +92,18 @@ store.checkIfIncomplete = function (threads) {
  * @param division
  */
 store.getDivisionThreads = (division) => {
-  store.loadingThreads = true;
-  axios.post(store.base_url + '/search-division-threads', {
-    division: division,
-    string: store.member_id,
-    isTesting: store.inDemoMode,
-  }).then(function (response) {
-    store.loadingThreads = false;
-    store.division.threads = response.data;
-    store.checkIfIncomplete(store.division.threads);
-  }).catch(function (error) {
-    toastr.error(error, 'Something went wrong while fetching division threads');
-  });
+    store.loadingThreads = true;
+    axios.post(store.base_url + '/search-division-threads', {
+        division: division,
+        string: store.member_id,
+        isTesting: store.inDemoMode,
+    }).then(function (response) {
+        store.loadingThreads = false;
+        store.division.threads = response.data;
+        store.checkIfIncomplete(store.division.threads);
+    }).catch(function (error) {
+        toastr.error(error, 'Something went wrong while fetching division threads');
+    });
 };
 
 /**
@@ -111,17 +112,17 @@ store.getDivisionThreads = (division) => {
  * @param platoon
  */
 store.getPlatoonSquads = (platoon) => {
-  if (store.platoon == '') {
-    return;
-  }
+    if (store.platoon == '') {
+        return;
+    }
 
-  axios.post(store.base_url + '/platoon-squads/', {
-    platoon: platoon
-  }).then(function (response) {
-    store.division.squads = response.data;
-  }).catch(function (error) {
-    toastr.error(error, 'Something went wrong while fetching platoon squads');
-  });
+    axios.post(store.base_url + '/platoon-squads/', {
+        platoon: platoon
+    }).then(function (response) {
+        store.division.squads = response.data;
+    }).catch(function (error) {
+        toastr.error(error, 'Something went wrong while fetching platoon squads');
+    });
 };
 
 /**
@@ -130,42 +131,42 @@ store.getPlatoonSquads = (platoon) => {
  * @param division
  */
 store.getTasks = (division) => {
-  axios.post(store.base_url + '/division-tasks/', {
-    division: division
-  }).then(function (response) {
-    store.division.tasks = response.data;
-  }).catch(function (error) {
-    toastr.error(error, 'Division tasks could not be retrieved');
-  });
+    axios.post(store.base_url + '/division-tasks/', {
+        division: division
+    }).then(function (response) {
+        store.division.tasks = response.data;
+    }).catch(function (error) {
+        toastr.error(error, 'Division tasks could not be retrieved');
+    });
 };
 
 /**
  * pushes a request to create a new member
  */
 store.createMember = () => {
-  if (!store.inDemoMode) {
-    axios.post(store.base_url + '/add-member/', {
-      division: store.division.abbreviation,
-      member_id: store.member_id,
-      forum_name: store.forum_name,
-      ingame_name: store.ingame_name,
-      platoon: store.platoon,
-      rank: store.rank,
-      squad: store.squad
-    }).then(function (response) {
-      toastr.success('Your recruit has been added to the tracker and a member status has been submitted!');
-    }).catch(function (error) {
-      toastr.error(error, 'The creation process could not be completed...');
-    });
-  }
+    if (!store.inDemoMode) {
+        axios.post(store.base_url + '/add-member/', {
+            division: store.division.abbreviation,
+            member_id: store.member_id,
+            forum_name: store.forum_name,
+            ingame_name: store.ingame_name,
+            platoon: store.platoon,
+            rank: store.rank,
+            squad: store.squad
+        }).then(function (response) {
+            toastr.success('Your recruit has been added to the tracker and a member status has been submitted!');
+        }).catch(function (error) {
+            toastr.error(error, 'The creation process could not be completed...');
+        });
+    }
 };
 
 window.onbeforeunload = function (e) {
-  if (store.currentStep !== 'step-four') {
-    let dialogText = 'You are about to leave the recruiting process. Are you sure?';
-    e.returnValue = dialogText;
-    return dialogText;
-  }
+    if (store.currentStep !== 'step-four') {
+        let dialogText = 'You are about to leave the recruiting process. Are you sure?';
+        e.returnValue = dialogText;
+        return dialogText;
+    }
 };
 
 /**
