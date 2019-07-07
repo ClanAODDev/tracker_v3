668db22b29b6fdc27028a10c3dd97e394114f2fb
Chris Deaton <flashadvocate@gmail.com>
2019-07-07 11:31:50 -0400

    add maintenance page

---
+++ resources/views/errors/418.blade.php
@@ -0,0 +1,29 @@
+@extends('application.base')
+
+@section('content')
+
+    <div class="container-center md animated fadeInDown">
+        @component('application.components.view-heading')
+            @slot('heading')
+                Maintenance
+            @endslot
+            @slot('subheading')
+                Application currently in maintenance mode
+            @endslot
+            @slot('icon')
+                <i class="pe page-header-icon pe-7s-moon"></i>
+            @endslot
+            @slot('currentPage')
+                v3
+            @endslot
+        @endcomponent
+
+        <div class="panel panel-filled">
+            <div class="panel-body">
+                The AOD Tracker is currently unavailable due to maintenance or updates being performed. We should be back shortly, though. Hang in there!
+            </div>
+        </div>
+    </div>
+
+@endsection
+
