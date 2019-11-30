869b7ac10f70dbe35e2797c4b5b8fefb70047f43
Chris Deaton <dcdeaton@ncsu.edu>
2019-11-29 22:48:29 -0500

    add dev favicon

--- resources/views/application/header.blade.php
+++ resources/views/application/header.blade.php
@@ -1,25 +1,30 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="UTF-8">
 
-<link href="{{ asset('favicon.ico?v=3') }}" type="image/x-icon" rel="Shortcut Icon" />
+@if(App::environment('develop'))
+    <link href="{{ asset('favicon_dev.ico?v=3.3') }}" type="image/x-icon" rel="Shortcut Icon"/>
+@else
+    <link href="{{ asset('favicon.ico?v=3.3') }}" type="image/x-icon" rel="Shortcut Icon" />
+@endif
 
 <!-- vendor styles -->
 <!-- @TODO: bundle CSS resources -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+      integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-<link rel="stylesheet" href="{{ asset('vendor/animate.css/animate.css') }}" />
-<link rel="stylesheet" href="{{ asset('vendor/toastr/toastr.min.css') }}" />
-<link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.css') }}" />
+<link rel="stylesheet" href="{{ asset('vendor/animate.css/animate.css') }}"/>
+<link rel="stylesheet" href="{{ asset('vendor/toastr/toastr.min.css') }}"/>
+<link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.css') }}"/>
 
 <!-- App styles -->
 <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('css/pe-icons/pe-icon-7-stroke.css') }}" />
-<link rel="stylesheet" href="{{ asset('css/pe-icons/helper.css') }}" />
-<link rel="stylesheet" href="{{ asset('css/stroke-icons/style.css') }}" />
-<link rel="stylesheet" href="{{ asset('vendor/datatables/datatables.min.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/pe-icons/pe-icon-7-stroke.css') }}"/>
+<link rel="stylesheet" href="{{ asset('css/pe-icons/helper.css') }}"/>
+<link rel="stylesheet" href="{{ asset('css/stroke-icons/style.css') }}"/>
+<link rel="stylesheet" href="{{ asset('vendor/datatables/datatables.min.css') }}"/>
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
 <script src="{{ asset('js/libs.js?v=2.0') }}"></script>
@@ -30,7 +35,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <script>
-  window.Laravel = <?php echo json_encode([
+    window.Laravel = <?php echo json_encode([
         'csrfToken' => csrf_token(),
         'appPath' => route('index'),
     ]); ?>
