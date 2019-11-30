5481c36c635aec588b90b1d8a895feddc90ad385
Chris Deaton <dcdeaton@ncsu.edu>
2019-11-29 22:10:44 -0500

    complete 5.7 upgrade

--- config/database.php
+++ config/database.php
@@ -82,6 +82,20 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'aod_forums' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('AOD_DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ]
+
     ],
 
     /*
