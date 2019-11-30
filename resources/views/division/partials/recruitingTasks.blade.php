10bef9d2e84c43a151b404cd1037aaea3095df76
Chris Deaton <dcdeaton@ncsu.edu>
2019-11-30 06:22:44 -0500

    icon fixes

--- resources/views/division/partials/recruitingTasks.blade.php
+++ resources/views/division/partials/recruitingTasks.blade.php
@@ -4,12 +4,14 @@
         <td class="col-xs-11">
 
             <div class="input-group">
-                <i class="fas fa-grip-vertical input-group-addon text-muted" style="cursor: move"></i>
+                <span class="input-group-addon  text-muted " style="cursor: move">
+                    <i class="fa fa-drag-handle"></i>
+                </span>
                 <input type="text" name="tasks[{{ $loop->index }}][task_description]"
                        class="form-control" placeholder="Task description"
                        title="Do not use double-quotes in task"
                        pattern="[^\x22]*$"
-                       value="{{ $task['task_description'] }}" required />
+                       value="{{ $task['task_description'] }}" required/>
             </div>
 
         </td>
