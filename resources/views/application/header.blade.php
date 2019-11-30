877ff76c0b0563728af31f28791626137fa043ac
Chris Deaton <dcdeaton@ncsu.edu>
2019-11-29 22:48:29 -0500

    add dev favicon

--- resources/views/application/header.blade.php
+++ resources/views/application/header.blade.php
@@ -10,7 +10,7 @@
 <!-- vendor styles -->
 <!-- @TODO: bundle CSS resources -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"/>
-{{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>--}}
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
       integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
