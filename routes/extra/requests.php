21c7f5541a2f1bebbe12f51ce32d8a7a3f6ad631
Chris Deaton <dcdeaton@ncsu.edu>
2020-03-15 10:06:30 -0400

    cleanup extras, cache bust

--- routes/extra/requests.php
+++ routes/extra/requests.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Mail;
 
 Route::get('requests-count.png', function () {
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
     header('Content-Type: image/png');
     date_default_timezone_set('America/New_York');
 
@@ -80,53 +83,4 @@ Route::get('tickets-count.png', function () {
 
     imagepng($im);
     imagedestroy($im);
-});
-
-/**
- * Vegas Attendee Opt-in
- */
-Route::middleware('auth')->group(function () {
-    Route::get('vegas2019', function () {
-        $optedIn = DB::table('opt_in')
-            ->where('member_id', auth()->user()->member->clan_id)
-            ->whereOptedOut(null)
-            ->exists();
-
-        return view('pages.vegas-opt-in', compact('optedIn'));
-    })->middleware('auth')->name('vegas-survey');
-
-    Route::post('vegas2019/opt-in', function () {
-        $user = auth()->user()->member;
-
-        if ($attendee = \App\VegasAttendee::whereMemberId($user->clan_id)->first()) {
-            $attendee->opted_out = null;
-            $attendee->save();
-        } else {
-            Mail::to(auth()->user())->send(new \App\Mail\VegasNotify());
-            VegasAttendee::optIn();
-        }
-
-        return redirect(route('vegas-survey'));
-    })->middleware('auth');
-
-    Route::delete('vegas2019/opt-out', function () {
-        $user = auth()->user()->member;
-
-        VegasAttendee::whereMemberId($user->clan_id)
-            ->first()
-            ->optOut();
-
-        return redirect(route('vegas-survey'));
-    });
-});
-
-Route::middleware(['auth', 'admin'])->get('/test-the-webhook', function () {
-    $division = App\Division::active()->first();
-    try {
-        $division->notify(new \App\Notifications\TestingWebhook($division));
-
-        return "Success!";
-    } catch (Exception $exception) {
-        return $exception;
-    }
 });
