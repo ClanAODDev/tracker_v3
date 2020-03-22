8168aade10c394af115c3afa4e56bacfc16a7067
Chris Deaton <dcdeaton@ncsu.edu>
2020-03-22 16:46:56 -0400

    implement banned role
    also log access changes

---
+++ app/Http/Middleware/IsBanned.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IsBanned
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->user()->role_id == 6) {
+            abort(403, "You are banned.");
+        }
+
+        return $next($request);
+    }
+}
