b5d6d1c4e00c43593984304ff8688b91e735499d
Chris Deaton <dcdeaton@ncsu.edu>
2020-03-28 12:43:28 -0400

    fix unk flaggers

--- resources/views/division/partials/inactive-activity-log-entry.blade.php
+++ resources/views/division/partials/inactive-activity-log-entry.blade.php
@@ -1,11 +1,11 @@
 <tr>
     <td>
         @if ($activity->name == 'unflagged_member')
-            <i class="fa fa-flag-o"></i> {{ $activity->user->name }} unflagged {{ $activity->subject->name }}
+            <i class="fa fa-flag-o"></i> {{ ($activity->user) ? $activity->user->name : 'Unk' }} unflagged {{ $activity->subject->name }}
         @elseif ($activity->name == 'flagged_member')
-            <i class="fa fa-flag text-warning"></i> {{ $activity->user->name }} flagged {{ $activity->subject->name }} for removal
+            <i class="fa fa-flag text-warning"></i> {{ ($activity->user) ? $activity->user->name : 'Unk' }} flagged {{ $activity->subject->name }} for removal
         @else
-            <i class="fa fa-trash text-danger"></i> {{ $activity->user->name }} removed {{ $activity->subject->name }}
+            <i class="fa fa-trash text-danger"></i> {{ ($activity->user) ? $activity->user->name : 'Unk' }} removed {{ $activity->subject->name }}
         @endif
         <span class="badge text-muted">{{ $activity->created_at->diffForHumans() }}</span>
     </td>
