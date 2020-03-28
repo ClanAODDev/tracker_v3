1688417d5717358f9b57f2c11005eac0048e7eca
Chris Deaton <dcdeaton@ncsu.edu>
2020-03-28 13:18:43 -0400

    fix note author unk

--- resources/views/member/partials/note.blade.php
+++ resources/views/member/partials/note.blade.php
@@ -14,21 +14,27 @@
             <div class="panel-footer">
                 <div class="slight">
 
-                    <a href="{{ route('member', $note->author->member->getUrlParams()) }}">{{ $note->author->name }}</a>
-                    &mdash;
-                    {{ $note->created_at->diffForHumans() }}
+                    @if ($note->author)
+                        <a href="{{ route('member', $note->author->member->getUrlParams()) }}">
+                            {{ $note->author->name }}
+                        </a>
+                    @else
+                        <span>Unk Author</span>
+                        @endif
+                        &mdash;
+                        {{ $note->created_at->diffForHumans() }}
 
-                    @if ($note->forum_thread_id)
-                        <a href="{{ doForumFunction([$note->forum_thread_id], 'showThread') }}"
-                           target="_blank" class="btn btn-default btn-xs m-l-sm">View Discussion</a>
-                    @endif
+                        @if ($note->forum_thread_id)
+                            <a href="{{ doForumFunction([$note->forum_thread_id], 'showThread') }}"
+                               target="_blank" class="btn btn-default btn-xs m-l-sm">View Discussion</a>
+                        @endif
 
-                    @if ($member->division)
-                        @can('edit', [$note, $member->clan_id])
-                            <a href="{{ route('editNote', [$member->clan_id, $note]) }}"
-                               class="btn btn-default btn-xs">Edit</a>
-                        @endcan
-                    @endif
+                        @if ($member->division)
+                            @can('edit', [$note, $member->clan_id])
+                                <a href="{{ route('editNote', [$member->clan_id, $note]) }}"
+                                   class="btn btn-default btn-xs">Edit</a>
+                            @endcan
+                        @endif
                 </div>
             </div>
         </div>
