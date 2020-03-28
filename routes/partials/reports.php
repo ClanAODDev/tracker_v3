bf0a479938af9d159a6e60d6bba2a0574c7abe06
Chris Deaton <dcdeaton@ncsu.edu>
2020-03-28 09:20:58 -0400

    consolidate routes

---
+++ routes/partials/reports.php
@@ -0,0 +1,21 @@
+<?php
+
+Route::group(['prefix' => 'reports'], function () {
+    Route::middleware('admin')->get('division-turnover', 'ReportsController@divisionTurnoverReport')
+        ->name('reports.division-turnover');
+
+    Route::middleware('admin')->get('division-roles', 'ReportsController@divisionUsersWithAccess')
+        ->name('reports.division-roles');
+
+    Route::get('no-discord', 'ReportsController@usersWithoutDiscordReport')
+        ->name('reports.discord');
+    Route::get(
+        'outstanding-inactives',
+        'ReportsController@outstandingMembersReport'
+    )->name('reports.outstanding-inactives');
+    Route::get('/clan-census', 'ReportsController@clanCensusReport')->name('reports.clan-census');
+    Route::get('/clan-ts-report', 'ReportsController@clanTsReport')->name('reports.clan-ts-report');
+
+    // other reporty things
+    Route::get('sergeants', 'MemberController@sergeants')->name('sergeants');
+});
