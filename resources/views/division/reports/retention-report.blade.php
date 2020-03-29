764d3a695b61c17e052de1e77ea7fd12b6aa8a7b
Chris Deaton <dcdeaton@ncsu.edu>
2020-03-28 21:00:01 -0400

    fix missing member record

--- resources/views/division/reports/retention-report.blade.php
+++ resources/views/division/reports/retention-report.blade.php
@@ -4,7 +4,7 @@
 
     @component ('application.components.division-heading')
         @slot ('icon')
-            <img src="{{ getDivisionIconPath($division->abbreviation) }}" class="division-icon-large" />
+            <img src="{{ getDivisionIconPath($division->abbreviation) }}" class="division-icon-large"/>
         @endslot
         @slot ('heading')
             Member Retention
@@ -44,7 +44,7 @@
             </div>
         </div>
 
-        <hr />
+        <hr/>
 
         <div class="row">
             <div class="col-md-6">
@@ -82,10 +82,15 @@
                         <span class="pull-right">{{ $totalRecruitCount }}</span>
                     </div>
                     @foreach ($members as $item)
+
                         <li class="list-group-item">
-                            <a href="{{ route('member', $item['member']->getUrlParams()) }}">
-                                {{ $item['member']->present()->rankName }}
-                            </a>
+                            @if ($item['member'] instanceof \App\Member)
+                                <a href="{{ route('member', $item['member']->getUrlParams()) }}">
+                                    {{ $item['member']->present()->rankName }}
+                                </a>
+                            @else
+                                Member id {{ $item['member']->id }}
+                            @endif
                             <span class="pull-right badge">{{ $item['recruits'] }}</span>
                         </li>
                     @endforeach
@@ -95,128 +100,134 @@
     </div>
 
     <script>
-      !function (r) {
-        function o (r, o, e, i) {
-          var s = 'categories' == o.xaxis.options.mode, n = 'categories' == o.yaxis.options.mode;
-          if (s || n) {
-            var a = i.format;
-            if (!a) {
-              var t = o;
-              if ((a = []).push({x: !0, number: !0, required: !0}), a.push({
-                y: !0,
-                number: !0,
-                required: !0
-              }), t.bars.show || t.lines.show && t.lines.fill) {
-                var u = !!(t.bars.show && t.bars.zero || t.lines.show && t.lines.zero);
-                a.push({
-                  y: !0,
-                  number: !0,
-                  required: !1,
-                  defaultValue: 0,
-                  autoscale: u
-                }), t.bars.horizontal && (delete a[a.length - 1].y, a[a.length - 1].x = !0);
-              }
-              i.format = a;
+        !function (r) {
+            function o(r, o, e, i) {
+                var s = 'categories' == o.xaxis.options.mode, n = 'categories' == o.yaxis.options.mode;
+                if (s || n) {
+                    var a = i.format;
+                    if (!a) {
+                        var t = o;
+                        if ((a = []).push({x: !0, number: !0, required: !0}), a.push({
+                            y: !0,
+                            number: !0,
+                            required: !0
+                        }), t.bars.show || t.lines.show && t.lines.fill) {
+                            var u = !!(t.bars.show && t.bars.zero || t.lines.show && t.lines.zero);
+                            a.push({
+                                y: !0,
+                                number: !0,
+                                required: !1,
+                                defaultValue: 0,
+                                autoscale: u
+                            }), t.bars.horizontal && (delete a[a.length - 1].y, a[a.length - 1].x = !0);
+                        }
+                        i.format = a;
+                    }
+                    for (var f = 0; f < a.length; ++f) a[f].x && s && (a[f].number = !1), a[f].y && n && (a[f].number = !1);
+                }
             }
-            for (var f = 0; f < a.length; ++f) a[f].x && s && (a[f].number = !1), a[f].y && n && (a[f].number = !1);
-          }
-        }
 
-        function e (r) {
-          var o = [];
-          for (var e in r.categories) {
-            var i = r.categories[e];
-            i >= r.min && i <= r.max && o.push([i, e]);
-          }
-          return o.sort(function (r, o) {return r[0] - o[0];}), o;
-        }
-
-        function i (o, i, s) {
-          if ('categories' == o[i].options.mode) {
-            if (!o[i].categories) {
-              var n = {}, a = o[i].options.categories || {};
-              if (r.isArray(a)) for (var t = 0; t < a.length; ++t) n[a[t]] = t; else for (var u in a) n[u] = a[u];
-              o[i].categories = n;
+            function e(r) {
+                var o = [];
+                for (var e in r.categories) {
+                    var i = r.categories[e];
+                    i >= r.min && i <= r.max && o.push([i, e]);
+                }
+                return o.sort(function (r, o) {
+                    return r[0] - o[0];
+                }), o;
             }
-            o[i].options.ticks || (o[i].options.ticks = e), function (r, o, e) {
-              for (var i = r.points, s = r.pointsize, n = r.format, a = o.charAt(0), t = function (r) {
-                var o = -1;
-                for (var e in r) r[e] > o && (o = r[e]);
-                return o + 1;
-              }(e), u = 0; u < i.length; u += s) if (null != i[u]) for (var f = 0; f < s; ++f) {
-                var c = i[u + f];
-                null != c && n[f][a] && (c in e || (e[c] = t, ++t), i[u + f] = e[c]);
-              }
-            }(s, i, o[i].categories);
-          }
-        }
 
-        function s (r, o, e) {i(o, 'xaxis', e), i(o, 'yaxis', e);}
+            function i(o, i, s) {
+                if ('categories' == o[i].options.mode) {
+                    if (!o[i].categories) {
+                        var n = {}, a = o[i].options.categories || {};
+                        if (r.isArray(a)) for (var t = 0; t < a.length; ++t) n[a[t]] = t; else for (var u in a) n[u] = a[u];
+                        o[i].categories = n;
+                    }
+                    o[i].options.ticks || (o[i].options.ticks = e), function (r, o, e) {
+                        for (var i = r.points, s = r.pointsize, n = r.format, a = o.charAt(0), t = function (r) {
+                            var o = -1;
+                            for (var e in r) r[e] > o && (o = r[e]);
+                            return o + 1;
+                        }(e), u = 0; u < i.length; u += s) if (null != i[u]) for (var f = 0; f < s; ++f) {
+                            var c = i[u + f];
+                            null != c && n[f][a] && (c in e || (e[c] = t, ++t), i[u + f] = e[c]);
+                        }
+                    }(s, i, o[i].categories);
+                }
+            }
 
-        r.plot.plugins.push({
-          init: function (r) {r.hooks.processRawData.push(o), r.hooks.processDatapoints.push(s);},
-          options: {xaxis: {categories: null}, yaxis: {categories: null}},
-          name: 'categories',
-          version: '1.0'
-        });
-      }(jQuery);
+            function s(r, o, e) {
+                i(o, 'xaxis', e), i(o, 'yaxis', e);
+            }
+
+            r.plot.plugins.push({
+                init: function (r) {
+                    r.hooks.processRawData.push(o), r.hooks.processDatapoints.push(s);
+                },
+                options: {xaxis: {categories: null}, yaxis: {categories: null}},
+                name: 'categories',
+                version: '1.0'
+            });
+        }(jQuery);
     </script>
 
 
     <script>
-      // Flot charts data and options
-      let data1 = $('#flot-line-chart').data('recruits'),
-        data2 = $('#flot-line-chart').data('removals'),
-        data3 = $('#flot-line-chart').data('population'),
-        chartUsersOptions = {
+        // Flot charts data and options
+        let data1 = $('#flot-line-chart').data('recruits'),
+            data2 = $('#flot-line-chart').data('removals'),
+            data3 = $('#flot-line-chart').data('population'),
+            chartUsersOptions = {
 
-          series: {
+                series: {
 
-            points: {
-              show: true,
-              radius: 2,
-              symbol: 'circle'
-            },
+                    points: {
+                        show: true,
+                        radius: 2,
+                        symbol: 'circle'
+                    },
 
-            splines: {
-              show: true,
-              tension: 0.4,
-              lineWidth: 1,
-              fill: .10,
-            }
-          },
+                    splines: {
+                        show: true,
+                        tension: 0.4,
+                        lineWidth: 1,
+                        fill: .10,
+                    }
+                },
 
-          grid: {
-            tickColor: '#404652',
-            borderWidth: 1,
-            color: '#000',
-            borderColor: '#404652',
-          },
+                grid: {
+                    tickColor: '#404652',
+                    borderWidth: 1,
+                    color: '#000',
+                    borderColor: '#404652',
+                },
 
-          comment: {
-            show: true,
-          },
+                comment: {
+                    show: true,
+                },
 
-          tooltip: false,
+                tooltip: false,
 
-          tooltippage: {
-            show: true,
-            content: '%y members'
-          },
+                tooltippage: {
+                    show: true,
+                    content: '%y members'
+                },
 
-          xaxis: {
-            mode: 'categories',
-            tickLength: 0,
-          },
+                xaxis: {
+                    mode: 'categories',
+                    tickLength: 0,
+                },
 
-          colors: ['#1bbf89', '#f7af3e', '#0F83C9']
-        };
+                colors: ['#1bbf89', '#f7af3e', '#0F83C9']
+            };
 
-      $.plot($('#flot-line-chart'), [data1, data2, data3], chartUsersOptions);
-
-      $(window).resize(function () {
         $.plot($('#flot-line-chart'), [data1, data2, data3], chartUsersOptions);
-      });
+
+        $(window).resize(function () {
+            $.plot($('#flot-line-chart'), [data1, data2, data3], chartUsersOptions);
+        });
     </script>
 
 @stop
