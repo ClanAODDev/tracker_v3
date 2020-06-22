9b08d7ab769d20dd0a50f1e7aea2210c685f719c
Chris Deaton <dcdeaton@ncsu.edu>
2020-06-21 22:01:55 -0400

    fix note icon, valorant

--- resources/views/member/partials/note-icon.blade.php
+++ resources/views/member/partials/note-icon.blade.php
@@ -3,7 +3,7 @@
 @elseif ($note->type == 'positive')
     <i class="fa fa-thumbs-up text-success"></i>
 @elseif ($note->type == 'sr_ldr')
-    <i class="fa fa-shield text-primary"></i>
+    <i class="fas fa-shield-alt"></i>
 @else
     <i class="fa fa-comment text-accent"></i>
 @endif
