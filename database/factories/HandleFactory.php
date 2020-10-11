43df26c86ca2539f489ab2baf9e1ee9df64473e0
Chris Deaton <dcdeaton@ncsu.edu>
2020-10-11 12:07:05 -0400

    fix factory namespaces

--- database/factories/HandleFactory.php
+++ database/factories/HandleFactory.php
@@ -3,7 +3,7 @@
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
-$factory->define(\App\Handle::class, function (Faker $faker) {
+$factory->define(\App\Models\Handle::class, function (Faker $faker) {
     return [
         'label' => $faker->words(4, true),
         'type' => Str::slug($faker->words(4, true))
