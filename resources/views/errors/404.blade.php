1c826f136cb1711325163b1e190dbf3582881898
Chris Deaton <dcdeaton@ncsu.edu>
2020-10-11 11:21:11 -0400

    clean up error views

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -27,35 +27,6 @@
             <a href="{{ route('home') }}" class="btn btn-accent">Back to app</a>
         </div>
 
-
-
-
-
-{{--                @if(app()->bound('sentry') && !empty(Sentry::getLastEventID()))--}}
-                    <div class="subtitle">Error ID: {{ Sentry::getLastEventID() }}</div>
-
-                    <!-- Sentry JS SDK 2.1.+ required -->
-                    <script src="https://cdn.ravenjs.com/3.3.0/raven.min.js"></script>
-
-                    <script>
-                        Raven.showReportDialog({
-                            eventId: '{{ Sentry::getLastEventID() }}',
-                            // use the public DSN (dont include your secret!)
-                            dsn: 'https://2a218d8004d84610bfa58ba2ec13e74f@o166180.ingest.sentry.io/1238719',
-                            user: {
-                                'name': 'Jane Doe',
-                                'email': 'jane.doe@example.com',
-                            }
-                        });
-                    </script>
-{{--                @endif--}}
-            </div>
-
-
-
-
-
-
     </div>
 
 @endsection
