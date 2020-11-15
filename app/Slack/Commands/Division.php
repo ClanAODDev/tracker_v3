58925bf369004cd2d2558606fd8f4b83309e55d8
Chris Deaton <dcdeaton@ncsu.edu>
2020-11-15 16:03:05 -0500

    division info command

---
+++ app/Slack/Commands/Division.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Slack\Commands;
+
+use App\Slack\Base;
+use App\Slack\Command;
+
+/**
+ * Class Search
+ *
+ * @package App\Slack\Commands
+ */
+class Division extends Base implements Command
+{
+
+    public function __construct($data)
+    {
+        parent::__construct($data);
+
+        $this->request = $data;
+    }
+
+    /**
+     * @return array
+     */
+    public function handle()
+    {
+        if (strlen($this->params) < 3) {
+            return [
+                'text' => "Provide 3 characters or more of the division name",
+            ];
+        }
+
+        $division = Division::where('name', 'LIKE', "%{$this->params}%")->get();
+
+        if ($division) {
+            $leaders = $division->leaders->toArray();
+            return [
+                "embed" => [
+                    'color' => 10181046,
+                    'title' => "Search results",
+                    'fields' =>  [
+                        'name' => "{$division->name}",
+                        'value' => $division->leaders->each()
+                    ]
+                ]
+            ];
+        }
+
+        return [
+            'text' => "No results were found",
+        ];
+    }
+
+    /**
+     * @param $member
+     * @return string|null
+     */
+    private function buildActivityBlock($member)
+    {
+        $forumActivity = $member->last_activity->diffForHumans();
+
+        return PHP_EOL . "Forum activity: {$forumActivity}";
+    }
+}
