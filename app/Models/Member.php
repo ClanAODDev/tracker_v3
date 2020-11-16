de3196d0f8af3dbdf023264537cc4c63d380b2bc
Chris Deaton <dcdeaton@ncsu.edu>
2020-11-16 18:41:56 -0500

    cleanup

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,15 +2,13 @@
 
 namespace App\Models;
 
-use App\Activities\RecordsActivity;
 use App\Presenters\MemberPresenter;
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 /**
  * Class Member
  *
@@ -20,6 +18,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Member extends \Illuminate\Database\Eloquent\Model
 {
     use \App\Models\Member\HasCustomAttributes, \App\Activities\RecordsActivity, \Illuminate\Database\Eloquent\SoftDeletes;
+
     public const UNVERIFIED_EMAIL_GROUP_ID = 3;
     /**
      * @var array
@@ -29,7 +28,10 @@ class Member extends \Illuminate\Database\Eloquent\Model
     /**
      * @var array
      */
-    protected $fillable = ['name', 'clan_id', 'platoon_id', 'squad_id', 'position_id', 'division_id', 'posts', 'join_date', 'last_activity', 'last_ts_activity', 'last_promoted_at', 'recruiter_id'];
+    protected $fillable = [
+        'name', 'clan_id', 'platoon_id', 'squad_id', 'position_id', 'division_id', 'posts', 'join_date',
+        'last_activity', 'last_ts_activity', 'last_promoted_at', 'recruiter_id'
+    ];
     /**
      * @var array
      */
@@ -42,6 +44,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
         'xo_at',
         'co_at',
     ];
+
     /**
      * @return MemberPresenter
      */
@@ -49,6 +52,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return new \App\Presenters\MemberPresenter($this);
     }
+
     /**
      * relationship - user has one member
      */
@@ -56,6 +60,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->hasOne(\App\Models\User::class);
     }
+
     /**
      * @return HasMany
      */
@@ -63,6 +68,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->hasMany(\App\Models\Note::class, 'member_id')->orderBy('created_at', 'desc');
     }
+
     /**
      * @param $position
      * @return Model
@@ -71,7 +77,8 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         $newPosition = $position instanceof \App\Models\Position ? $position : \App\Models\Position::whereName(strtolower($position))->firstOrFail();
         // reset assignments for specific positions
-        if (in_array($newPosition->name, ["Commanding Officer", "Executive Officer", "General Sergeant", "Clan Admin"])) {
+        if (in_array($newPosition->name,
+            ["Commanding Officer", "Executive Officer", "General Sergeant", "Clan Admin"])) {
             $this->platoon_id = 0;
             $this->squad_id = 0;
         }
@@ -85,6 +92,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
         }
         return $this->position()->associate($newPosition);
     }
+
     /**
      * relationship - member belongs to a position
      */
@@ -92,6 +100,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->belongsTo(\App\Models\Position::class);
     }
+
     /**
      * @param $rank
      * @return Model
@@ -100,6 +109,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->rank()->associate(\App\Models\Rank::whereName(strtolower($rank))->firstOrFail());
     }
+
     /**
      * relationship - member belongs to a rank
      */
@@ -107,6 +117,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->belongsTo(\App\Models\Rank::class);
     }
+
     /**
      * Enforce a singleton relationship for squad leaders
      *
@@ -118,6 +129,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->hasOne(\App\Models\Squad::class, 'leader_id');
     }
+
     /**
      * Handle Staff Sergeant assignments
      * division/
@@ -128,14 +140,19 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->belongsToMany(\App\Models\Division::class, 'staff_sergeants')->withTimestamps();
     }
+
     /**
      * Resets member's positions and division assignments
      * including part-time divisions
      */
     public function resetPositionAndAssignments()
     {
-        $this->update(['division_id' => 0, 'platoon_id' => 0, 'squad_id' => 0, 'position_id' => 1, 'flagged_for_inactivity' => false]);
+        $this->update([
+            'division_id' => 0, 'platoon_id' => 0, 'squad_id' => 0, 'position_id' => 1,
+            'flagged_for_inactivity' => false
+        ]);
     }
+
     /**
      * Handle Staff Sergeant assignments
      * division/
@@ -146,6 +163,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->belongsToMany(\App\Models\Division::class, 'division_parttimer')->withTimestamps();
     }
+
     /**
      * relationship - member belongs to a platoon
      */
@@ -153,6 +171,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->belongsTo(\App\Models\Platoon::class);
     }
+
     /**
      * relationship - member belongs to a squad
      */
@@ -160,6 +179,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->belongsTo(\App\Models\Squad::class);
     }
+
     /**
      * relationship - member has many divisions
      */
@@ -167,6 +187,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->belongsTo(\App\Models\Division::class);
     }
+
     /**
      * @return BelongsTo
      */
@@ -174,14 +195,17 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->belongsTo(\App\Models\Member::class, 'recruiter_id', 'clan_id');
     }
+
     public function recruits()
     {
         return $this->hasMany(\App\Models\Member::class, 'recruiter_id', 'clan_id');
     }
+
     public function trainer()
     {
         return $this->belongsTo(\App\Models\Member::class, 'last_trained_by', 'clan_id');
     }
+
     /**
      * @return HasOne
      */
@@ -189,6 +213,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->hasOne(\App\Models\Leave::class, 'member_id', 'clan_id');
     }
+
     /**
      * @return mixed
      */
@@ -196,6 +221,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->hasOne(\App\Models\Leave::class)->where('end_date', '<', \Carbon\Carbon::today());
     }
+
     /**
      * @return mixed
      */
@@ -206,6 +232,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
         }
         return false;
     }
+
     /**
      * @return HasOne
      */
@@ -213,6 +240,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->hasOne(\App\Models\MemberRequest::class, 'member_id', 'clan_id');
     }
+
     /**
      * @return mixed
      */
@@ -232,27 +260,30 @@ class Member extends \Illuminate\Database\Eloquent\Model
     {
         return $this->belongsToMany(\App\Models\Handle::class)->withPivot('value');
     }
+
     /**
-     * @param Squad $squad
+     * @param  Squad  $squad
      * @return bool
      */
     public function isSquadLeader(\App\Models\Squad $squad)
     {
         return $this->clan_id === $squad->leader_id;
     }
+
     /**
-     * @param Platoon $platoon
+     * @param  Platoon  $platoon
      * @return bool
      */
     public function isPlatoonLeader(\App\Models\Platoon $platoon)
     {
         return $this->clan_id === $platoon->leader_id;
     }
+
     /**
      * Check to see if the member is a division leader
      * and also assigned to the given division
      *
-     * @param Division $division
+     * @param  Division  $division
      * @return bool
      */
     public function isDivisionLeader(\App\Models\Division $division)
@@ -262,6 +293,7 @@ class Member extends \Illuminate\Database\Eloquent\Model
         }
         return false;
     }
+
     /**
      * @param $rank
      * @return bool
@@ -276,10 +308,12 @@ class Member extends \Illuminate\Database\Eloquent\Model
         }
         return $this->rank->abbreviation === $rank;
     }
+
     public function getUrlParams()
     {
         return [$this->clan_id, $this->rank->abbreviation . '-' . $this->name];
     }
+
     /**
      * @return HasMany
      */
