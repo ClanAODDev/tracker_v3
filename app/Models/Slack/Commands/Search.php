9fd5eda7fbb975c035a395ca3c145b75703c4ab5
Chris Deaton <dcdeaton@ncsu.edu>
2020-11-16 18:47:50 -0500

    fix namespacing, add parent

--- app/Models/Slack/Commands/Search.php
+++ app/Models/Slack/Commands/Search.php
@@ -3,8 +3,8 @@
 namespace App\Models\Slack\Commands;
 
 use App\Models\Member;
-use App\Slack\Base;
-use App\Slack\Command;
+use App\Models\Slack\Base;
+use App\Models\Slack\Command;
 
 /**
  * Class Search
