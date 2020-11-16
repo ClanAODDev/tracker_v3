fad27f053dacc38d6e1e8aad11d16ae5f973ed4a
Chris Deaton <dcdeaton@ncsu.edu>
2020-11-16 18:42:02 -0500

    this is not real

---
+++ app/Models/Slack/Commands/Delete.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models\Slack\Commands;
+
+use App\Models\Slack\Base;
+use App\Models\Slack\Command;
+
+/**
+ * This doesn't actually do anything. Just a gag.
+ *
+ * Class Division
+ * @package App\Models\Slack\Commands
+ */
+class Delete extends Base implements Command
+{
+    /**
+     * @return array
+     */
+    public function handle()
+    {
+        if (strlen($this->params) <= 3) {
+            return [
+                'text' => "Please provide the member ID you wish to delete",
+            ];
+        }
+
+        $member = \App\Models\Member::where('clan_id', 'LIKE', "%{$this->params}%")->first();
+
+        if (!$member) {
+            return [
+                'text' => "I can't delete a member that doesn't exist."
+            ];
+        }
+
+        $responses = [
+            ":skull_crossbones: I will delete {$member->name} in 5 seconds unless you cancel the command...",
+            ":skull_crossbones: {$member->name} has been permanently deleted! Recovery is not possible.",
+            ":skull_crossbones: Permission denied! Your attempt to delete has been reported to the clan admins.",
+            ":skull_crossbones: Do you really think {$member->name} wants you to delete them? That's not nice.",
+        ];
+
+        return [
+            'text' => array_rand($responses)
+        ];
+    }
+}
