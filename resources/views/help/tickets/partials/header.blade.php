630394fdce842e271513e5dcd7fe3a2168e76100
Chris Deaton <dcdeaton@ncsu.edu>
2020-11-16 13:48:43 -0500

    create partial for header

---
+++ resources/views/help/tickets/partials/header.blade.php
@@ -0,0 +1,6 @@
+<h4>Create an Admin Ticket</h4>
+<p>Submit a ticket only if you require assistance from the <strong>MSGT+</strong> team. If you have
+    questions regarding the use of this form, please use the Admin discord channel. </p>
+
+<p>Note that structural changes to division assets either on the forums or website must come from the
+    division CO/XO team.</p>
