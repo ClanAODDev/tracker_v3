c52aa2ecf547e00c1342416eafaa0a102d4e6367
Chris Deaton <dcdeaton@ncsu.edu>
2020-11-18 13:59:22 -0500

    view cleanup, add boilerplate area

--- resources/views/help/tickets/create.blade.php
+++ resources/views/help/tickets/create.blade.php
@@ -23,15 +23,21 @@
 
             <div class="row">
                 <div class="col-md-12">
-
-                    @include('application.partials.errors')
                     @include('help.tickets.partials.header')
                     <hr>
+
+                    @if ($type->boilerplate)
+                        <h5>Required information</h5>
+                        <p>Ensure your ticket includes the following:</p>
+                        <pre><code class="language-markdown">{{ $type->boilerplate }}</code></pre>
+                        <hr>
+                    @endif
+                    @include('application.partials.errors')
                     <div class="form-group">
                         <label for="description">Issue description:</label>
                         <textarea name="description" id="description" class="form-control"
                                   style="resize: vertical; min-height: 140px;" rows="10"
-                                  required>{{ $type->boilerplate }}</textarea>
+                                  required>{{ old('description') }}</textarea>
                         <input type="hidden" value="{{ $type->id }}" name="ticket_type" id="ticket_type">
                         <small class="help-text">Please provide as much detail as possible. Min 25 characters.</small>
                     </div>
@@ -39,7 +45,6 @@
             </div>
 
             <hr>
-
 
             <div class="text-right">
                 <button type="reset" class="btn btn-default">Reset form</button>
