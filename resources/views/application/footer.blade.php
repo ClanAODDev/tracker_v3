bfce779e6aafeb9e5fbcc16a2db8fbb7e52f332b
Chris Deaton <dcdeaton@ncsu.edu>
2020-11-24 18:57:37 -0500

    merry xmas

--- resources/views/application/footer.blade.php
+++ resources/views/application/footer.blade.php
@@ -5,3 +5,4 @@
 <script src="{{ asset('/js/main.js?v=3.2') }}"></script>
 
 <script src="https://cdn.ravenjs.com/3.26.2/raven.min.js" crossorigin="anonymous"></script>
+<script src="{{ asset('/js/snowstorm.js') }}"></script>
