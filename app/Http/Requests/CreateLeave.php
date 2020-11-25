28964a9477f938b14fe055949f0e33713e52f992
Chris Deaton <dcdeaton@ncsu.edu>
2020-11-25 12:14:18 -0500

    fix missing author id
    also include reason in body of note generated

--- app/Http/Requests/CreateLeave.php
+++ app/Http/Requests/CreateLeave.php
@@ -2,11 +2,6 @@
 
 namespace App\Http\Requests;
 
-use App\Models\Leave;
-use App\Models\Member;
-use App\Models\Note;
-use Carbon\Carbon;
-use Illuminate\Foundation\Http\FormRequest;
 class CreateLeave extends \Illuminate\Foundation\Http\FormRequest
 {
     /**
@@ -18,6 +13,7 @@ class CreateLeave extends \Illuminate\Foundation\Http\FormRequest
     {
         return true;
     }
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -27,10 +23,15 @@ class CreateLeave extends \Illuminate\Foundation\Http\FormRequest
     {
         return ['end_date' => 'date|after:today', 'member_id' => ['exists:members,clan_id', 'unique:leaves,member_id']];
     }
+
     public function messages()
     {
-        return ['member_id.exists' => 'Not a valid AOD member', 'member_id.unique' => 'Member already has a leave of absence'];
+        return [
+            'member_id.exists' => 'Not a valid AOD member',
+            'member_id.unique' => 'Member already has a leave of absence'
+        ];
     }
+
     /**
      * store leave and note
      */
@@ -38,8 +39,17 @@ class CreateLeave extends \Illuminate\Foundation\Http\FormRequest
     {
         // search is by clan id, but we want member id (tracker id)
         $memberRequestingLeave = \App\Models\Member::whereClanId($this->member_id)->firstOrFail();
-        $note = \App\Models\Note::create(['body' => $this->note_body, 'forum_thread_id' => $this->note_thread_id, 'type' => 'misc', 'author_id' => auth()->user(), 'member_id' => $memberRequestingLeave->id]);
-        $leave = \App\Models\Leave::create(['reason' => $this->leave_type, 'end_date' => \Carbon\Carbon::parse($this->end_date), 'extended' => false]);
+
+        $note = \App\Models\Note::create([
+            'body' => "Leave of absence requested. Reason: {$this->note_body}",
+            'forum_thread_id' => $this->note_thread_id, 'type' => 'misc',
+            'author_id' => auth()->id(), 'member_id' => $memberRequestingLeave->id
+        ]);
+
+        $leave = \App\Models\Leave::create([
+            'reason' => $this->leave_type, 'end_date' => \Carbon\Carbon::parse($this->end_date), 'extended' => false
+        ]);
+
         $leave->member()->associate($this->member_id);
         $leave->requester()->associate(auth()->user());
         $leave->note()->associate($note);
