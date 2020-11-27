226242db062ab4eec0c381d79ddf1c88c3001cdf
Chris Deaton <dcdeaton@ncsu.edu>
2020-11-26 19:20:29 -0500

    resolve stash

--- resources/views/application/base-website.blade.php
+++ resources/views/application/base-website.blade.php
@@ -1,16 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <title>ClanAOD.net </title>
+    <title>ClanAOD.net</title>
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
           integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('site.css') }}"/>
 
-    <script src="<?= get_template_directory_uri() . '/assets/js/modernizr.js' ?>"></script>
+    <link rel="stylesheet" href="{{ asset('css/site.css') }}"/>
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('images/favicons/apple-icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('images/favicons/apple-icon-60x60.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('images/favicons/apple-icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('images/favicons/apple-icon-76x76.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('images/favicons/apple-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('images/favicons/apple-icon-120x120.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('images/favicons/apple-icon-144x144.png') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('images/favicons/apple-icon-152x152.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicons/apple-icon-180x180.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('images/favicons/android-icon-192x192.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('images/favicons/favicon-96x96.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('images/favicons/manifest.json') }}">
 
-    <?php include(get_template_directory() . '/assets/partials/favicons.php'); ?>
-    <?php include(get_template_directory() . '/assets/partials/site-meta.php'); ?>
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="{{ asset('images/favicons/ms-icon-144x144.png') }}">
+    <meta name="theme-color" content="#ffffff">
 
+    <!-- open graph -->
+    <meta name="og:title" content="Battlefield Division">
+    <meta name="og:site_name" content="ClanAOD.net">
+    <meta name="og:image" content="{{ asset('images/game_icons/48x48/bf.png') }}">
+    <meta content="There are over 600 members in the Battlefield Division of Clan AOD. While we are a casual gaming group, we also enjoy and embody the competitive spirit of the first-person shooter. Engaging in intense battles over massive maps, we endeavor to create a true community environment of good sportsmanship and absolute fun." property="og:description">
+    
 </head>
 
 <body>
