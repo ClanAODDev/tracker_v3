06bd347bceb0ad886af06e589d8b19507fda3f3d
Chris Deaton <dcdeaton@ncsu.edu>
2020-11-28 19:07:46 -0500

    wip

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -8,6 +8,11 @@ class WebsiteController extends Controller
 {
     public function index()
     {
+        $channel = (new ClanAOD\Twitch('clanaodstream'))->getChannel();
+        $status = is_null($channel->stream) ? "offline" : 'online';
+
+        ddd($channel);
+
         $commo = [
             'ts' => AOD::request('https://www.clanaod.net/forums/aodinfo.php?type=last_ts_population_json&'),
             'discord' => AOD::request('https://www.clanaod.net/forums/aodinfo.php?type=last_discord_population_json&'),
