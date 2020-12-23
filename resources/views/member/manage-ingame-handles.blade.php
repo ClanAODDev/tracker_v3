8ec7a712189bafd6a175c19777577942dc87959f
Chris Deaton <dcdeaton@ncsu.edu>
2020-12-23 13:09:21 -0500

    fix size

--- resources/views/member/manage-ingame-handles.blade.php
+++ resources/views/member/manage-ingame-handles.blade.php
@@ -4,7 +4,7 @@
     @component ('application.components.division-heading')
         @slot ('icon')
             @if ($division)
-                <img src="{{ getDivisionIconPath($division->abbreviation) }}" class="division-icon-medium" />
+                <img src="{{ getDivisionIconPath($division->abbreviation) }}" class="division-icon-large" />
             @else
                 <img src="{{ asset('images/logo_v2.svg') }}" width="50px" style="opacity: .2;" />
             @endif
