f553ea95ceb69c058f4688cf8df5951f96433a18
Chris Deaton <dcdeaton@ncsu.edu>
2021-01-25 10:43:01 -0500

    improved appearance

--- resources/assets/js/components/admin/_request.vue
+++ resources/assets/js/components/admin/_request.vue
@@ -1,82 +1,84 @@
 <template>
-    <tr :class="{'grace-period': !isPastGracePeriod}">
-        <td>
-            <i class="fa fa-hourglass-half" v-show="!isPastGracePeriod" title="Grace period"></i>
-            <div class="input-group" v-if="editing">
-                <input type="text" class="form-control" v-model="name"
-                       @input="buildPath">
-                <span class="input-group-btn m-l-sm">
+  <tr :class="{'grace-period': !isPastGracePeriod}">
+    <td>
+      <i class="fa fa-hourglass-half" v-show="!isPastGracePeriod" title="Grace period"></i>
+      <div class="input-group" v-if="editing">
+        <input type="text" class="form-control" v-model="name"
+               @input="buildPath">
+        <span class="input-group-btn m-l-sm">
                     <button class="btn btn-default" type="button"
                             @click="editing = false">
                         <i class="fa fa-check text-success"></i>
                     </button>
                 </span>
-            </div>
-            <code v-else @click="editing = true" style="cursor:pointer;">{{ name }}</code>
-        </td>
-        <td>{{ this.data.requester.name }}</td>
-        <td>{{ division }}</td>
-        <td class="text-center">{{ this.data.timeWaiting }}</td>
-        <td>
+      </div>
+      <code v-else @click="editing = true" style="cursor:pointer;">{{ name }}</code>
+    </td>
+    <td>{{ this.data.requester.name }}</td>
+    <td>{{ division }}</td>
+    <td class="text-center">{{ this.data.timeWaiting }}</td>
+    <td>
 
-            <div class="btn-group">
-                <button @click="approve()"
-                        class="btn btn-success mr-2">
-                    <small class="text-uppercase"><i class="fa fa-user-plus"></i> Approve</small>
-                </button>
-                <button @click="placeOnHold()"
-                        class="btn btn-warning mr-2">
-                    <small class="text-uppercase"><i class="fa fa-hourglass-half"></i> Hold</small>
-                </button>
-            </div>
-        </td>
-    </tr>
+      <div class="col-md-6 col-sm-12">
+        <button @click="approve()"
+                class="btn btn-success mr-2">
+          <small class="text-uppercase"><i class="fa fa-user-plus"></i> Approve</small>
+        </button>
+      </div>
+      <div class="col-md-6 col-sm-12">
+        <button @click="placeOnHold()"
+                class="btn btn-warning mr-2">
+          <small class="text-uppercase"><i class="fa fa-hourglass-half"></i> Hold</small>
+        </button>
+      </div>
+    </td>
+  </tr>
 </template>
 
 <script>
-    export default {
-        props: ['data'],
-        created() {
-            this.buildPath();
-        },
+export default {
+  props: ['data'],
+  created() {
+    this.buildPath();
+  },
 
-        computed: {
-            isPastGracePeriod() {
-                console.log(this.data.isPastGracePeriod);
-                return (this.data.isPastGracePeriod);
-            },
-        },
-        methods: {
+  computed: {
+    isPastGracePeriod() {
+      console.log(this.data.isPastGracePeriod);
+      return (this.data.isPastGracePeriod);
+    },
+  },
+  methods: {
 
-            approve() {
-                this.$emit('approved', {id: this.id, path: this.path});
+    approve() {
+      this.$emit('approved', {id: this.id, path: this.path});
 
-                if (this.name !== this.data.name) {
-                    this.$emit('name-changed', {oldName: this.data.name, newName: this.name, id: this.id});
-                }
-            },
-            placeOnHold() {
-                this.$emit('placedOnHold', this.id);
-            },
-            buildPath() {
-                this.path = this.data.approvePath + this.name;
-            }
-        },
-        data: function () {
-            return {
-                editing: false,
-                id: this.data.id,
-                name: this.data.name,
-                division: this.data.division.name,
-                path: '',
-                notes: '',
-            };
-        },
+      if (this.name !== this.data.name) {
+        this.$emit('name-changed', {oldName: this.data.name, newName: this.name, id: this.id});
+      }
+    },
+    placeOnHold() {
+      this.$emit('placedOnHold', this.id);
+    },
+    buildPath() {
+      this.path = this.data.approvePath + this.name;
+    }
+  },
+  data: function () {
+    return {
+      editing: false,
+      id: this.data.id,
+      name: this.data.name,
+      division: this.data.division.name,
+      path: '',
+      notes: '',
     };
+  },
+};
 </script>
 
 <style>
-    .grace-period {
-        opacity: .35;
-    }
+.grace-period {
+  opacity: .35;
+}
 </style>
