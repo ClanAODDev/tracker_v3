cd2b28f851233fcc2f81ab770055e66f1002b3d7
Chris Deaton <dcdeaton@ncsu.edu>
2021-06-19 09:14:45 -0400

    update primaryDivision middleware
    shouldn't happen in production, but can happen in local environment where no user has been generated

--- app/Http/Middleware/HasPrimaryDivision.php
+++ app/Http/Middleware/HasPrimaryDivision.php
@@ -11,8 +11,8 @@ class HasPrimaryDivision
     /**
      * Handle an incoming request.
      *
-     * @param Request $request
-     * @param Closure $next
+     * @param  Request  $request
+     * @param  Closure  $next
      * @return mixed
      */
     public function handle($request, Closure $next)
@@ -20,9 +20,16 @@ class HasPrimaryDivision
         if (Auth::check()) {
             $user = Auth::user();
 
+            if (!$user->member) {
+                abort(408, "No associated member record");
+                return false;
+            }
+
+
             if (!$user->member->division) {
                 //Auth::logout();
                 abort(408, "You do not have a primary division.");
+                return false;
             }
         }
 
