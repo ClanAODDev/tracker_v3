afcf694164e2af372118f0928a1de214092da16b
Chris Deaton <dcdeaton@ncsu.edu>
2021-06-20 17:01:15 -0400

    add HasFactory traits

--- app/Models/Squad.php
+++ app/Models/Squad.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use App\Activities\RecordsActivity;
 use App\Presenters\SquadPresenter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Squad extends Model
 {
-    use RecordsActivity;
+    use RecordsActivity, HasFactory;
 
     protected static $recordEvents = [
         'created',
