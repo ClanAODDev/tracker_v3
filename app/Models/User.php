3544a9847fea1617238af660d3b68eab9dbd63c6
Chris Deaton <dcdeaton@ncsu.edu>
2021-07-04 20:09:07 -0400

    swap passport out with sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 
 /**
  * Class User
