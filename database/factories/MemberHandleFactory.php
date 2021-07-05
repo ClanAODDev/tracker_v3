b24302b57811621b15bcc4bb977a0553255f64d7
Chris Deaton <dcdeaton@ncsu.edu>
2021-07-04 20:14:07 -0400

    add memberhandle factory
    can't believe I never made one of these...

---
+++ database/factories/MemberHandleFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Handle;
+use App\Models\Member;
+use App\Models\MemberHandle;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MemberHandleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = MemberHandle::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'handle_id' => Handle::factory(),
+            'member_id' => Member::factory(),
+            'value' => $this->faker->userName
+        ];
+    }
+}
