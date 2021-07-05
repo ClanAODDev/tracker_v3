6f896a7dd500f27f0d81b10f420edd499341208f
Chris Deaton <dcdeaton@ncsu.edu>
2021-07-04 20:13:42 -0400

    build out api controllers
    includes basic tests for tokens. still need to do policies for access

--- app/Http/Controllers/API/v1/DivisionController.php
+++ app/Http/Controllers/API/v1/DivisionController.php
@@ -3,52 +3,53 @@
 namespace App\Http\Controllers\API\v1;
 
 use App\Models\Division;
-use App\Transformers\DivisionTransformer;
+use App\Transformers\DivisionBasicTransformer;
+use App\Transformers\DivisionFullTransformer;
 use Illuminate\Http\JsonResponse;
 
 class DivisionController extends ApiController
 {
-    /**
-     * @var DivisionTransformer
-     */
-    protected $divisionTransformer;
+    protected $basicTransformer;
+    protected $fullTransformer;
 
-    /**
-     * DivisionController constructor.
-     *
-     * @param DivisionTransformer $divisionTransformer
-     */
-    public function __construct(DivisionTransformer $divisionTransformer)
-    {
-        $this->divisionTransformer = $divisionTransformer;
-
-        $this->middleware('auth');
+    public function __construct(
+        DivisionBasicTransformer $basicTransformer,
+        DivisionFullTransformer $fullTransformer
+    ) {
+        $this->basicTransformer = $basicTransformer;
+        $this->fullTransformer = $fullTransformer;
     }
 
-    /**
-     * @return JsonResponse
-     */
-    public function index()
-    {
-        $divisions = Division::active()
-            ->shuttingDown(false)
-            ->with('members')->get();
 
-        return $this->respond([
-            'data' => $this->divisionTransformer->transformCollection($divisions->all())
-        ]);
+    public function index(): JsonResponse
+    {
+        if ($this->tokenCan('basic:read')) {
+            $divisions = Division::get();
+
+            return $this->respond([
+                'data' => $this->basicTransformer->transformCollection($divisions->all()),
+            ]);
+        }
+
+        return $this->setStatusCode(403)
+            ->respondWithError("Not authorized to access this endpoint");
     }
 
-    public function show($abbreviation)
+    public function show($abbreviation): JsonResponse
     {
         $division = Division::whereAbbreviation($abbreviation)->first();
 
         if (!$division) {
-            return $this->respondNotFound('Division could not be found');
+            return $this->respondNotFound();
+        }
+
+        if ($division != auth()->user()->member->division && !$this->tokenCan('clan:read')) {
+            return $this->setStatusCode(403)
+                ->respondWithError("Not authorized to access this endpoint");
         }
 
         return $this->respond([
-            'data' => $this->divisionTransformer->transform($division)
+            'data' => $this->fullTransformer->transform($division, true),
         ]);
     }
 }
