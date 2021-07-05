483396c9713f901d09ddb4ab0d5636d30729326e
Chris Deaton <dcdeaton@ncsu.edu>
2021-07-04 20:13:42 -0400

    build out api controllers
    includes basic tests for tokens. still need to do policies for access

---
+++ app/Http/Controllers/API/v1/MemberController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Models\Member;
+use App\Transformers\MemberFullTransformer;
+
+class MemberController extends ApiController
+{
+    protected MemberFullTransformer $memberTransformer;
+
+    public function __construct(MemberFullTransformer $memberTransformer)
+    {
+        $this->memberTransformer = $memberTransformer;
+    }
+
+    public function show($clan_id)
+    {
+        $member = Member::whereClanId($clan_id)->first();
+
+        if (!$member) {
+            return $this->respondNotFound();
+        }
+
+        // TODO
+    }
+}
