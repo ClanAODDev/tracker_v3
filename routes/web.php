a58e82af628b515ebe816860993cb8ef17d7059a
Chris Deaton <dcdeaton@ncsu.edu>
2021-07-04 20:16:11 -0400

    require routes instead of include
    weird behavior during unit testing otherwise

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,16 @@ include_once('extra/requests.php');
 Route::view('unauthorized', 'errors.403')->name('errors.unauthorized');
 
 Auth::routes();
+
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 
-include_once('partials/application.php');
-include_once('partials/ajax.php');
-include_once('partials/members.php');
-include_once('partials/divisions.php');
-include_once('partials/slack.php');
-include_once('partials/reports.php');
-include_once('partials/clan.php');
+require('partials/application.php');
+require('partials/ajax.php');
+require('partials/members.php');
+require('partials/divisions.php');
+require('partials/reports.php');
+require('partials/clan.php');
 
 
-include_once('partials/website.php');
+require('partials/website.php');
