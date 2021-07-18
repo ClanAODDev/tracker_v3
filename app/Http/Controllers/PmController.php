d8c1e753aa7d658b2d533f9d9b79567090f025f0
Chris Deaton <dcdeaton@ncsu.edu>
2021-07-18 19:18:46 -0400

    Refactor to form request

--- app/Http/Controllers/PmController.php
+++ app/Http/Controllers/PmController.php
@@ -2,17 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SendBulkPm;
 use Spatie\ValidationRules\Rules\Delimited;
 
 use Illuminate\Http\Request;
 
 class PmController extends Controller
 {
-    public function create(Request $request)
+    public function create(SendBulkPm $request)
     {
-        $validated = $request->validate([
-            'pm-member-data' => (new Delimited('numeric'))->min(2),
-        ]);
+        $validated = $request->validated();
 
         return view('division.create-pm')->with([
             'members' => collect(explode(',', $validated['pm-member-data'])),
---
+++ app/Http/Requests/SendBulkPm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Spatie\ValidationRules\Rules\Delimited;
+
+class SendBulkPm extends FormRequest
+{
+    public function authorize()
+    {
+        return $this->user()->can('create', \App\Models\Member::class);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'pm-member-data' => [(new Delimited('numeric', $this->messages()))->min(2)],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'pm-member-data.min' => 'You must select at least :min members'
+        ];
+    }
+}
