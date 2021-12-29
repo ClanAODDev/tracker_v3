8fdf138b4aef23d9f0552f17582b157dd3b4ba0c
Chris Deaton <dcdeaton@ncsu.edu>
2021-12-28 21:13:26 -0500

    Reset last promoted at on recruitment

--- app/Http/Controllers/RecruitingController.php
+++ app/Http/Controllers/RecruitingController.php
@@ -196,13 +196,14 @@ class RecruitingController extends \App\Http\Controllers\Controller
 
         // update member properties
         $member->name = $request->forum_name;
-        $member->join_date = \Carbon\Carbon::today();
-        $member->last_activity = \Carbon\Carbon::today();
+        $member->join_date = now();
+        $member->last_activity = now();
         $member->recruiter_id = auth()->user()->member->clan_id;
         $member->rank_id = $request->rank;
         $member->position_id = 1;
         $member->division_id = $division->id;
         $member->flagged_for_inactivity = false;
+        $member->last_promoted_at = now();
         $member->save();
 
         // handle ingame name assignment
