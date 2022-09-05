d1ebc8487614bfbead3e76124535c3cfc9de1216
Chris Deaton <dcdeaton@ncsu.edu>
2022-09-05 14:04:49 -0400

    Update census report

--- resources/views/division/forms/census.blade.php
+++ resources/views/division/forms/census.blade.php
@@ -4,27 +4,20 @@
         <tr>
             <th>Date</th>
             <th class="text-center">Population</th>
-            <th class="text-center">Weekly Active</th>
-            <th class="text-center">Weekly TS Active</th>
+            <th class="text-center">Weekly Comms Active</th>
+{{--            <th class="text-center">Weekly Forum Active</th>--}}
             {{--<th>Notes</th>--}}
         </tr>
         </thead>
         @foreach ($censuses as $census)
 
             @php
-                $popMinus = $census->count - $census->weekly_active_count;
+                $popMinus = $census->count - $census->weekly_ts_count;
             @endphp
 
             <tr>
                 <td>{{ $census->created_at->format('m/d/Y') }}</td>
                 <td class="text-center">{{ $census->count }}</td>
-                <td class="text-center slight">
-                    {{ $census->count > 0 ? number_format($census->weekly_active_count / $census->count * 100, 1) : 0 }}%
-                    <span class="census-pie"
-                          data-colors="{{ json_encode(['#404652', '#1bbf89']) }}"
-                          data-counts="{{ json_encode([$popMinus, $census->weekly_active_count]) }}">
-                    </span>
-                </td>
                 <td class="text-center slight">
                     {{ $census->count > 0 ? number_format($census->weekly_ts_count / $census->count * 100, 1) : 0 }}%
                     <span class="census-pie"
@@ -32,6 +25,13 @@
                           data-counts="{{ json_encode([$popMinus, $census->weekly_ts_count]) }}">
                     </span>
                 </td>
+{{--                <td class="text-center slight">--}}
+{{--                    {{ $census->count > 0 ? number_format($census->weekly_active_count / $census->count * 100, 1) : 0 }}%--}}
+{{--                    <span class="census-pie"--}}
+{{--                          data-colors="{{ json_encode(['#404652', '#1bbf89']) }}"--}}
+{{--                          data-counts="{{ json_encode([$popMinus, $census->weekly_active_count]) }}">--}}
+{{--                    </span>--}}
+{{--                </td>--}}
                 {{--<td>{{ $census->notes }}</td>--}}
             </tr>
         @endforeach
