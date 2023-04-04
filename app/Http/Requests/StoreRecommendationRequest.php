14b19c516d5cb9fe4a0a33faafdc96a24dd467aa
Chris Deaton <dcdeaton@ncsu.edu>
2023-04-04 16:03:04 -0400

    Ensure actor authorized to recommend member

--- app/Http/Requests/StoreRecommendationRequest.php
+++ app/Http/Requests/StoreRecommendationRequest.php
@@ -13,7 +13,7 @@ class StoreRecommendationRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return auth()->user()->can('recommend', request()->member);
     }
 
     /**
