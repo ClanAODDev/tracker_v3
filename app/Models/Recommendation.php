03f78894572739e56af5e954786ee303c23b5c92
Chris Deaton <dcdeaton@ncsu.edu>
2023-05-13 04:38:38 -0400

    More work on Recommendation model

--- app/Models/Recommendation.php
+++ app/Models/Recommendation.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use App\Enums\RecommendationDecision;
+use App\Enums\Role;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Builder;
 
 class Recommendation extends Model
 {
@@ -17,26 +20,42 @@ class Recommendation extends Model
     protected $casts = [
         'decision' => RecommendationDecision::class,
         'type' => RecommendationDecision::class,
+        'effective_at' => 'datetime',
     ];
+
+    public function division()
+    {
+        return $this->hasOneThrough(Member::class, Division::class);
+    }
 
     public function recommendable(): MorphTo
     {
         return $this->morphTo();
     }
 
-    public static function scopeForCurrentMonth($query)
+    public function scopeForRank($query): Builder
     {
-        return $query->whereMonth('effective_at', date('m'));
+        return $query->whereRecommendableType('App\Models\Rank');
     }
 
-    public function member():HasOne
+    public static function scopePending($query): Builder
     {
-        return $this->hasOne(Member::class);
+        return $query->whereDecision(RecommendationDecision::PENDING);
     }
 
-    public function admin(): HasOne
+    public static function scopeForDivision($query, $division_id): Builder
     {
-        return $this->hasOne(Member::class);
+        return $query->whereDivisionId($division_id);
+    }
+
+    public function member(): BelongsTo
+    {
+        return $this->belongsTo(Member::class);
+    }
+
+    public function admin(): BelongsTo
+    {
+        return $this->belongsTo(Member::class);
     }
 
     public function isPromotion(): bool
@@ -44,5 +63,42 @@ class Recommendation extends Model
         if ($this->recommendable_type === 'App\Models\Rank' && $this->member) {
             return $this->member->rank_id < $this->recommendable_id;
         }
+    }
+
+    public function scopeForCurrentUser($query): Builder
+    {
+        // officer -> where user squad = recommendation squad
+        if (auth()->user()->role === Role::OFFICER && auth()->user()->squad) {
+            $query->whereHas('member.squad', function ($query) {
+                $query->whereId(auth()->user()->squad->id);
+            });
+        }
+
+        // jr_leader -> where user platoon = recommendation platoon
+        if (auth()->user()->role === Role::JUNIOR_LEADER && auth()->user()->platoon) {
+            $query->whereHas('member.platoon', function ($query) {
+                $query->whereId(auth()->user()->platoon->id);
+            });
+        }
+
+        // sr_leader -> where user division = recommendation division
+        if (auth()->user()->role === Role::SENIOR_LEADER && auth()->user()->division) {
+            $query->whereHas('member.division', function ($query) {
+                // division id
+                $query->whereId(auth()->user()->division_id);
+            });
+        }
+
+        return $query;
+    }
+
+    public function squad(): HasOneThrough
+    {
+        return $this->hasOneThrough(Squad::class, Member::class, 'id', 'id', 'member_id', 'squad_id');
+    }
+
+    public function platoon(): HasOneThrough
+    {
+        return $this->hasOneThrough(Platoon::class, 'member', 'id', 'id', 'member_id', 'platoon_id');
     }
 }
