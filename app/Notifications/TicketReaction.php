cf644d440479b747af6a8f221c248f37be722bbd
Chris Deaton <dcdeaton@ncsu.edu>
2023-05-19 08:21:05 -0400

    Create TicketReaction notification

---
+++ app/Notifications/TicketReaction.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Channels\Messages\DiscordMessageReact;
+use App\Channels\WebhookChannel;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class TicketReaction extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(private string $status)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [WebhookChannel::class];
+    }
+
+    /**
+     * @param $ticket
+     * @return array
+     *
+     * @throws \Exception
+     */
+    public function toWebhook($ticket)
+    {
+        return (new DiscordMessageReact())
+            ->to(channel: config('app.aod.admin-ticketing-channel'))
+            ->messageId($ticket->message_id)
+            ->status($this->status)
+            ->send();
+    }
+}
