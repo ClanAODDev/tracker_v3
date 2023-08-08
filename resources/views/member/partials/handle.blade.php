6f258a6172b25c650c019bd1149af06b23ea50f1
Chris Deaton <dcdeaton@ncsu.edu>
2023-08-08 00:34:31 -0400

    Add copy buttons to handles

--- resources/views/member/partials/handle.blade.php
+++ resources/views/member/partials/handle.blade.php
@@ -5,5 +5,9 @@
         </small>
         <br />
         <span class="text-uppercase">{{ $handle->pivot->value }}</span>
+        <button data-clipboard-text="{{ $handle->pivot->value }}"
+                class="copy-to-clipboard btn-outline-warning btm-xs btn" style="float: right;display: inline;">
+            <i class="fa fa-clone"></i>
+        </button>
     </div>
 </div>
