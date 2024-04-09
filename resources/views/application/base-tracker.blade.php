fa26e47ab68b4451e0f79683b6138dee709cb9cc
Chris Deaton <dcdeaton@ncsu.edu>
2024-04-09 03:48:07 -0400

    Hide header info

--- resources/views/application/base-tracker.blade.php
+++ resources/views/application/base-tracker.blade.php
@@ -8,25 +8,33 @@
 
 @if (Auth::check() && Auth::user()->member && Auth::user()->member->division)
     <body class="{{ session('primary_nav_collapsed') === true ? 'nav-toggle' : null }}">
-{{--    <canvas id="canvas" style="z-index:5">--}}
+    <span style="display:none;" class="a-little-fancy">
 
-        {!! Toastr::message() !!}
+ ██████ ██       █████  ███    ██  █████   ██████  ██████      ████████ ██████   █████   ██████ ██   ██ ███████ ██████
+██      ██      ██   ██ ████   ██ ██   ██ ██    ██ ██   ██        ██    ██   ██ ██   ██ ██      ██  ██  ██      ██   ██
+██      ██      ███████ ██ ██  ██ ███████ ██    ██ ██   ██        ██    ██████  ███████ ██      █████   █████   ██████
+██      ██      ██   ██ ██  ██ ██ ██   ██ ██    ██ ██   ██        ██    ██   ██ ██   ██ ██      ██  ██  ██      ██   ██
+ ██████ ███████ ██   ██ ██   ████ ██   ██  ██████  ██████         ██    ██   ██ ██   ██  ██████ ██   ██ ███████ ██   ██
 
-        <div class="wrapper">
-            <canvas id="canvas" style="position: fixed; z-index:-1"></canvas>
-            <nav class="navbar navbar-default navbar-fixed-top">
-                @include('application.partials.primaryHeader')
-            </nav>
-            <aside class="navigation">
-                @include('application.partials.navigation')
-            </aside>
+    </span>
 
-            <section class="search-results closed text-center"></section>
-            <section class="content">
-                @include('application.partials.alert')
-                @yield('content')
-            </section>
-        </div>
+    {!! Toastr::message() !!}
+
+    <div class="wrapper">
+        <canvas id="canvas" style="position: fixed; z-index:-1"></canvas>
+        <nav class="navbar navbar-default navbar-fixed-top">
+            @include('application.partials.primaryHeader')
+        </nav>
+        <aside class="navigation">
+            @include('application.partials.navigation')
+        </aside>
+
+        <section class="search-results closed text-center"></section>
+        <section class="content">
+            @include('application.partials.alert')
+            @yield('content')
+        </section>
+    </div>
     </body>
 
 @else
