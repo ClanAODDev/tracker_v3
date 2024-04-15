dc25ea9d89a90a9dd6a5ac80ac090d2a9050d095
Chris Deaton <dcdeaton@ncsu.edu>
2024-04-15 13:04:23 -0400

    Add trophies to anniversary badges

--- resources/views/division/partials/anniversaries.blade.php
+++ resources/views/division/partials/anniversaries.blade.php
@@ -8,6 +8,16 @@
         <a href="{{ doForumFunction([$anniversary->clan_id], 'forumProfile') }}&tab=myawards#myawards"
            class="btn btn-default" style="margin-bottom:15px;" target="_blank"
         >
+            @if($anniversary->years_since_joined >= 5)
+                <i class="fas fa-trophy fa-sm" style="color: #cd7f32;" title="5 Years"></i>
+            @elseif($anniversary->years_since_joined >= 10)
+                <i class="fas fa-trophy fa-sm" style="color: #C0C0C0;" title="10 Years"></i>
+            @elseif($anniversary->years_since_joined >= 15)
+                <i class="fas fa-trophy fa-md" style="color: #D4AF37;" title="15 Years"></i>
+            @elseif($anniversary->years_since_joined >= 20)
+                <i class="fas fa-trophy fa-lg" title="20 Years" style="color: #E5E4E2;"></i>
+            @endif
+
             {{ $anniversary->name }}
             <span class="label label-success text-uppercase" style="color:#000;">
                 {{ $anniversary->years_since_joined }} {{ Str::plural('yr', $anniversary->years_since_joined) }}
