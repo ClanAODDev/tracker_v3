c51a44b99ce46fb8a5c1901dd57e92fd143881e6
Chris Deaton <dcdeaton@ncsu.edu>
2024-04-17 22:24:28 -0400

    Require auth on doc routes

--- routes/partials/documentation.php
+++ routes/partials/documentation.php
@@ -1,26 +1,28 @@
 <?php
 
+Route::middleware('auth')->group(function () {
+
 // @TODO: Convert to markdown
-Route::get('changelog', 'AppController@changelog')->name('changelog');
-Route::group(['prefix' => 'help/docs'], function () {
-    
-    Route::get('/', 'HelpController@index')->name('help');
-    Route::get('/division-structures', 'HelpController@divisionStructures')->name('divisionStructures');
+    Route::get('changelog', 'AppController@changelog')->name('changelog');
+    Route::group(['prefix' => 'help/docs'], function () {
 
-
-    /**
-     * Admin documentation routes
-     *
-     * Routes start with /help/admin
-     */
-    Route::group(['prefix' => 'admin', 'middleware' => ['admin']], function () {
+        Route::get('/', 'HelpController@index')->name('help');
+        Route::get('/division-structures', 'HelpController@divisionStructures')->name('divisionStructures');
 
         /**
-         * New admin documentation routes should go here
+         * Admin documentation routes
+         *
+         * Routes start with /help/admin
          */
-        Route::view('division-checklist', 'help.admin.division-checklist')->name('help.admin.division-checklist');
-        Route::view('sink', 'help.admin.sink')->name('help.admin.sink');
-        Route::view('', 'help.admin.index')->name('help.admin.home');
+        Route::group(['prefix' => 'admin', 'middleware' => ['admin']], function () {
 
+            /**
+             * New admin documentation routes should go here
+             */
+            Route::view('division-checklist', 'help.admin.division-checklist')->name('help.admin.division-checklist');
+            Route::view('sink', 'help.admin.sink')->name('help.admin.sink');
+            Route::view('', 'help.admin.index')->name('help.admin.home');
+
+        });
     });
 });
