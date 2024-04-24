b8404d038e7ab57102721284b3b173215dff065b
manimal-prime <daniel.mccarrie@gmail.com>
2024-04-24 15:04:21 -0400

    Redesigned Sgts Training - MP

--- resources/views/training/partials/sgt/forum-moderation.blade.php
+++ resources/views/training/partials/sgt/forum-moderation.blade.php
@@ -1,33 +1,28 @@
-<h4>Forum Moderation</h4>
-<p>
-    Review how to make announcements on the forums.
-    Show them the Forum Manager link (a second way to post announcements (easier IMO)
-    Go over Search for users, Search IP addresses. Give examples of why you would use that
-    feature. Explain why you
-    would Edit AOD info (promotions, demotions). Remind them of the poor placement of the
-    "Remove Member" button and
-    to be careful.
-    Review the Ban User feature, Do not gloss over this. Spend some time discussing it.
-    Banning
-    is never an
-    emotional response. It is a tool to protect AOD from threats that could hinder the clan.
-    Make sure they
-    understand that they need to remove AOD membership status before banning. Make sure the
-    ban
-    is permanent and
-    that a reason is given for the ban. Discuss reversing a ban, and the does and don't.
-    When in
-    doubt seek higher
-    authority. Review a few bans in the Sgt section for appropriate structure.
-    Review award section and how to assign an award. Show them where the Request for new
-    Awards
-    sticky is in the Sgt
-    section.
+<h3>Forum Moderation</h3>
+<ul>
+    <h4>Review Basic Forum Moderation Functions</h4>
+    <p>
+    	<li>Go over Search for users: Remind them of the poor placement of the "Ban User" button and to be careful.</li>
+    	<li>Search IP addresses: Give examples of why you would use that feature.</li> 
+    	<li>Explain why/how you would Edit AOD info (promotions, demotions).</li>
+    </p>
+<h4>Review the Ban User feature, Do not gloss over this.</h4> 
+ 	
+    <p>
+ 			<li>Banning is never an emotional response. It is a tool to protect AOD from threats that could hinder the clan.</li>
+  			<li>Make sure they understand that they need to remove AOD membership status before banning.</li>
+  			<li>Make sure the ban is permanent and that a reason is given for the ban.</li> 
+  			<li>Discuss reversing a ban, and the do's and don't. When in doubt seek higher authority.</li>
+  			<li>Review some past bans in the Sgt section to ensure that they know the correct format:</li>
+		
+  			<p class="m-t-lg">
+    			<a href="https://www.clanaod.net/forums/forumdisplay.php?f=339"
+       			class="btn btn-primary"
+       			target="_blank"> AOD Ban List <i
+                class="fas fa-external-link-alt text-primary"></i>
+    			</a>
 </p>
-
-<p class="m-t-lg">
-    <a href="https://www.clanaod.net/forums/modcp/index.php"
-       class="btn btn-primary"
-       target="_blank"> Open ModCP <i class="fas fa-external-link-alt text-primary"></i>
-    </a>
+    <h4>Review award section and how to assign an award.</h4> 
+ <p>   	<li>Review the process on how to assign an award. Do a test having the Sgt award you an award and then revoke the award</li>
 </p>
+</ul>
--- resources/views/training/partials/sgt/sgt-structure.blade.php
+++ resources/views/training/partials/sgt/sgt-structure.blade.php
@@ -1,19 +1,24 @@
-<h4>SGT Structure</h4>
-<p>
-    Review both the Sgt structure and the SSgt lists below. Explain briefly what a SSgt's role
-    is. They should come away with an understanding that they are part of a highly organized structure, and that there
-    is always someone there to help if they get stuck. </p>
-<p>Be sure to plan time in the coming days and weeks to check on their progress.</p>
+<h3>SGT Structure & SSgts Duties</h3>
+<ul>
+    <h4>Review both the Sgt structure and the SSgt lists below.</h4>
+    
+ 	  <p><ul>This is a good link to reference who is a Sgt in AOD and what their role is:
 
-<p class="m-t-lg">
+    <p class="m-t-lg">
     <a href="https://tracker.clanaod.net/reports/leadership"
        class="btn btn-primary"
        target="_blank"> Open Leadership Structure <i
                 class="fas fa-external-link-alt text-primary"></i>
     </a>
-    <a href="https://www.clanaod.net/forums/showthread.php?t=56367"
-       class="btn btn-primary"
-       target="_blank"> Open SSgt Duties <i
-                class="fas fa-external-link-alt text-primary"></i>
-    </a>
-</p>
+    </ul></p>
+ 
+   <h4>Explain briefly what a SSgt's role is:</h4>
+    <li>SSgts are responsible for the onbaording and training of all newly promoted Sgts.</li>
+    <li>SSgts also monitor, edit and guide along the Division Request Forums. A SSgt should be driving the conversations around a new AOD division request and keep the forum posts updated and orderly</li>
+    <li>SSgts are experienced Sgts who typically will hold CO or XO tags in their division. They are the first stop for any Sgt who has questions about how to perform their tasks</li>
+    <li>Your trainee Sgt should come away from this conversation with an understanding that they are part of a highly organized structure, and that there is always someone there to help if they get stuck. Take ownership of their development and offer to help them if they have any questions</p>
+
+<h4>Be sure to plan time in the coming days and weeks to check on their progress.</h4>
+
+
+</ul>
