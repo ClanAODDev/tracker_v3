365c71d6e1fa4f983c8ed7123764eab997c1c4f8
Chris Deaton <dcdeaton@ncsu.edu>
2024-04-28 19:01:59 -0400

    Fix division routes

--- resources/views/platoon/partials/edit-platoon-button.blade.php
+++ resources/views/platoon/partials/edit-platoon-button.blade.php
@@ -1,11 +1,11 @@
 <div class="pull-right">
     @can('update', $platoon)
-        <a href="{{ route('platoon.manage-squads', [$division->abbreviation, $platoon]) }}" title="Manage squads"
+        <a href="{{ route('platoon.manage-squads', [?$division->abbreviation, $platoon]) }}" title="Manage squads"
            class="btn btn-default "><i class="fa fa-users text-accent"></i>
             <span class="hidden-xs hidden-sm">Manage Members</span>
         </a>
 
-        <a href="{{ route('editPlatoon', [$division->abbreviation, $platoon]) }}"
+        <a href="{{ route('editPlatoon', $division->slug, $platoon]) }}"
            title="Edit {{ $platoon->name }}" class="btn btn-default">
             <i class="fa fa-wrench text-accent"></i>
             <span class="hidden-xs hidden-sm">Manage {{ $division->locality('platoon') }}</span>
