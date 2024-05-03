d84f51390d40ef5e4a2e6692e396a4c1e07d2d91
Chris Deaton <dcdeaton@ncsu.edu>
2024-05-03 05:26:08 -0700

    Add voice activity to seeder

--- database/factories/MemberFactory.php
+++ database/factories/MemberFactory.php
@@ -30,6 +30,7 @@ class MemberFactory extends Factory
             'join_date' => $this->faker->dateTimeThisDecade,
             'last_activity' => $this->faker->dateTimeThisMonth,
             'last_ts_activity' => $this->faker->dateTimeThisMonth,
+            'last_voice_activity' => $this->faker->dateTimeThisMonth,
             'last_promoted_at' => $this->faker->dateTimeThisYear,
             'allow_pm' => array_rand([0, 1], 1),
             'privacy_flag' => array_rand([0, 1], 1),
