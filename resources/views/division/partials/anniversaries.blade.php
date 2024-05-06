4689edade87a818b06e4fa6d71798b4ab4693cc7
Chris Deaton <dcdeaton@ncsu.edu>
2024-05-06 09:31:44 -0400

    Formatting struggle bus

--- resources/views/division/partials/anniversaries.blade.php
+++ resources/views/division/partials/anniversaries.blade.php
@@ -8,28 +8,26 @@
         <a href="{{ doForumFunction([$anniversary->clan_id], 'forumProfile') }}&tab=myawards#myawards"
            class="btn btn-default" style="margin-bottom:15px;" target="_blank"
         >
-            
-        @php
-            
-            $iconClass = 'fas fa-trophy';
-            $iconColor = '#cd7f32';
-            $iconTitle = '5+ Years';
-        
-            if ($anniversary->years_since_joined >= 20) {
-                $iconClass .= ' fa-lg';
-                $iconColor = '#E5E4E2';
-                $iconTitle = '20+ Years';
-            } elseif ($anniversary->years_since_joined >= 15) {
-                $iconClass .= ' fa-md';
-                $iconColor = '#D4AF37';
-                $iconTitle = '15+ Years';
-            } elseif ($anniversary->years_since_joined >= 10) {
-                $iconClass .= ' fa-sm';
-                $iconColor = '#C0C0C0';
-                $iconTitle = '10+ Years';
-            }
-            
-        @endphp
+
+            @php
+                $iconClass = 'fas fa-trophy';
+                $iconColor = '#cd7f32';
+                $iconTitle = '5+ Years';
+
+                if ($anniversary->years_since_joined >= 20) {
+                    $iconClass .= ' fa-lg';
+                    $iconColor = '#E5E4E2';
+                    $iconTitle = '20+ Years';
+                } elseif ($anniversary->years_since_joined >= 15) {
+                    $iconClass .= ' fa-md';
+                    $iconColor = '#D4AF37';
+                    $iconTitle = '15+ Years';
+                } elseif ($anniversary->years_since_joined >= 10) {
+                    $iconClass .= ' fa-sm';
+                    $iconColor = '#C0C0C0';
+                    $iconTitle = '10+ Years';
+                }
+            @endphp
 
             <i class="{{ $iconClass }}" style="color: {{ $iconColor }};" title="{{ $iconTitle }}"></i>
 
