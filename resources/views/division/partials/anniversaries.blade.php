338ec823983d04d5e89657153d9db6475613cc15
Guybrush <flashadvocate@gmail.com>
2024-05-06 02:45:03 -0400

    Move anniversary logic
    Calculate actual anniversary difference on the repository so we don't have to keep the logic in the view

--- resources/views/division/partials/anniversaries.blade.php
+++ resources/views/division/partials/anniversaries.blade.php
@@ -8,21 +8,19 @@
         <a href="{{ doForumFunction([$anniversary->clan_id], 'forumProfile') }}&tab=myawards#myawards"
            class="btn btn-default" style="margin-bottom:15px;" target="_blank"
         >
-
-            <?php $anniversaryYears = $anniversary->years_since_joined + ((Carbon::parse($anniversary->join_date)->day > Carbon::now()->day) ? 1 : 0) ?>
-            @if($anniversaryYears >= 5)
+            @if($anniversary->years_since_joined >= 5)
                 <i class="fas fa-trophy fa-sm" style="color: #cd7f32;" title="5+ Years"></i>
-            @elseif($anniversaryYears >= 10)
+            @elseif($anniversary->years_since_joined >= 10)
                 <i class="fas fa-trophy fa-sm" style="color: #C0C0C0;" title="10+ Years"></i>
-            @elseif($anniversaryYears >= 15)
+            @elseif($anniversary->years_since_joined >= 15)
                 <i class="fas fa-trophy fa-md" style="color: #D4AF37;" title="15+ Years"></i>
-            @elseif($anniversaryYears >= 20)
+            @elseif($anniversary->years_since_joined >= 20)
                 <i class="fas fa-trophy fa-lg" title="20+ Years" style="color: #E5E4E2;"></i>
             @endif
 
             {{ $anniversary->name }}
             <span class="label label-success text-uppercase" style="color:#000;">
-                {{ $anniversaryYears }} {{ str()->plural('yr', $anniversaryYears) }}
+                {{ $anniversary->years_since_joined }} {{ str('yr')->plural($anniversary->years_since_joined) }}
             </span>
         </a>
     @endforeach
