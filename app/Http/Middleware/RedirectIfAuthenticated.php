0c70ab32f422ae7c09f21968317107291eecfad3
Chris Deaton <dcdeaton@ncsu.edu>
2024-07-08 11:06:47 -0400

    Clean up middleware

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,6 +6,7 @@ use Closure;
 use Facades\App\AOD\ClanForumSession;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class RedirectIfAuthenticated
 {
