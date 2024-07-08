bcce44b41edb30dbc39d724c1b8dd4a0f3805cc4
Chris Deaton <dcdeaton@ncsu.edu>
2024-07-08 15:47:16 -0400

    Actually fix colors

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Red,
-                'secondary' => Color::Gray,
+                'primary' => '#f6a821',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
