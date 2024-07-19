32547bed88545459d41b9f80b45a5a967a3170d3
Chris Deaton <dcdeaton@ncsu.edu>
2024-07-19 08:58:06 -0400

    Laravel 11 upgrade

--- config/sanctum.php
+++ config/sanctum.php
@@ -59,7 +59,7 @@ return [
 
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
-        'verify_csrf_token' => App\Http\Middleware\VerifyCsrfToken::class,
-        'encrypt_cookies' => App\Http\Middleware\EncryptCookies::class,
+        'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
+        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
 ];
