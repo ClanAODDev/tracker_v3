550e063a7f2f5b8bac47f4cde6c1896a84edff52
Chris Deaton <dcdeaton@ncsu.edu>
2024-09-22 09:19:42 -0400

    Cleanup

--- resources/assets/js/voice.js
+++ resources/assets/js/voice.js
@@ -1,30 +1,16 @@
 if ($('.members-table').length) {
     var dataTable = $('table.members-table').DataTable({
-        'initComplete': function (settings, json) {
-            setTimeout(function () {
-                $('.ld-loading').removeClass('ld-loading');
-            }, 2000);
-        },
         oLanguage: {
             sLengthMenu: '' // _MENU_
+        }, columnDefs: [{
+            orderable: false, className: 'select-checkbox', targets: 0
+        }, {
+            targets: 'col-hidden', visible: false
         },
-        columnDefs: [{
-            orderable: false,
-            className: 'select-checkbox',
-            targets: 0
-        },
-            {
-                targets: 'col-hidden', visible: false
-            },
 
-        ],
-        select: {
-            style: 'os',
-            selector: 'td:first-child',
-        },
-        stateSave: true, paging: false,
-        autoWidth: true, bInfo: false,
-        searching: false, info: false,
+        ], select: {
+            style: 'os', selector: 'td:first-child',
+        }, stateSave: true, paging: false, autoWidth: true, bInfo: false, searching: false, info: false,
     });
     // handle PM selection
     dataTable.on("select", function (e, t, a, d) {
@@ -32,9 +18,7 @@ if ($('.members-table').length) {
             return e[6]
         });
         if (l.length >= 2) {
-            $("#selected-data").show(),
-                $("#selected-data .status-text").text("With selected (" + l.length + ")"),
-                $("#pm-member-data").val(l);
+            $("#selected-data").show(), $("#selected-data .status-text").text("With selected (" + l.length + ")"), $("#pm-member-data").val(l);
         }
     });
 }
