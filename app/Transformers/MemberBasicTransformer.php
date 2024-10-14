2cf738fcea7324184db5acecf51c77463645d52a
Chris Deaton <dcdeaton@ncsu.edu>
2024-10-13 20:14:51 -0400

    Expand rank information in member info

--- app/Transformers/MemberBasicTransformer.php
+++ app/Transformers/MemberBasicTransformer.php
@@ -13,7 +13,10 @@ class MemberBasicTransformer extends Transformer
         return [
             'name' => $item->name,
             'position' => $item->position->getLabel(),
-            'rank' => $item->rank->getAbbreviation(),
+            'rank' => [
+                'label' => $item->rank->getAbbreviation(),
+                'color' => $item->rank->colorHex()
+            ],
             'discord_id' => (string) $item->discord_id,
         ];
     }
