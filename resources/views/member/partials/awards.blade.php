65821bdf42f383f2c6b1d184b8cc8b95a79da22e
Chris Deaton <dcdeaton@ncsu.edu>
2024-12-21 17:33:05 -0500

    View cleanup

--- resources/views/member/partials/awards.blade.php
+++ resources/views/member/partials/awards.blade.php
@@ -12,34 +12,20 @@
     <div class="row">
         <div class="row">
             @foreach ($member->awards->sortBy('display_order') as $index => $record)
-                    <div class="col-lg-4">
-                        <div class="panel panel-filled">
-                            <div class="panel-body" style="overflow: hidden"  title="{{ $record->award->description }}">
-                                <img src="{{ asset(Storage::url($record->award->image)) }}" alt="Award"
-                                     class="align-self-start">
-                                <p style="text-align: right;" class="pull-right text-nowrap">
-                                    {{ $record->award->name }} <br/>
-                                    <span class="text-muted">Earned {{ $record->created_at->format('M d, Y') }}</span>
-                                </p>
-                            </div>
+                <div class="col-lg-4">
+                    <a href="#" class="panel panel-filled">
+                        <div class="panel-body" style="overflow: hidden" title="{{ $record->award->description }}">
+                            <img src="{{ asset(Storage::url($record->award->image)) }}"
+                                 alt="{{ $record->award->name }}"
+                                 class="align-self-start">
+                            <p style="text-align: right;" class="pull-right text-nowrap">
+                                <span class="c-white">{{ $record->award->name }}</span> <br/>
+                                Earned {{ $record->created_at->format('M d, Y') }}
+                            </p>
                         </div>
-                    </div>
+                    </a>
+                </div>
             @endforeach
         </div>
-
-        {{--        @foreach ($member->awards as $record)--}}
-        {{--            <div class="col-lg-4">--}}
-        {{--                <div class="panel panel-filled ">--}}
-        {{--                <div class="panel-body" style="overflow: hidden">--}}
-        {{--                        <img src="{{ asset(Storage::url($record->award->image)) }}" alt="Award" class="align-self-start">--}}
-        {{--                        <p style="text-align: right;" class="pull-right  text-nowrap">{{--}}
-        {{--                        $record->award->name--}}
-        {{--                         }} <br />--}}
-        {{--                            <span class="text-muted">Earned {{ $record->created_at->format('M d, Y') }}</span>--}}
-        {{--                        </p>--}}
-        {{--                </div>--}}
-        {{--                </div>--}}
-        {{--            </div>--}}
-        {{--        @endforeach--}}
     </div>
 @endif
