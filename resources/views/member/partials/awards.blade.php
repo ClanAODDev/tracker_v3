ddc3792fd2bcb451f45ace1b69319e6e3e904de5
Chris Deaton <dcdeaton@ncsu.edu>
2024-12-21 21:04:30 -0500

    Award display tweaks

--- resources/views/member/partials/awards.blade.php
+++ resources/views/member/partials/awards.blade.php
@@ -12,16 +12,17 @@
     <div class="row">
         <div class="row">
             @foreach ($member->awards->sortBy('display_order') as $index => $record)
-                <div class="col-lg-4">
-                    <a href="#" class="panel panel-filled">
-                        <div class="panel-body" style="overflow: hidden" title="{{ $record->award->description }}">
+                <div class="col-lg-3">
+                    <a href="#" class="btn btn-default btn-block" style="margin-bottom:20px;">
+                        <div class="panel-body" title="{{ $record->award->description }}">
                             <img src="{{ asset(Storage::url($record->award->image)) }}"
                                  alt="{{ $record->award->name }}"
-                                 class="align-self-start">
-                            <p style="text-align: right;" class="pull-right text-nowrap">
+                                 style="overflow: hidden"
+                                 class="align-self-start pull-left">
+                            <small style="text-align: right; padding-top:10px; " class="pull-right">
                                 <span class="c-white">{{ $record->award->name }}</span> <br/>
-                                Earned {{ $record->created_at->format('M d, Y') }}
-                            </p>
+                                <span class="text-muted">Earned {{ $record->created_at->format('M d, Y') }}</span>
+                            </small>
                         </div>
                     </a>
                 </div>
