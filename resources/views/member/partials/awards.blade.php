e5d462748044916cbb7e09e9483661ec98e15b8e
Chris Deaton <dcdeaton@ncsu.edu>
2024-12-22 14:45:56 -0500

    Fix typo'd element

--- resources/views/member/partials/awards.blade.php
+++ resources/views/member/partials/awards.blade.php
@@ -10,27 +10,27 @@
     </h4>
     <hr/>
     <div class="row">
-        <div class="row">
-            @foreach ($member->awards->sortBy('display_order') as $index => $record)
-                <div class="col col-lg-3 col-xl-2 col-sm-6">
-                    <a href="#" class="btn btn-default btn-block" style="margin-bottom:20px;">
-                        <div class="panel-body award-item" title="{{ $record->award->description }}">
-                            <img src="{{ asset(Storage::url($record->award->image)) }}"
-                                 alt="{{ $record->award->name }}"
-                                 class="clan-award"
-                            />
 
-                            <div class="col-xs-9 p-0"
-                                 style="height:100%;display:flex;align-items: center;justify-content: center;">
-                                <div class="text-align: right;">
-                                    <span class="c-white">{{ $record->award->name }}</span> <br/>
-                                    <span class="text-muted">{{ $record->created_at->format('M d, Y') }}</span>
-                                </div>
+        @foreach ($member->awards->sortBy('display_order') as $index => $record)
+            <div class="col-lg-3 col-xl-2 col-sm-6">
+                <a href="{{ route('awards.show', $record->award) }}" class="btn btn-default btn-block"
+                   style="margin-bottom:20px;">
+                    <div class="panel-body award-item" title="{{ $record->award->description }}">
+                        <img src="{{ asset(Storage::url($record->award->image)) }}"
+                             alt="{{ $record->award->name }}"
+                             class="clan-award"
+                        />
+                        <div class="col-xs-9 p-0"
+                             style="height:100%;display:flex;align-items: center;justify-content: center;">
+                            <div class="text-align: right;">
+                                <span class="c-white">{{ $record->award->name }}</span> <br/>
+                                <span class="text-muted">{{ $record->created_at->format('M d, Y') }}</span>
                             </div>
                         </div>
-                    </a>
-                </div>
-            @endforeach
-        </div>
+                    </div>
+                </a>
+            </div>
+        @endforeach
     </div>
+
 @endif
