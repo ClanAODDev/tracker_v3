3a319db4f7bd8571635c719d55ccc7349e02e757
Chris Deaton <dcdeaton@ncsu.edu>
2024-12-24 18:19:59 -0500

    Add Member Award relation manager

--- app/Filament/Admin/Resources/MemberResource.php
+++ app/Filament/Admin/Resources/MemberResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Admin\Resources;
 use App\Enums\DiscordStatus;
 use App\Enums\Position;
 use App\Enums\Rank;
+use App\Filament\Admin\Resources\MemberHasManyAwardsResource\RelationManagers\AwardsRelationManager;
 use App\Filament\Admin\Resources\MemberResource\Pages;
 use App\Models\Member;
 use Filament\Forms;
@@ -225,6 +226,7 @@ class MemberResource extends Resource
     public static function getRelations(): array
     {
         return [
+            AwardsRelationManager::class,
         ];
     }
 
