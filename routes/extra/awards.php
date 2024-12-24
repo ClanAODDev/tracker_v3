bba59522b8bac203541930c59eef9f673bf7c3a6
Chris Deaton <dcdeaton@ncsu.edu>
2024-12-23 20:46:29 -0500

    Add award cluster generation

--- routes/extra/awards.php
+++ routes/extra/awards.php
@@ -229,3 +229,69 @@ function gracefulFail()
         abort(500, 'Broken image not found.');
     }
 }
+
+Route::get('members/{member}/my-awards-cluster.png', function (Member $member) {
+    $awards = \App\Models\MemberAward::where('member_id', $member->clan_id)
+        ->orderByDesc('created_at')
+        ->get()
+        ->take(6)
+        ->pluck('award.image')
+        ->toArray();
+
+    $awardWidth = 60;
+    $awardHeight = 60;
+    $columns = 3;
+    $rows = ceil(count($awards) / $columns);
+    $padding = 10;
+
+    $baseWidth = ($columns * $awardWidth) + (($columns - 1) * $padding);
+    $baseHeight = ($rows * $awardHeight) + (($rows - 1) * $padding);
+
+    $baseImage = imagecreatetruecolor($baseWidth, $baseHeight);
+
+    imagesavealpha($baseImage, true);
+    $transparentColor = imagecolorallocatealpha($baseImage, 0, 0, 0, 127);
+    imagefill($baseImage, 0, 0, $transparentColor);
+
+    $x = 0;
+    $y = 0;
+    $counter = 0;
+
+    foreach ($awards as $awardPath) {
+        $awardImagePath = Storage::path('public/' . $awardPath);
+
+        if (file_exists($awardImagePath)) {
+            $awardImage = imagecreatefrompng($awardImagePath);
+
+            imagesavealpha($awardImage, true);
+
+            imagecopyresampled(
+                $baseImage,
+                $awardImage,
+                $x,
+                $y,
+                0,
+                0,
+                $awardWidth,
+                $awardHeight,
+                imagesx($awardImage),
+                imagesy($awardImage)
+            );
+
+            imagedestroy($awardImage);
+        }
+
+        $x += $awardWidth + $padding;
+        $counter++;
+
+        if ($counter % $columns == 0) {
+            $x = 0;
+            $y += $awardHeight + $padding;
+        }
+    }
+
+    header('Content-Type: image/png');
+    imagepng($baseImage);
+    imagedestroy($baseImage);
+
+});
