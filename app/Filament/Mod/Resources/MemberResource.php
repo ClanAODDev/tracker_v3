af2ebc4addd86ec5ef5b8724b189eeb68a625825
Chris Deaton <dcdeaton@ncsu.edu>
2024-12-24 18:44:47 -0500

    Pint

--- app/Filament/Mod/Resources/MemberResource.php
+++ app/Filament/Mod/Resources/MemberResource.php
@@ -229,7 +229,7 @@ class MemberResource extends Resource
     public static function getRelations(): array
     {
         return [
-            AwardsRelationManager::class
+            AwardsRelationManager::class,
         ];
     }
 
