38e3505de3a213271bd45af8db982404f6a7998f
Chris Deaton <dcdeaton@ncsu.edu>
2025-01-11 19:40:59 -0500

    Fix naughty trainer field Also update trained_at date when trainer changes

--- app/Filament/Mod/Resources/MemberResource/Pages/EditMember.php
+++ app/Filament/Mod/Resources/MemberResource/Pages/EditMember.php
@@ -5,10 +5,21 @@ namespace App\Filament\Mod\Resources\MemberResource\Pages;
 use App\Filament\Mod\Resources\MemberResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditMember extends EditRecord
 {
     protected static string $resource = MemberResource::class;
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        if ($record->isDirty('last_trained_by')) {
+            $data['last_trained_at'] = now();
+            $record->update($data);
+        }
+
+        return $record;
+    }
 
     protected function getHeaderActions(): array
     {
