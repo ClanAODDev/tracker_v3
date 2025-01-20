67ce20dad15c1a3e3f031cd0d5cffd2b6de8e42b
Chris Deaton <dcdeaton@ncsu.edu>
2025-01-20 14:56:28 -0500

    Show award image

--- app/Notifications/MemberAwarded.php
+++ app/Notifications/MemberAwarded.php
@@ -4,16 +4,18 @@ namespace App\Notifications;
 
 use App\Channels\BotChannel;
 use App\Channels\Messages\BotChannelMessage;
+use App\Models\Award;
 use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Storage;
 
 class MemberAwarded extends Notification implements ShouldQueue
 {
     use Queueable;
 
-    public function __construct(private readonly string $member, private string $award) {}
+    public function __construct(private readonly string $member, private readonly Award $award) {}
 
     /**
      * Get the notification's delivery channels.
@@ -36,8 +38,8 @@ class MemberAwarded extends Notification implements ShouldQueue
         return (new BotChannelMessage($notifiable))
             ->title($notifiable->name . ' Division')
             ->target($notifiable->settings()->get('chat_alerts.member_awarded'))
-            ->thumbnail($notifiable->getLogoPath())
-            ->message(sprintf('%s received an award: %s', $this->member, $this->award))
+            ->thumbnail(Storage::url($this->award->image))
+            ->message(sprintf('%s received an award: %s', $this->member, $this->award->name))
             ->info()
             ->send();
     }
