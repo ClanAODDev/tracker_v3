e2341548757aafd3db411a8516d93104adec2032
Chris Deaton <dcdeaton@ncsu.edu>
2025-01-24 11:02:02 -0500

    Convert to class-based reference

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,10 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DivisionCensus;
+use App\Console\Commands\FetchApplicationFeeds;
+use App\Console\Commands\MemberSync;
+use App\Console\Commands\PartTimeMemberCleanup;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,10 +28,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('do:fetch-application-feeds')->everyTenMinutes();
-        $schedule->command('do:membersync')->hourly();
-        $schedule->command('do:divisioncensus')->weekly();
-        $schedule->command('do:part-time-member-cleanup')->weekly();
+        $schedule->command(FetchApplicationFeeds::class)->everyTenMinutes();
+        $schedule->command(MemberSync::class)->hourly();
+        $schedule->command(DivisionCensus::class)->weekly();
+        $schedule->command(PartTimeMemberCleanup::class)->weekly();
     }
 
     /**
