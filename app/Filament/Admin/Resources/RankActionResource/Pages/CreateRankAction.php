8cb6ad8ee46fc3d4387fe3f9b8ae619629153b58
Chris Deaton <dcdeaton@ncsu.edu>
2025-02-04 08:46:21 -0500

    Forum update, auto accept, approve on admin rank actions

--- app/Filament/Admin/Resources/RankActionResource/Pages/CreateRankAction.php
+++ app/Filament/Admin/Resources/RankActionResource/Pages/CreateRankAction.php
@@ -3,9 +3,30 @@
 namespace App\Filament\Admin\Resources\RankActionResource\Pages;
 
 use App\Filament\Admin\Resources\RankActionResource;
+use App\Jobs\UpdateRankForMember;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateRankAction extends CreateRecord
 {
     protected static string $resource = RankActionResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['approved_at'] = now();
+        $data['accepted_at'] = now();
+
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        $record = $this->record;
+
+        try {
+            UpdateRankForMember::dispatch($record);
+        } catch (\Exception $exception) {
+            \Log::error($exception->getMessage());
+        }
+    }
+
 }
