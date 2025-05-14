761c71d7571712ee1cd76325297aaa3d7196c6a4
Chris Deaton <dcdeaton@ncsu.edu>
2025-05-14 03:49:28 -0400

    Use proper `do=` action Deeplinking to quickreply doesn't work because the form is hidden initially - this used to be an html injection vulnerability!

--- resources/views/recruit/form.blade.php
+++ resources/views/recruit/form.blade.php
@@ -20,5 +20,5 @@
 @endsection
 
 @section('footer_scripts')
-    <script src="{!! asset('/js/recruiting.js?v=7.51') !!}"></script>
+    <script src="{!! asset('/js/recruiting.js?v=7.52') !!}"></script>
 @endsection
