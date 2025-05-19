353b27aaa8040c800a539b863d6dd0333d1773ad
Chris Deaton <dcdeaton@ncsu.edu>
2025-05-19 19:58:49 -0400

    Manage position when platoon, squad leader are assigned Also deprecates older platoon, squad edit forms in favor of Filament resources
    Assigning a leader will set the position and squad/platoon id of the leader. Any existing leader will be reduced to `Position::MEMBER` but will remain in their current organization
    
    Closes #501

--- app/Filament/Mod/Resources/SquadResource/Pages/EditSquad.php
+++ app/Filament/Mod/Resources/SquadResource/Pages/EditSquad.php
@@ -2,12 +2,55 @@
 
 namespace App\Filament\Mod\Resources\SquadResource\Pages;
 
+use App\Enums\Position;
 use App\Filament\Mod\Resources\SquadResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
 class EditSquad extends EditRecord
 {
+    public function getRedirectUrl(): ?string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    public function getTitle(): string
+    {
+        return sprintf('Edit %s', $this->record->division->locality('Squad'));
+    }
+
+    public function mount($record): void
+    {
+        parent::mount($record);
+
+        $this->form->fill([
+            ...$this->form->getState(),
+            'original_leader_id' => $this->record->leader_id,
+        ]);
+    }
+
+    protected function afterSave(): void
+    {
+        $state = $this->form->getState();
+
+        $originalLeaderId = $state['original_leader_id'] ?? null;
+        $newLeaderId = (int) $this->record->leader_id;
+
+        if ($originalLeaderId !== $newLeaderId) {
+            if ($newLeaderId) {
+                \App\Models\Member::where('clan_id', $newLeaderId)?->update([
+                    'position' => \App\Enums\Position::SQUAD_LEADER,
+                    'platoon_id' => $this->record->platoon_id,
+                    'squad_id' => $this->record->id,
+                ]);
+            }
+
+            \App\Models\Member::where('clan_id', $originalLeaderId)?->update([
+                'position' => Position::MEMBER,
+            ]);
+        }
+    }
+
     protected static string $resource = SquadResource::class;
 
     protected function getHeaderActions(): array
