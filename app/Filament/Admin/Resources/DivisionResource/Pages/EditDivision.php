84f72d9fca058bb6936268b7d7fb46f05c2b9794
Chris Deaton <dcdeaton@ncsu.edu>
2025-05-25 18:05:56 -0400

    Move division leadership to admin

--- app/Filament/Admin/Resources/DivisionResource/Pages/EditDivision.php
+++ app/Filament/Admin/Resources/DivisionResource/Pages/EditDivision.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Admin\Resources\DivisionResource\Pages;
 
+use App\Enums\Position;
 use App\Filament\Admin\Resources\DivisionResource;
+use App\Models\Member;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Mansoor\FilamentVersionable\Page\RevisionsAction;
@@ -17,5 +19,77 @@ class EditDivision extends EditRecord
             Actions\DeleteAction::make(),
             RevisionsAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $divisionId = $this->record->id;
+
+        if (isset($data['new_co'])) {
+            $data = $this->handleNewCO($divisionId, $data);
+        }
+
+        $data = $this->handleXOs($divisionId, $data);
+
+        unset($data['executive_officers'], $data['new_co']);
+
+        return $data;
+    }
+
+    protected function handleNewCO(mixed $divisionId, array $data): array
+    {
+        $previousCoId = Member::where('division_id', $divisionId)
+            ->where('position', Position::COMMANDING_OFFICER)
+            ->value('id');
+
+        $newCoId = $data['new_co'];
+
+        if ($previousCoId && $previousCoId !== $newCoId) {
+            Member::where('id', $previousCoId)
+                ->update([
+                    'position' => Position::MEMBER]);
+            Member::where('id', $newCoId)
+                ->update([
+                    'position' => Position::COMMANDING_OFFICER,
+                    'platoon_id' => 0,
+                    'squad_id' => 0,
+                ]);
+        } elseif (! $previousCoId) {
+            Member::where('id', $newCoId)
+                ->update([
+                    'position' => Position::COMMANDING_OFFICER,
+                    'platoon_id' => 0,
+                    'squad_id' => 0,
+                ]);
+        }
+
+        return $data;
+    }
+
+    protected function handleXOs(mixed $divisionId, array $data): array
+    {
+        $previousXos = Member::where('division_id', $divisionId)
+            ->where('position', Position::EXECUTIVE_OFFICER)
+            ->pluck('id');
+
+        $newXos = collect($data['executive_officers'] ?? [])->pluck('xo');
+
+        $toRemove = $previousXos->diff($newXos);
+        if ($toRemove->isNotEmpty()) {
+            Member::whereIn('id', $toRemove)->update([
+                'position' => Position::MEMBER]);
+        }
+
+        $toAdd = $newXos->diff($previousXos);
+        if ($toAdd->isNotEmpty()) {
+            Member::whereIn('id', $toAdd)
+                ->update([
+                    'position' => Position::EXECUTIVE_OFFICER,
+                    'platoon_id' => 0,
+                    'squad_id' => 0,
+                ]);
+        }
+
+        return $data;
     }
 }
--- app/Filament/Mod/Resources/DivisionResource/Pages/EditDivision.php
+++ app/Filament/Mod/Resources/DivisionResource/Pages/EditDivision.php
@@ -2,9 +2,7 @@
 
 namespace App\Filament\Mod\Resources\DivisionResource\Pages;
 
-use App\Enums\Position;
 use App\Filament\Mod\Resources\DivisionResource;
-use App\Models\Member;
 use App\Notifications\Channel\NotifyDivisionSettingsEdited;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
@@ -20,23 +18,7 @@ class EditDivision extends EditRecord
         return [
             Actions\DeleteAction::make(),
             RevisionsAction::make(),
-
         ];
-    }
-
-    protected function mutateFormDataBeforeSave(array $data): array
-    {
-        $divisionId = $this->record->id;
-
-        if (isset($data['new_co'])) {
-            $data = $this->handleNewCO($divisionId, $data);
-        }
-
-        $data = $this->handleXOs($divisionId, $data);
-
-        unset($data['executive_officers'], $data['new_co']);
-
-        return $data;
     }
 
     protected function handleRecordUpdate(Model $record, array $data): Model
@@ -45,62 +27,5 @@ class EditDivision extends EditRecord
         $record->notify(new NotifyDivisionSettingsEdited(auth()->user()->name));
 
         return $record;
-    }
-
-    protected function handleNewCO(mixed $divisionId, array $data): array
-    {
-        $previousCoId = Member::where('division_id', $divisionId)
-            ->where('position', Position::COMMANDING_OFFICER)
-            ->value('id');
-
-        $newCoId = $data['new_co'];
-
-        if ($previousCoId && $previousCoId !== $newCoId) {
-            Member::where('id', $previousCoId)
-                ->update([
-                    'position' => Position::MEMBER]);
-            Member::where('id', $newCoId)
-                ->update([
-                    'position' => Position::COMMANDING_OFFICER,
-                    'platoon_id' => 0,
-                    'squad_id' => 0,
-                ]);
-        } elseif (! $previousCoId) {
-            Member::where('id', $newCoId)
-                ->update([
-                    'position' => Position::COMMANDING_OFFICER,
-                    'platoon_id' => 0,
-                    'squad_id' => 0,
-                ]);
-        }
-
-        return $data;
-    }
-
-    protected function handleXOs(mixed $divisionId, array $data): array
-    {
-        $previousXos = Member::where('division_id', $divisionId)
-            ->where('position', Position::EXECUTIVE_OFFICER)
-            ->pluck('id');
-
-        $newXos = collect($data['executive_officers'] ?? [])->pluck('xo');
-
-        $toRemove = $previousXos->diff($newXos);
-        if ($toRemove->isNotEmpty()) {
-            Member::whereIn('id', $toRemove)->update([
-                'position' => Position::MEMBER]);
-        }
-
-        $toAdd = $newXos->diff($previousXos);
-        if ($toAdd->isNotEmpty()) {
-            Member::whereIn('id', $toAdd)
-                ->update([
-                    'position' => Position::EXECUTIVE_OFFICER,
-                    'platoon_id' => 0,
-                    'squad_id' => 0,
-                ]);
-        }
-
-        return $data;
     }
 }
