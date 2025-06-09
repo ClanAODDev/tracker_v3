a33afabed4d826a9161a681fee4bbd3c8c707c6f
Guybrush <flashadvocate@gmail.com>
2025-06-09 13:15:45 -0400

    Notices, policy fixes (#520)
    * Add division pending transfer notice
    * Hide "transfer from" placeholder unless member set
    * Add custom `flag-inactive` permission
    * Fix permissions bug

--- resources/views/division/partials/notices.blade.php
+++ resources/views/division/partials/notices.blade.php
@@ -1,78 +1,77 @@
 @if ($division->isShutdown())
     <div class="alert alert-danger">
-        @if ($division->shut_down_at > now())
-            <i class="fa fa-exclamation-circle"></i> This division was shut down on
-            <strong>{{ $division->shutdown_at->format('Y-m-d') }}</strong>. Assigned members should find new divisions
-            or be removed immediately.
+        <i class="fa fa-exclamation-circle"></i>
+        @if ($division->shutdown_at > now())
+            This division will be shut down on <strong>{{ $division->shutdown_at->format('Y-m-d') }}</strong>. Assigned members should begin looking for new divisions.
         @else
-            <i class="fa fa-exclamation-circle"></i> This division will be shut down on
-            <strong>{{ $division->shutdown_at->format('Y-m-d') }}</strong>. Assigned members should begin looking for
-            new divisions.
+            This division was shut down on <strong>{{ $division->shutdown_at->format('Y-m-d') }}</strong>. Assigned members should find new divisions or be removed immediately.
         @endif
     </div>
 @endif
 
+{{-- pending member requests --}}
 @can('manage', \App\Models\MemberRequest::class)
-    @if ($count = $division->memberRequests()->cancelled()->count())
+    @php
+        $cancelledCount = $division->memberRequests()->cancelled()->count();
+        $pendingCount   = $division->memberRequests()->pending()->count();
+    @endphp
+
+    @if ($cancelledCount)
         <div class="alert alert-warning">
-            There are <code>{{ $count }}</code> {{ Str::plural('member request', $count) }} in need of attention. <a
-                    class="alert-link pull-right"
-                    href="{{ route('admin.member-request.index') }}"
-            >Manage Member Requests</a>
+            <i class="fa fa-exclamation-triangle"></i>
+            There {{ $cancelledCount === 1 ? 'is' : 'are' }} <code>{{ $cancelledCount }}</code> cancelled {{ Str::plural('member request', $cancelledCount) }} needing attention.
+            <a href="{{ route('admin.member-request.index') }}" class="alert-link pull-right">Manage Member Requests</a>
         </div>
-    @elseif($count = $division->memberRequests()->pending()->count())
+    @elseif ($pendingCount)
         <div class="alert alert-warning">
-            There are <code>{{ $count }}</code> {{ Str::plural('member request', $count) }} pending.
-            <a class="alert-link pull-right"
-               href="{{ route('admin.member-request.index') }}"
-            >Manage Member Requests</a>
+            <i class="fa fa-exclamation-circle"></i>
+            There {{ $pendingCount === 1 ? 'is' : 'are' }} <code>{{ $pendingCount }}</code> pending {{ Str::plural('member request', $pendingCount) }}.
+            <a href="{{ route('admin.member-request.index') }}" class="alert-link pull-right">Manage Member Requests</a>
         </div>
     @endif
 @endcan
 
-@if($division->outstandingInactives && auth()->user()->isRole('sr_ldr'))
+{{-- pending award requests --}}
+@if ($division->outstandingInactives && auth()->user()->isRole('sr_ldr'))
     <div class="alert alert-default">
-        There are
-        <code>{{ $division->outstandingInactives }}</code> outstanding
-        inactive {{ Str::plural('member', $division->outstandingInactives) }}. AOD does not allow divisions to maintain
-        members whose last activity exceeds <code>{{ config('app.aod.maximum_days_inactive') }}</code> days
-        except where a leave of absence exists. Please
-        <a href="{{ route('division.inactive-members', $division) }}">process these members</a> out of AOD.
+        <i class="fa fa-user-clock"></i>
+        There {{ $division->outstandingInactives === 1 ? 'is' : 'are' }} <code>{{ $division->outstandingInactives }}</code> inactive {{ Str::plural('member', $division->outstandingInactives) }} whose last activity exceeds <code>{{ config('app.aod.maximum_days_inactive') }}</code> days. Please <a href="{{ route('division.inactive-members', $division) }}" class="alert-link">process these members</a> out of AOD.
     </div>
 @endif
 
-@if($division->outstandingAwardRequests && auth()->user()->isDivisionLeader())
+{{-- pending transfers --}}
+@if ($division->outstandingAwardRequests && auth()->user()->isDivisionLeader())
     <div class="alert alert-default">
-        <i class="fa fa-trophy fa-lg c-white"></i> Pending award requests for approval.
-
-        <a href="{{ route('filament.mod.resources.member-awards.index') . reviewDivisionAwardsQuery($division->id) }}">Manage Requests</a>
+        <i class="fa fa-trophy"></i>
+        There {{ $division->outstandingAwardRequests === 1 ? 'is' : 'are' }} <code>{{ $division->outstandingAwardRequests }}</code> pending award {{ Str::plural('request', $division->outstandingAwardRequests) }} for approval.
+        <a href="{{ route('filament.mod.resources.member-awards.index') . reviewDivisionAwardsQuery($division->id) }}" class="alert-link">Manage Requests</a>
     </div>
 @endif
 
+{{-- misconfigured comms --}}
 @if ($division->members()->misconfiguredDiscord()->count() && auth()->user()->isRole(['sr_ldr', 'jr_ldr']))
     <div class="alert alert-default">
-        There are assigned members with voice comms issues. Please review the
-        <a href="{{ route('division.voice-report', $division->slug) }}">Voice Comms Report</a>.
+        <i class="fa fa-comments"></i>
+        There {{ $division->members()->misconfiguredDiscord()->count() === 1 ? 'is' : 'are' }} member{{ $division->members()->misconfiguredDiscord()->count() === 1 ? '' : 's' }} with voice‐comms issues. Please review the <a href="{{ route('division.voice-report', $division->slug) }}" class="alert-link">Voice Comms Report</a>.
     </div>
 @endif
 
-@if (! $division->handle)
-    <div class="alert alert-default">
-        Your division does not have a primary handle. Contact clan leadership to resolve this.
-    </div>
-@endif
-
+{{-- unassigned members --}}
 @if (count($division->unassigned) > 0)
-    {{-- only show notice if user is capable of taking action --}}
     @can('create', [App\Models\Platoon::class, $division])
         <div class="alert alert-default">
-            <i class="fa fa-users text-info"></i>
-            There are
-            <code>{{ count($division->unassigned) }}</code>
-            unassigned {{ Str::plural('member', count($division->unassigned)) }} in
-            <strong>{{ $division->name }}</strong>. Drag members into a
-            <a href="{{ route('division', $division->slug) }}/#platoons"
-               class="alert-link">{{ $division->locality('platoon') }}</a> to assign them
+            <i class="fa fa-users"></i>
+            There {{ count($division->unassigned) === 1 ? 'is' : 'are' }} <code>{{ count($division->unassigned) }}</code> unassigned {{ Str::plural('member', count($division->unassigned)) }}  <strong>{{ $division->name }}</strong>. Drag them into a <a href="{{ route('division', $division->slug) }}/#platoons" class="alert-link">{{ $division->locality('platoon') }}</a> to assign.
         </div>
     @endcan
+@endif
+
+{{-- pending transfers --}}
+@if (count($pendingTransfers = $division->transfers()->pending()->get()) > 0 && auth()->user()->isDivisionLeader())
+        <div class="alert alert-default">
+            <i class="fas fa-exchange-alt"></i>
+            There {{ count($pendingTransfers) === 1 ? 'is' : 'are' }} <code>{{ count($pendingTransfers) }}</code>
+            pending incoming {{ Str::plural('transfer', count($pendingTransfers)) }}.
+            <a class="alert-link" href="{{ route('filament.mod.resources.transfers.index')}}?tableFilters[incomplete][isActive]=true&tableFilters[transferring_to][value]={{ $division->id }}">Manage incoming transfers</a>
+        </div>
 @endif
