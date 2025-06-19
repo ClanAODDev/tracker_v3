d36167e409c6e1010de872248f1b258a7c5c6eab
Guybrush <flashadvocate@gmail.com>
2025-06-19 11:05:21 -0400

    Release dev (#527)
    * Make recruitment feed nullable
      * Fixes issue with BR division settings
    * Track rank action approvers
      * Approver field on historical rank actions will be hidden unless an approver is set
    * Security updates for software dependencies

--- app/Filament/Mod/Resources/DivisionResource.php
+++ app/Filament/Mod/Resources/DivisionResource.php
@@ -116,7 +116,6 @@ class DivisionResource extends Resource
                         Forms\Components\TextInput::make('Applications Feed')
                             ->label('Recruit Applications RSS URL')
                             ->statePath('recruitment_rss_feed')
-                            ->required()
                             ->helperText('RSS feed URL where new division applications are posted'),
 
                         Forms\Components\Section::make('Tasks')->collapsible()->collapsed()
