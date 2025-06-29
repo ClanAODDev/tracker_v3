5952d22060a1dff6adecb3b7a981a3bee103660d
Andrew Bashore <andrew@bashtech.net>
2025-06-29 16:07:42 -0400

    Urlencode handles when used in a URL

--- resources/views/member/partials/handle-link.blade.php
+++ resources/views/member/partials/handle-link.blade.php
@@ -1,11 +1,11 @@
-<a href="{{ $handle->url }}{{ $handle->pivot->value }}"
+<a href="{{ $handle->full_url }}"
    target="_blank" class="panel panel-filled panel-c-success">
     <div class="panel-body ">
         <small class="c-white slight text-uppercase">
             {{ $handle->label }}
         </small>
         <span class="pull-right"><i class="fa fa-link"></i></span>
-        <br />
+        <br/>
         <span class="text-uppercase">{{ $handle->pivot->value }}</span>
     </div>
 </a>
