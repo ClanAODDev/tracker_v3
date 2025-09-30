ee02096731e778a6669982f51cf9ea13b0b2d654
mightyguybrush <dcdeaton@ncsu.edu>
2025-09-30 04:44:06 -0400

    Revamp promotions, improve retention reports This PR updates the promotion report to list promotions that happened at a given time rather than just those members who were last promoted that month. We don't currently record the division the promotion occurred in, so divisions will see historical promotions for any member currently assigned to them, even if the promotion happened in a previous division.
    Also makes some improvements to the retention report, aggregating counts in SQL rather than PHP for performance, improves `endDate` handling for chart series

--- app/Repositories/MemberRepository.php
+++ app/Repositories/MemberRepository.php
@@ -3,28 +3,11 @@
 namespace App\Repositories;
 
 use App\Models\Member;
-use Illuminate\Support\Facades\DB;
 
 class MemberRepository
 {
     public function search($name)
     {
         return Member::where('name', 'LIKE', "%{$name}%")->get();
-    }
-
-    /**
-     * Returns promotion periods for members in the
-     * form of year and month.
-     *
-     * @return static
-     */
-    public function promotionPeriods()
-    {
-        return collect(DB::select("
-            SELECT Year(last_promoted_at) AS year, MONTHNAME(STR_TO_DATE(Month(last_promoted_at), '%m')) AS month
-            FROM members 
-            GROUP BY Year(last_promoted_at), Month(last_promoted_at)
-            ORDER BY year DESC
-        "))->filter(fn ($values) => $values->month !== null);
     }
 }
