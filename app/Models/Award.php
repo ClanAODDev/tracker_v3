91bb50216d58dd0d97df8679ad7752a68fa9f5c4
Guybrush <dcdeaton@ncsu.edu>
2025-12-01 13:13:48 -0500

    Soft-delete awards
    To preserve artwork in case of reuse

--- app/Models/Award.php
+++ app/Models/Award.php
@@ -2,12 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Award extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
 
     protected $guarded = [];
 
