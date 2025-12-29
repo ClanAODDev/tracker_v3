708e4cb98c5075c7acfc5bbedc42a2fbeb3d788a
Guybrush <dcdeaton@ncsu.edu>
2025-12-29 15:16:37 -0500

    Add accent to manage action

--- resources/views/division/partials/edit-division-button.blade.php
+++ resources/views/division/partials/edit-division-button.blade.php
@@ -1,6 +1,6 @@
 @can('update', $division)
     <a href="{{ route('filament.mod.resources.divisions.edit', $division) }}"
-       title="Edit {{ $division->name }}" class="btn btn-default pull-right">
+       title="Edit {{ $division->name }}" class="btn btn-accent pull-right">
         <i class="fa fa-wrench text-accent"></i> <span class="hidden-xs">Manage Division</span>
     </a>
 @endcan
