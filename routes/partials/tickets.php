fe131c021799b2c2dceb86bac0d80d80998f42dd
Guybrush <dcdeaton@ncsu.edu>
2026-01-01 17:27:46 -0500

    Fix missing route name

--- routes/partials/tickets.php
+++ routes/partials/tickets.php
@@ -7,5 +7,5 @@ Route::prefix('help/tickets')->name('help.tickets.')->middleware('auth')->group(
 
     Route::get('setup', fn () => redirect()->route('help.tickets.widget'));
     Route::get('create', fn () => redirect()->route('help.tickets.widget'));
-    Route::get('{ticket}', fn () => redirect()->route('help.tickets.widget'));
+    Route::get('{ticket}', fn () => redirect()->route('help.tickets.widget'))->name('show');
 });
