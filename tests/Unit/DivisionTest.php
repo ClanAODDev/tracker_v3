3f1e7aab27b866ee87b9d59cb8aebd42cf7e3913
Guybrush <dcdeaton@ncsu.edu>
2026-01-02 18:42:38 -0500

    Avoid handle factory for division test

--- tests/Unit/DivisionTest.php
+++ tests/Unit/DivisionTest.php
@@ -11,7 +11,10 @@ final class DivisionTest extends TestCase
     #[Test]
     public function it_has_a_lowercase_abbreviation()
     {
-        $division = Division::factory(['abbreviation' => 'UPPERCASE'])->make();
+        $division = Division::factory()->make([
+            'abbreviation' => 'UPPERCASE',
+            'handle_id' => 1,
+        ]);
 
         $this->assertSame($division->abbreviation, 'uppercase');
     }
