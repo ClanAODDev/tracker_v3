bf5c452a3b34e2f1d74caf4e9517240ca731bf7f
Guybrush <me@chrisdeaton.dev>
2026-04-20 09:08:47 -0400

    Propagate BotChannel failures so jobs retry

--- app/Channels/BotChannel.php
+++ app/Channels/BotChannel.php
@@ -55,7 +55,7 @@ class BotChannel
                 'error'        => $e->getMessage(),
             ]);
 
-            return;
+            throw $e;
         }
 
         if ($notification instanceof NotifyAdminTicketCreated) {
