c6c7c0c2e3804163c21bf780238d2c857aec476e
Guybrush <me@chrisdeaton.dev>
2026-09-20 12:36:49 -0400

    Use array validation rule format consistently

--- app/Http/Requests/API/UpdateDivision.php
+++ app/Http/Requests/API/UpdateDivision.php
@@ -30,8 +30,8 @@ class UpdateDivision extends FormRequest
     public function rules()
     {
         return [
-            'officer_channel'  => 'nullable|alpha_dash',
-            'member_channel'   => 'nullable|alpha_dash',
+            'officer_channel'  => ['nullable', 'alpha_dash'],
+            'member_channel'   => ['nullable', 'alpha_dash'],
             'division_channel' => ['nullable', 'regex:/^\d{17,19}$/'],
         ];
     }
